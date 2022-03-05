a6313f01df29c364b84330cdf7608052dec15931
Sebastian Menski <sebastian.menski@camunda.com>
2022-03-05 12:17:38 +0100

    ci: add release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -xue
+
+RELEASE_VERSION=${1}
+NEXT_VERSION=${2}-SNAPSHOT
+RELEASE_MESSAGE="ci: release version ${RELEASE_VERSION}"
+NEXT_MESSAGE="ci: set next version ${NEXT_VERSION}"
+
+mvn --batch-mode versions:set -DnewVersion=${RELEASE_VERSION} -DgenerateBackupPoms=false
+mvn --batch-mode verify
+git commit -a -m "${RELEASE_MESSAGE}"
+git tag -a ${RELEASE_VERSION} -m "${RELEASE_MESSAGE}"
+mvn --batch-mode versions:set -DnewVersion=${NEXT_VERSION} -DgenerateBackupPoms=false
+mvn --batch-mode verify
+git commit -a -m "${NEXT_MESSAGE}"
+git push --follow-tags
