e8d191e4328613262ed44554af2b3b5a350b4fb2
Tobias Metzke-Bernstein <tobias.metzke@camunda.com>
2022-07-04 07:47:40 +0200

    chore: secret handling & testing
    * Secret handling is provided by the SDK, secret provider is dynamic
    * Context builder is provided for convenient test setup

--- connector-runtime-job-worker/src/test/java/io/camunda/connector/runtime/jobworker/TestSecretProvider.java
+++ connector-runtime-job-worker/src/test/java/io/camunda/connector/runtime/jobworker/TestSecretProvider.java
@@ -1,21 +1,20 @@
 package io.camunda.connector.runtime.jobworker;
 
-import io.camunda.connector.sdk.SecretStore;
+import io.camunda.connector.sdk.SecretProvider;
 import java.util.Collections;
 import java.util.Map;
-import java.util.Optional;
 
-public class TestSecretStore implements SecretStore {
+public class TestSecretProvider implements SecretProvider {
 
-  public static final String SECRET_NAME = "foo";
+  public static final String SECRET_NAME = "FOO";
   public static final String SECRET_VALUE = "bar";
 
   private static final Map<String, String> SECRETS =
       Collections.singletonMap(SECRET_NAME, SECRET_VALUE);
 
   @Override
-  public String replaceSecret(String value) {
+  public String getSecret(String value) {
     System.out.println("REPLACE " + value);
-    return Optional.ofNullable(SECRETS.get(value)).orElse(value);
+    return SECRETS.get(value);
   }
 }
