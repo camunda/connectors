e7635c0418ce7ef2b7de9b6b5aa8b4f6338e27c1
Nico Rehwaldt <git_nikku@nixis.de>
2022-07-10 09:47:05 +0200

    chore(sdk): rework ConnectorContextBuilder
    The context builder is now explicit in terms of variables it expects.
    
    No serialization / deserialization anymore in the core.

---
+++ connector-sdk/src/test/java/io/camunda/connector/sdk/test/ConnectorContextBuilderTest.java
@@ -0,0 +1,147 @@
+package io.camunda.connector.sdk.test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Map;
+import org.junit.Test;
+
+public class ConnectorContextBuilderTest {
+
+  @Test
+  public void shouldProvideVariablesAsObject() {
+
+    // given
+    var obj = new Object();
+
+    // when
+    var context = ConnectorContextBuilder.create().variables(obj).build();
+
+    // then
+    assertThat(context.getVariablesAsType(Object.class)).isEqualTo(obj);
+  }
+
+  @Test
+  public void shouldProvideVariablesAsObject_failIfNotProvided() {
+
+    // given
+    var context = ConnectorContextBuilder.create().build();
+
+    // when
+    var exception =
+        catchException(
+            () -> {
+              context.getVariablesAsType(Object.class);
+            });
+
+    // then
+    assertThat(exception).hasMessage("variablesAsObject not provided");
+  }
+
+  @Test
+  public void shouldProvideVariablesAsObject_failIfIncompatible() {
+
+    // given
+    var set = new HashSet<String>();
+
+    var context = ConnectorContextBuilder.create().variables(set).build();
+
+    // when
+    var exception =
+        catchException(
+            () -> {
+              context.getVariablesAsType(Map.class);
+            });
+
+    // then
+    assertThat(exception).hasMessage("no variablesAsObject of type java.util.Map provided");
+  }
+
+  @Test
+  public void shouldProvideVariablesAsString() {
+
+    // given
+    var json = "{ \"foo\" : \"FOO\" }";
+
+    // when
+    var context = ConnectorContextBuilder.create().variables(json).build();
+
+    // then
+    assertThat(context.getVariables()).isEqualTo(json);
+  }
+
+  @Test
+  public void shouldProvideVariablesAsString_failIfNotProvided() {
+
+    // given
+    var context = ConnectorContextBuilder.create().build();
+
+    // when
+    var exception =
+        catchException(
+            () -> {
+              context.getVariables();
+            });
+
+    // then
+    assertThat(exception).hasMessage("variablesAsJSON not provided");
+  }
+
+  @Test
+  public void shouldThrowOnConflictingVariableDefinitions() {
+
+    // when
+    var exception =
+        catchException(
+            () -> {
+              ConnectorContextBuilder.create().variables("{ }").variables(new Object());
+            });
+
+    // then
+    assertThat(exception).hasMessage("variablesAsJSON already set");
+  }
+
+  @Test
+  public void shouldThrowOnDuplicateVariableDefinition() {
+
+    // when
+    var exception =
+        catchException(
+            () -> {
+              ConnectorContextBuilder.create().variables("{ }").variables("{ }");
+            });
+
+    // then
+    assertThat(exception).hasMessage("variablesAsJSON already set");
+  }
+
+  @Test
+  public void shouldProvideSecret() {
+
+    // given
+    var context = ConnectorContextBuilder.create().secret("foo", "FOO").build();
+
+    // when
+    var replaced = context.getSecretStore().replaceSecret("secrets.foo");
+
+    // then
+    assertThat(replaced).isEqualTo("FOO");
+  }
+
+  @Test
+  public void shouldProvideSecret_failIfNotProvided() {
+
+    // given
+    var context = ConnectorContextBuilder.create().build();
+
+    // when
+    var exception =
+        catchException(
+            () -> {
+              context.getSecretStore().replaceSecret("secrets.foo");
+            });
+
+    // then
+    assertThat(exception).hasMessage("Secret with name 'foo' is not available");
+  }
+}
