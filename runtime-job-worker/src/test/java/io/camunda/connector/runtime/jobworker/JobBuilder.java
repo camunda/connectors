d74c5fd9a585a4974a69157a4cbbbcc8a40c0db2
Nico Rehwaldt <git_nikku@nixis.de>
2022-08-16 12:34:29 +0200

    test: refactor job handler secrets tests
    * use static imports of test utilities
    * provide a simple abstraction around the endless mocking we do
    * use that abstraction for simple, understandable tests
    * create explicit tests that verify output mapping (with and without
      result variable)

---
+++ runtime-job-worker/src/test/java/io/camunda/connector/runtime/jobworker/JobBuilder.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.connector.runtime.jobworker;
+
+import static io.camunda.connector.runtime.jobworker.ConnectorJobHandler.RESULT_VARIABLE_HEADER_NAME;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import io.camunda.zeebe.client.api.command.CompleteJobCommandStep1;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import java.util.Map;
+import org.mockito.ArgumentCaptor;
+
+class JobBuilder {
+
+  public static class JobBuilderStep {
+
+    private final JobClient jobClient;
+    private final ActivatedJob job;
+    private final CompleteJobCommandStep1 completeCommand;
+
+    public JobBuilderStep() {
+
+      this.jobClient = mock(JobClient.class);
+      this.job = mock(ActivatedJob.class);
+      this.completeCommand = mock(CompleteJobCommandStep1.class, RETURNS_DEEP_STUBS);
+
+      when(jobClient.newCompleteCommand(any())).thenReturn(completeCommand);
+      when(job.getKey()).thenReturn(-1l);
+    }
+
+    public JobBuilderStep withHeaders(Map<String, String> headers) {
+      when(job.getCustomHeaders()).thenReturn(headers);
+
+      return this;
+    }
+
+    public JobBuilderStep withResultHeader(String value) {
+      return withHeader(RESULT_VARIABLE_HEADER_NAME, value);
+    }
+
+    public JobBuilderStep withHeader(String key, String value) {
+      return withHeaders(Map.of(key, value));
+    }
+
+    public JobResult execute(ConnectorJobHandler connectorJobHandler) {
+
+      // when
+      connectorJobHandler.handle(jobClient, job);
+
+      var variablesCaptor = ArgumentCaptor.forClass(Map.class);
+
+      // then
+      verify(completeCommand).variables(variablesCaptor.capture());
+
+      return new JobResult(variablesCaptor.getValue());
+    }
+  }
+
+  public static class JobResult {
+
+    private final Map<String, Object> variables;
+
+    public JobResult(Map<String, Object> variables) {
+      this.variables = variables;
+    }
+
+    public Map<String, Object> getVariables() {
+      return variables;
+    }
+
+    public Object getVariable(String key) {
+      return variables.get(key);
+    }
+  }
+
+  static JobBuilderStep create() {
+    return new JobBuilderStep();
+  }
+}
