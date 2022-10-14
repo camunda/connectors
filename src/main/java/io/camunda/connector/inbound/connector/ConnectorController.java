2d54baf9a97899f41a62340079408bf9ee81dbdb
Nico Rehwaldt <git_nikku@nixis.de>
2022-10-14 08:43:31 +0200

    chore: explicitly declare webhook data type

--- src/main/java/io/camunda/connector/inbound/connector/ConnectorController.java
+++ src/main/java/io/camunda/connector/inbound/connector/ConnectorController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -31,7 +32,10 @@ public class ConnectorController {
     this.feelEngine = feelEngine;
   }
 
-  @PostMapping("/inbound/{context}")
+  @PostMapping(
+      value = "/inbound/{context}",
+      produces = MediaType.APPLICATION_JSON_VALUE,
+      consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<ProcessInstanceEvent> inbound(
       @PathVariable String context,
       @RequestBody Map<String, Object> body,
