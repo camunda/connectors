ea0ce728bea44ddda2c737c64e7374ba296e8bdf
Igor Petrov <igor.petrov-ext@camunda.com>
2022-11-10 18:51:53 +0200

    chore: added kafka response handling

--- connectors/kafka/src/main/java/io/camunda/connector/model/KafkaConnectorResponse.java
+++ connectors/kafka/src/main/java/io/camunda/connector/model/KafkaConnectorResponse.java
@@ -6,15 +6,77 @@
  */
 package io.camunda.connector.model;
 
+import java.util.Objects;
+
 public class KafkaConnectorResponse {
 
-  private Object kafkaResponse;
+  private String topic;
+  private long timestamp;
+  private long offset;
+  private int partition;
 
-  public Object getResponseValue() {
-    return kafkaResponse;
+  public String getTopic() {
+    return topic;
   }
 
-  public void setResponseValue(Object responseValue) {
-    this.kafkaResponse = responseValue;
+  public void setTopic(String topic) {
+    this.topic = topic;
+  }
+
+  public long getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public long getOffset() {
+    return offset;
+  }
+
+  public void setOffset(long offset) {
+    this.offset = offset;
+  }
+
+  public int getPartition() {
+    return partition;
+  }
+
+  public void setPartition(int partition) {
+    this.partition = partition;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) return false;
+    KafkaConnectorResponse that = (KafkaConnectorResponse) o;
+    return timestamp == that.timestamp
+        && offset == that.offset
+        && partition == that.partition
+        && topic.equals(that.topic);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(topic, timestamp, offset, partition);
+  }
+
+  @Override
+  public String toString() {
+    return "KafkaConnectorResponse{"
+        + "topic='"
+        + topic
+        + '\''
+        + ", timestamp="
+        + timestamp
+        + ", offset="
+        + offset
+        + ", partition="
+        + partition
+        + '}';
   }
 }
