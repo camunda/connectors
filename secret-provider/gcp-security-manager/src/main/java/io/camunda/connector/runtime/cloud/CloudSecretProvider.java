2cb0d3aae206dd5bd736379c27de332cd172cf31
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-11-22 15:24:08 +0100

    feat: Added ConnectorPropertyResolver

--- secret-provider/gcp-security-manager/src/main/java/io/camunda/connector/runtime/cloud/CloudSecretProvider.java
+++ secret-provider/gcp-security-manager/src/main/java/io/camunda/connector/runtime/cloud/CloudSecretProvider.java
@@ -19,30 +19,65 @@ package io.camunda.connector.runtime.cloud;
 import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 import com.google.cloud.secretmanager.v1.SecretVersionName;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import io.camunda.connector.api.error.ConnectorException;
 import io.camunda.connector.api.secret.SecretProvider;
+import io.camunda.connector.impl.config.ConnectorConfigurationUtil;
 import java.lang.reflect.Type;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CloudSecretProvider implements SecretProvider {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CloudSecretProvider.class);
+  private final Gson gson;
+  private final String clusterId;
   private static final Type MAP_TYPE = new TypeToken<Map<String, String>>() {}.getType();
+
   public static final String SECRETS_PROJECT_ENV_NAME = "SECRETS_PROJECT_ID";
   public static final String SECRETS_PREFIX_ENV_NAME = "SECRETS_PREFIX";
+  public static final String CLUSTER_ID_ENV_NAME = "CAMUNDA_CLUSTER_ID";
 
-  private final Map<String, String> secrets;
+  // private Map<String, String> secrets = new HashMap<>();
+  private static final String CACHE_KEY = "SECRETS";
+  LoadingCache<String, Map<String, String>> secretsCache;
 
-  public CloudSecretProvider(final Gson gson, final String clusterId) {
-    String json = loadGoogleSecrets(clusterId);
-    secrets = gson.fromJson(json, MAP_TYPE);
+  public CloudSecretProvider() {
+    this(new GsonBuilder().create(), ConnectorConfigurationUtil.getProperty(CLUSTER_ID_ENV_NAME));
   }
 
-  private static String loadGoogleSecrets(final String clusterId) {
+  public CloudSecretProvider(Gson gson, String clusterId) {
+    this.gson = gson;
+    this.clusterId = clusterId;
+    this.setupSecretsCache();
+  }
+
+  public void setupSecretsCache() {
+    // Load secrets via this loader function whenever necessary
+    CacheLoader<String, Map<String, String>> loader =
+        new CacheLoader<String, Map<String, String>>() {
+          @Override
+          public Map<String, String> load(String key) {
+            return unwrapSecrets(loadGoogleSecrets(clusterId));
+          }
+        };
+    secretsCache = CacheBuilder.newBuilder().refreshAfterWrite(5, TimeUnit.SECONDS).build(loader);
+  }
+
+  protected Map<String, String> unwrapSecrets(final String secretsAsjson) {
+    return gson.fromJson(secretsAsjson, MAP_TYPE);
+  }
+
+  protected String loadGoogleSecrets(final String clusterId) {
     Objects.requireNonNull(clusterId, "You need to specify the clusterId to load secrets for");
     LOGGER.info("Fetching secrets for cluster {} from secret manager", clusterId);
     try (final SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
@@ -68,6 +103,10 @@ public class CloudSecretProvider implements SecretProvider {
 
   @Override
   public String getSecret(String name) {
-    return secrets.get(name);
+    try {
+      return secretsCache.get(CACHE_KEY).get(name);
+    } catch (ExecutionException e) {
+      throw new ConnectorException("Could not resolve secrets: " + e.getMessage(), e);
+    }
   }
 }
