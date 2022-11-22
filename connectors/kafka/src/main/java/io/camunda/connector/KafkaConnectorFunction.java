7b038a4d7066d2fb8b70ffd476d895752a3022d8
Igor Petrov <igor.petrov-ext@camunda.com>
2022-11-22 17:48:57 +0200

    fix: properly close kafka producer

--- connectors/kafka/src/main/java/io/camunda/connector/KafkaConnectorFunction.java
+++ connectors/kafka/src/main/java/io/camunda/connector/KafkaConnectorFunction.java
@@ -7,15 +7,14 @@
 package io.camunda.connector;
 
 import io.camunda.connector.api.annotation.OutboundConnector;
+import io.camunda.connector.api.error.ConnectorException;
 import io.camunda.connector.api.outbound.OutboundConnectorContext;
 import io.camunda.connector.api.outbound.OutboundConnectorFunction;
 import io.camunda.connector.model.KafkaConnectorRequest;
 import io.camunda.connector.model.KafkaConnectorResponse;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.function.Function;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
@@ -51,22 +50,28 @@ public class KafkaConnectorFunction implements OutboundConnectorFunction {
     return executeConnector(connectorRequest);
   }
 
-  private KafkaConnectorResponse executeConnector(final KafkaConnectorRequest request)
-      throws ExecutionException, InterruptedException, TimeoutException {
+  private KafkaConnectorResponse executeConnector(final KafkaConnectorRequest request) {
     Properties props = request.assembleKafkaClientProperties();
     Producer<String, String> producer = producerCreatorFunction.apply(props);
-    Future<RecordMetadata> kafkaResponse =
-        producer.send(
-            new ProducerRecord<>(
-                request.getTopic().getTopicName(),
-                request.getMessage().getKey().toString(),
-                request.getMessage().getValue().toString()));
-    KafkaConnectorResponse result = new KafkaConnectorResponse();
-    RecordMetadata recordMetadata = kafkaResponse.get(60, TimeUnit.SECONDS);
-    result.setTopic(recordMetadata.topic());
-    result.setPartition(recordMetadata.partition());
-    result.setOffset(recordMetadata.offset());
-    result.setTimestamp(recordMetadata.timestamp());
-    return result;
+    RecordMetadata recordMetadata;
+    try {
+      Future<RecordMetadata> kafkaResponse =
+          producer.send(
+              new ProducerRecord<>(
+                  request.getTopic().getTopicName(),
+                  request.getMessage().getKey().toString(),
+                  request.getMessage().getValue().toString()));
+      KafkaConnectorResponse result = new KafkaConnectorResponse();
+      recordMetadata = kafkaResponse.get(45, TimeUnit.SECONDS);
+      result.setTopic(recordMetadata.topic());
+      result.setPartition(recordMetadata.partition());
+      result.setOffset(recordMetadata.offset());
+      result.setTimestamp(recordMetadata.timestamp());
+      return result;
+    } catch (Exception e) {
+      throw new ConnectorException("001", "Kafka Producer execution exception", e);
+    } finally {
+      producer.close();
+    }
   }
 }
