0fa0d40bdc4dd000c07782d320033a846c5d1851
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-11-25 12:18:24 +0100

    fixed package name

--- bundle/mvn/camunda-saas-bundle/src/test/java/io/camunda/connector/runtime/LocalConnectorRuntime.java
+++ bundle/mvn/camunda-saas-bundle/src/test/java/io/camunda/connector/runtime/LocalConnectorRuntime.java
@@ -14,9 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.camunda.connector.bundle;
+package io.camunda.connector.runtime;
 
-import io.camunda.connector.runtime.SaaSConnectorRuntimeApplication;
 import org.springframework.boot.SpringApplication;
 
 public class LocalConnectorRuntime {
