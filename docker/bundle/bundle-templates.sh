70007a59bb0455af110c65aa4b254a5b997d10a0
Pavel Kotelevsky <pavel.kotelevskii@camunda.com>
2022-12-06 15:14:58 +0100

    adjust build for monorepo

--- docker/bundle/bundle-templates.sh
+++ docker/bundle/bundle-templates.sh
@@ -13,16 +13,10 @@ trap 'rm -rf -- "${WORKING_DIR}"' EXIT
 
 pushd ${WORKING_DIR}
 
-wget -i - << EOF
-https://github.com/camunda/connector-aws-lambda/releases/download/${AWS_LAMBDA_VERSION}/aws-lambda-connector.json
-https://github.com/camunda/connector-google-drive/releases/download/${GOOGLE_DRIVE_VERSION}/google-drive-connector.json
-https://github.com/camunda/connector-http-json/releases/download/${HTTP_JSON_VERSION}/http-json-connector.json
-https://github.com/camunda/connector-rabbitmq/releases/download/${RABBITMQ_VERSION}/rabbitmq-connector.json
-https://github.com/camunda/connector-sendgrid/releases/download/${SENDGRID_VERSION}/sendgrid-connector.json
-https://github.com/camunda/connector-slack/releases/download/${SLACK_VERSION}/slack-connector.json
-https://github.com/camunda/connector-sqs/releases/download/${SQS_VERSION}/aws-sqs-connector.json
-https://github.com/camunda/connector-sns/releases/download/${SNS_VERSION}/aws-sns-connector.json
-EOF
+find ${ARTIFACT_DIR} -name "*.json" | grep "element-templates" |\
+while read TEMPLATE_FILE;
+do cp $TEMPLATE_FILE ${WORKING_DIR};
+done
 
 tag_version() {
   local file=$1
