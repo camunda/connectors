0ae803049707b08b835cec2879cf70e6d05b9bc0
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-12-06 11:07:46 +0100

    Fetching Operate credentials from Secret Manager

--- bundle/mvn/camunda-saas-bundle/src/main/java/io/camunda/connector/runtime/SaaSConfiguration.java
+++ bundle/mvn/camunda-saas-bundle/src/main/java/io/camunda/connector/runtime/SaaSConfiguration.java
@@ -19,14 +19,41 @@ package io.camunda.connector.runtime;
 import io.camunda.connector.api.secret.SecretProvider;
 import io.camunda.connector.runtime.cloud.GcpSecretManagerSecretProvider;
 import io.camunda.zeebe.spring.client.properties.ZeebeClientConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SaaSConfiguration {
 
-  @Bean
-  public SecretProvider getSecretProvider(ZeebeClientConfigurationProperties conf) {
-    return new GcpSecretManagerSecretProvider(conf.getCloud().getClusterId());
+  @Value("${camunda.saas.secrets.projectId}")
+  private String secretsProjectId;
+
+  // TODO: Default value?
+  @Value("${camunda.saas.secrets.prefix}")
+  private String secretsNamePrefix;
+
+  /**
+   * Internal prefix used for M2M tokens
+   */
+  @Value("${camunda.saas.secrets.internalPrefix:internal-connector-secrets-}")
+  private String secretsInternalNamePrefix;
+
+  private final ZeebeClientConfigurationProperties conf;
+
+  @Autowired
+  public SaaSConfiguration(ZeebeClientConfigurationProperties conf) {
+    this.conf = conf;
   }
+
+  @Bean
+  public SecretProvider getSecretProvider() {
+    return new GcpSecretManagerSecretProvider(conf.getCloud().getClusterId(), secretsProjectId, secretsNamePrefix);
+  }
+
+  public SecretProvider getInternalSecretProvider() {
+    return new GcpSecretManagerSecretProvider(conf.getCloud().getClusterId(), secretsProjectId, secretsInternalNamePrefix);
+  }
+
 }
