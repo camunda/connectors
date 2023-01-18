837c5dcb20b2a6016d1b8b1619517947554b808d
Igor Petrov <igor.petrov-ext@camunda.com>
2023-01-18 17:02:10 +0200

    fix(sqs): pass JSONs instead of key-value's in payload

--- connectors/sqs/src/test/java/io/camunda/connector/SqsConnectorFunctionTest.java
+++ connectors/sqs/src/test/java/io/camunda/connector/SqsConnectorFunctionTest.java
@@ -19,6 +19,7 @@ import io.camunda.connector.test.outbound.OutboundConnectorContextBuilder;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
@@ -34,7 +35,7 @@ public class SqsConnectorFunctionTest extends BaseTest {
         OutboundConnectorContextBuilder.create()
             .secret(AWS_ACCESS_KEY, ACTUAL_ACCESS_KEY)
             .secret(AWS_SECRET_KEY, ACTUAL_SECRET_KEY)
-            .variables(DEFAULT_REQUEST_BODY)
+            .variables(DEFAULT_REQUEST_BODY_WITH_JSON_PAYLOAD)
             .build();
     sendMessageResult = new SendMessageResult();
     sendMessageResult.setMessageId(MSG_ID);
@@ -76,5 +77,63 @@ public class SqsConnectorFunctionTest extends BaseTest {
     Assertions.assertThat(execute).isInstanceOf(SqsConnectorResult.class);
     var result = (SqsConnectorResult) execute;
     Assertions.assertThat(result.getMessageId()).isEqualTo(MSG_ID);
+  }
+
+  @Test
+  public void execute_shouldPassPayloadAsJsonWhenJsonArrivesFromForm() {
+    // Given
+    AmazonSQS sqsClient = Mockito.mock(AmazonSQS.class);
+    Mockito.when(sqsClient.sendMessage(ArgumentMatchers.any(SendMessageRequest.class)))
+        .thenReturn(sendMessageResult);
+    SqsClientSupplier sqsClientSupplier = Mockito.mock(SqsClientSupplier.class);
+    Mockito.when(
+            sqsClientSupplier.sqsClient(
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString()))
+        .thenReturn(sqsClient);
+    ArgumentCaptor<SendMessageRequest> captor = ArgumentCaptor.forClass(SendMessageRequest.class);
+    Mockito.when(sqsClient.sendMessage(captor.capture())).thenReturn(new SendMessageResult());
+    connector = new SqsConnectorFunction(sqsClientSupplier, GSON);
+
+    // When
+    connector.execute(context);
+
+    // Then
+    SendMessageRequest capturedRequest = captor.getValue();
+    Assertions.assertThat(capturedRequest.getMessageBody()).isEqualTo("{\"data\":\"ok\"}");
+  }
+
+  @Test
+  public void execute_shouldPassPayloadAsStringWhenStringArrivesFromForm() {
+    // Given
+    context =
+        OutboundConnectorContextBuilder.create()
+            .secret(AWS_ACCESS_KEY, ACTUAL_ACCESS_KEY)
+            .secret(AWS_SECRET_KEY, ACTUAL_SECRET_KEY)
+            .variables(DEFAULT_REQUEST_BODY_WITH_STRING_PAYLOAD)
+            .build();
+    sendMessageResult = new SendMessageResult();
+    sendMessageResult.setMessageId(MSG_ID);
+    AmazonSQS sqsClient = Mockito.mock(AmazonSQS.class);
+    Mockito.when(sqsClient.sendMessage(ArgumentMatchers.any(SendMessageRequest.class)))
+        .thenReturn(sendMessageResult);
+    SqsClientSupplier sqsClientSupplier = Mockito.mock(SqsClientSupplier.class);
+    Mockito.when(
+            sqsClientSupplier.sqsClient(
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString()))
+        .thenReturn(sqsClient);
+    ArgumentCaptor<SendMessageRequest> captor = ArgumentCaptor.forClass(SendMessageRequest.class);
+    Mockito.when(sqsClient.sendMessage(captor.capture())).thenReturn(new SendMessageResult());
+    connector = new SqsConnectorFunction(sqsClientSupplier, GSON);
+
+    // When
+    connector.execute(context);
+
+    // Then
+    SendMessageRequest capturedRequest = captor.getValue();
+    Assertions.assertThat(capturedRequest.getMessageBody()).isEqualTo("I am a string value!");
   }
 }
