518a5bab9d00d7bdcb958ab4ffa903d9951eef26
markfarkas-camunda <mark.farkas@camunda.com>
2023-02-10 13:03:31 +0100

    fix(graphql): fix code style

--- connectors/graphql/src/main/java/io/camunda/connector/graphql/GraphQLFunction.java
+++ connectors/graphql/src/main/java/io/camunda/connector/graphql/GraphQLFunction.java
@@ -40,7 +40,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Map;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
