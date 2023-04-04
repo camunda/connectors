e710d7d269c6a40c70dcdb0db7594b7d82c5e9e1
markfarkas-camunda <mark.farkas@camunda.com>
2023-04-04 13:05:25 +0200

    fix: refactor LocalConnectorRuntime

--- bundle/mvn/default-bundle/src/test/java/io/camunda/connector/bundle/LocalConnectorRuntime.java
+++ bundle/mvn/default-bundle/src/test/java/io/camunda/connector/bundle/LocalConnectorRuntime.java
@@ -16,13 +16,12 @@
  */
 package io.camunda.connector.bundle;
 
+import io.camunda.connector.runtime.ConnectorRuntimeApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class LocalConnectorRuntime {
 
   public static void main(String[] args) {
-    SpringApplication.run(LocalConnectorRuntime.class, args);
+    SpringApplication.run(ConnectorRuntimeApplication.class, args);
   }
 }
