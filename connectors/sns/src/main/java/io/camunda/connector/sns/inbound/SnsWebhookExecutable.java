c34ffd77bd365891444f901b073fd03adad670b1
Igor Petrov <igor.petrov-ext@camunda.com>
2023-06-06 13:28:43 +0300

    chore(sns): change type to adopt to web-modeler

--- connectors/sns/src/main/java/io/camunda/connector/sns/inbound/SnsWebhookExecutable.java
+++ connectors/sns/src/main/java/io/camunda/connector/sns/inbound/SnsWebhookExecutable.java
@@ -28,7 +28,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@InboundConnector(name = "SNS_INBOUND", type = "io.camunda:aws-sns-inbound:1")
+@InboundConnector(name = "SNS_INBOUND", type = "io.camunda:aws-sns-webhook:1")
 public class SnsWebhookExecutable implements WebhookConnectorExecutable {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SnsWebhookExecutable.class);
