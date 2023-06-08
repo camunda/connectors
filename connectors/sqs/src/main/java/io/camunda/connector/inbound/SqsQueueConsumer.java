ed64aa41bee1920588ab0c06fb28a72914452874
Oleksii Ivanov <oleksii.ivanov-ext@camunda.com>
2023-06-08 12:24:05 +0300

    fix(sqs inbound): add try catch for correlate method

--- connectors/sqs/src/main/java/io/camunda/connector/inbound/SqsQueueConsumer.java
+++ connectors/sqs/src/main/java/io/camunda/connector/inbound/SqsQueueConsumer.java
@@ -12,6 +12,7 @@ import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 import io.camunda.connector.api.inbound.InboundConnectorContext;
 import io.camunda.connector.api.inbound.InboundConnectorResult;
+import io.camunda.connector.impl.ConnectorInputException;
 import io.camunda.connector.inbound.model.SqsInboundProperties;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -38,26 +39,36 @@ public class SqsQueueConsumer implements Runnable {
   @Override
   public void run() {
     LOGGER.info("Started SQS consumer for queue {}", properties.getQueue().getUrl());
+
     final ReceiveMessageRequest receiveMessageRequest = createReceiveMessageRequest();
     ReceiveMessageResult receiveMessageResult;
     do {
-      receiveMessageResult = sqsClient.receiveMessage(receiveMessageRequest);
-      List<Message> messages = receiveMessageResult.getMessages();
-      for (Message message : messages) {
-        InboundConnectorResult<?> correlate =
-            context.correlate(MessageMapper.toSqsInboundMessage(message));
-        if (correlate.isActivated()) {
-          sqsClient.deleteMessage(properties.getQueue().getUrl(), message.getReceiptHandle());
-          LOGGER.debug(
-              "Inbound event correlated successfully: {}, and message with ID {} was deleted",
-              correlate.getResponseData(),
-              message.getMessageId());
-        } else {
-          LOGGER.debug("Inbound event not correlated: {}", correlate.getErrorData());
+      try {
+        receiveMessageResult = sqsClient.receiveMessage(receiveMessageRequest);
+        List<Message> messages = receiveMessageResult.getMessages();
+        for (Message message : messages) {
+          try {
+            correlate(message);
+            sqsClient.deleteMessage(properties.getQueue().getUrl(), message.getReceiptHandle());
+          } catch (ConnectorInputException e) {
+            LOGGER.warn("NACK - failed to parse SQS message body: {}", e.getMessage());
+          }
         }
+      } catch (Exception e) {
+        LOGGER.debug("NACK - failed to correlate event", e);
       }
     } while (queueConsumerActive.get());
     LOGGER.info("Stopping SQS consumer for queue {}", properties.getQueue().getUrl());
+  }
+
+  private void correlate(final Message message) {
+    InboundConnectorResult<?> correlate =
+        context.correlate(MessageMapper.toSqsInboundMessage(message));
+    if (correlate.isActivated()) {
+      LOGGER.debug("Inbound event correlated successfully: {}", correlate.getResponseData());
+    } else {
+      LOGGER.debug("Inbound event was correlated but not activated: {}", correlate.getErrorData());
+    }
   }
 
   private ReceiveMessageRequest createReceiveMessageRequest() {
