fe1e123c3208e2933cf39ea364ab7f2457158b0e
Pavel Kotelevsky <pavel.kotelevskii@camunda.com>
2023-07-11 10:45:24 +0200

    fix: standardize object mapper config for all connectors

--- connectors/microsoft-teams/src/main/java/io/camunda/connector/suppliers/ObjectMapperSupplier.java
+++ connectors/microsoft-teams/src/main/java/io/camunda/connector/suppliers/ObjectMapperSupplier.java
@@ -10,12 +10,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.scala.DefaultScalaModule$;
 
 public final class ObjectMapperSupplier {
 
   private static final ObjectMapper objectMapper =
       new ObjectMapper()
           .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+          .registerModule(DefaultScalaModule$.MODULE$)
           .registerModule(new JavaTimeModule())
           .setSerializationInclusion(JsonInclude.Include.NON_NULL);
 
