0d1c45f27add71e53ff834e1eddf259ad7896d7d
Pavel Kotelevsky <38818382+chillleader@users.noreply.github.com>
2023-07-17 13:21:26 +0200

    feat: add jackson deserializer for FEEL expressions (#525)

---
+++ core/src/main/java/io/camunda/connector/impl/feel/FeelDeserializer.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.connector.impl.feel;
+
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import java.util.Map;
+
+/**
+ * A Jackson deserializer for FEEL expressions. It can be used to deserialize a string that contains
+ * a FEEL expression, e.g. in inbound connector properties. NB: for outbound connectors, FEEL
+ * expressions in connector variables are evaluated by Zeebe, so this deserializer is not needed.
+ */
+public class FeelDeserializer extends AbstractFeelDeserializer<Object> {
+
+  private final Class<?> outputType;
+  private static final FeelEngineWrapper FEEL_ENGINE_WRAPPER = new FeelEngineWrapper();
+
+  public FeelDeserializer() { // needed for references in @JsonDeserialize
+    this(FEEL_ENGINE_WRAPPER, Object.class);
+  }
+
+  protected FeelDeserializer(FeelEngineWrapper feelEngineWrapper, Class<?> outputType) {
+    super(feelEngineWrapper);
+    this.outputType = outputType;
+  }
+
+  @Override
+  protected Object doDeserialize(String expression) {
+    return FEEL_ENGINE_WRAPPER.evaluate(expression, Map.of(), outputType);
+  }
+
+  @Override
+  public JsonDeserializer<?> createContextual(DeserializationContext ctxt, BeanProperty property) {
+    return new FeelDeserializer(FEEL_ENGINE_WRAPPER, property.getType().getRawClass());
+  }
+}
---
+++ core/src/test/java/io/camunda/connector/impl/feel/FeelDeserializerTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.connector.impl.feel;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class FeelDeserializerTest {
+
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  @Test
+  void feelDeserializer_deserializeMap() throws JsonProcessingException {
+    // given
+    String json = """
+        { "props": "= { result: \\"foobar\\" }" }
+        """;
+
+    // when
+    TargetTypeMap targetType = mapper.readValue(json, TargetTypeMap.class);
+
+    // then
+    assertThat(targetType.props.size()).isEqualTo(1);
+    assertThat(targetType.props.get("result")).isEqualTo("foobar");
+  }
+
+  @Test
+  void feelDeserializer_deserializeNestedMap() throws JsonProcessingException {
+    // given
+    String json = """
+        { "stubObject": "= { props: { nested: \\"foobar\\" } }" }
+        """;
+
+    // when
+    TargetTypeObject targetType = mapper.readValue(json, TargetTypeObject.class);
+
+    // then
+    assertThat(targetType.stubObject.props.size()).isEqualTo(1);
+    assertThat(targetType.stubObject.props.get("nested")).isEqualTo("foobar");
+  }
+
+  @Test
+  void feelDeserializer_deserializePrimitive() throws JsonProcessingException {
+    // given
+    String json = """
+        { "props": "= \\"foobar\\"" }
+        """;
+
+    // when
+    TargetTypeString targetType = mapper.readValue(json, TargetTypeString.class);
+
+    // then
+    assertThat(targetType.props).isEqualTo("foobar");
+  }
+
+  @Test
+  void feelDeserializer_wrongType_throwsException() {
+    // given
+    String json = """
+        { "props": "= { result: \\"foobar\\" }" }
+        """;
+
+    // when & then
+    assertThrows(JsonMappingException.class, () -> mapper.readValue(json, TargetTypeString.class));
+  }
+
+  private record TargetTypeMap(@FEEL Map<String, String> props) {}
+
+  private record TargetTypeObject(@FEEL StubObject stubObject) {}
+
+  private record StubObject(Map<String, Object> props) {}
+
+  private record TargetTypeString(@FEEL String props) {}
+}
