19c302d44b089b83c4ea99a88d5e46488e21b457
Igor Petrov <igor.petrov-ext@camunda.com>
2023-08-10 17:59:31 +0300

    fix: AWS scala maps serialization

--- connectors/aws/aws-base/src/main/java/io/camunda/connector/aws/ObjectMapperSupplier.java
+++ connectors/aws/aws-base/src/main/java/io/camunda/connector/aws/ObjectMapperSupplier.java
@@ -6,22 +6,12 @@
  */
 package io.camunda.connector.aws;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.scala.DefaultScalaModule$;
+import io.camunda.connector.feel.ConnectorsObjectMapperSupplier;
 
 public final class ObjectMapperSupplier {
 
-  private static final ObjectMapper MAPPER =
-      new ObjectMapper()
-          .registerModule(new Jdk8Module())
-          .registerModule(DefaultScalaModule$.MODULE$)
-          .registerModule(new JavaTimeModule())
-          .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
-          .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+  private static final ObjectMapper MAPPER = ConnectorsObjectMapperSupplier.getCopy();
 
   private ObjectMapperSupplier() {}
 
