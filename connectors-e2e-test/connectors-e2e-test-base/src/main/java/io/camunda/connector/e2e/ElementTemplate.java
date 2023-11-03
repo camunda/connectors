832aaadce895b05865b29e0f7401db7939b4fc66
Simon <sbuettner@users.noreply.github.com>
2023-11-03 09:54:15 +0100

    Refine package structure. (#1360)
    * Refine package structure.
    
    * Fix formatting

--- connectors-e2e-test/connectors-e2e-test-base/src/main/java/io/camunda/connector/e2e/ElementTemplate.java
+++ connectors-e2e-test/connectors-e2e-test-base/src/main/java/io/camunda/connector/e2e/ElementTemplate.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.camunda.connector.test.e2e;
+package io.camunda.connector.e2e;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
