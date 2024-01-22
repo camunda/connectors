9431808a7cd38b18fc722e3d72ad71f6afcb8d81
Ev Bilske <ev.bilske@camunda.com>
2024-01-22 16:07:32 +0100

    feat: add valid test case from GitHub issue

--- connector-runtime/spring-boot-starter-camunda-connectors/src/test/java/io/camunda/connector/runtime/inbound/WebhookControllerPlainJavaTests.java
+++ connector-runtime/spring-boot-starter-camunda-connectors/src/test/java/io/camunda/connector/runtime/inbound/WebhookControllerPlainJavaTests.java
@@ -110,6 +110,7 @@ public class WebhookControllerPlainJavaTests {
   private static Stream<Arguments> webhookPathLogIfInvalidCharacters() {
     return Stream.of(
         Arguments.of(0, "validAlphaOnly"),
+              Arguments.of(0, "hello-world"),
               Arguments.of(1, "€"),
             Arguments.of(1, "-my-path"),
             Arguments.of(1, "my_path_")
