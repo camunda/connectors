594d06d372442979b504a0d2533f1f548a1d1658
Ev Bilske <ev.bilske@camunda.com>
2024-01-25 16:28:51 +0100

    fix: Use shorter "Warn" tag
    Will likely look better in UI.

--- connector-runtime/spring-boot-starter-camunda-connectors/src/test/java/io/camunda/connector/runtime/inbound/WebhookControllerLogTest.java
+++ connector-runtime/spring-boot-starter-camunda-connectors/src/test/java/io/camunda/connector/runtime/inbound/WebhookControllerLogTest.java
@@ -72,7 +72,7 @@ public class WebhookControllerLogTest {
     verify(processA1.context(), times(1)).log(activityCaptor.capture());
     Activity passedActivity = activityCaptor.getValue();
     assertEquals(Severity.WARNING, passedActivity.severity());
-    assertEquals("Warning", passedActivity.tag());
+    assertEquals("Warn", passedActivity.tag());
     assertTrue(passedActivity.message().contains(webhookPath));
 
     assertWebhookRegistered(webhookPath, webhook, processA1);
