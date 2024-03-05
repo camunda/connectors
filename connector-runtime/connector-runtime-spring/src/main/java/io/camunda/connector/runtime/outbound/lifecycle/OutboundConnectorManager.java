802cef8539b036bc08fc461aab87a8244bb60f44
Igor Petrov <108870003+igpetrov@users.noreply.github.com>
2024-03-05 19:32:40 +0200

    chore(spring-zeebe): update to relevant dep (#2101)

--- connector-runtime/connector-runtime-spring/src/main/java/io/camunda/connector/runtime/outbound/lifecycle/OutboundConnectorManager.java
+++ connector-runtime/connector-runtime-spring/src/main/java/io/camunda/connector/runtime/outbound/lifecycle/OutboundConnectorManager.java
@@ -79,13 +79,12 @@ public class OutboundConnectorManager {
 
   private void openWorkerForOutboundConnector(
       ZeebeClient client, OutboundConnectorConfiguration connector) {
-    ZeebeWorkerValue zeebeWorkerValue =
-        new ZeebeWorkerValue()
-            .name(connector.name())
-            .type(connector.type())
-            .fetchVariables(connector.inputVariables())
-            .timeout(connector.timeout())
-            .autoComplete(true);
+    ZeebeWorkerValue zeebeWorkerValue = new ZeebeWorkerValue();
+    zeebeWorkerValue.setName(connector.name());
+    zeebeWorkerValue.setType(connector.type());
+    zeebeWorkerValue.setFetchVariables(connector.inputVariables());
+    zeebeWorkerValue.setTimeout(connector.timeout());
+    zeebeWorkerValue.setAutoComplete(true);
 
     OutboundConnectorFunction connectorFunction = connectorFactory.getInstance(connector.type());
     LOG.trace("Opening worker for connector {}", connector.name());
