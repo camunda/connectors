89025cf5d190e2850b7874ac50848acfbfa1d70b
Pavel Kotelevsky <pavel.kotelevskii@camunda.com>
2024-04-25 17:35:09 +0200

    bump zeebe client version

--- connector-runtime/connector-runtime-core/src/test/java/io/camunda/connector/runtime/core/testutil/command/PublishMessageCommandDummy.java
+++ connector-runtime/connector-runtime-core/src/test/java/io/camunda/connector/runtime/core/testutil/command/PublishMessageCommandDummy.java
@@ -42,6 +42,11 @@ public class PublishMessageCommandDummy
   }
 
   @Override
+  public PublishMessageCommandStep3 withoutCorrelationKey() {
+    return null;
+  }
+
+  @Override
   public PublishMessageCommandStep3 messageId(String messageId) {
     return this;
   }
