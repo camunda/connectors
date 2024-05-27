2d6267994a29430caee56e10534015df26a6e7ea
Jonathan Roques <jonathan.roques@camunda.com>
2024-05-27 11:21:38 +0200

    chore(rest): Remove toString

--- connector-sdk/core/src/main/java/io/camunda/connector/api/error/ConnectorException.java
+++ connector-sdk/core/src/main/java/io/camunda/connector/api/error/ConnectorException.java
@@ -110,16 +110,4 @@ public class ConnectorException extends RuntimeException {
   public Map<String, Object> getErrorVariables() {
     return errorVariables;
   }
-
-  @Override
-  public String toString() {
-    return "ConnectorException{"
-        + "errorCode='"
-        + errorCode
-        + '\''
-        + ", errorVariables="
-        + errorVariables
-        + '}'
-        + (getCause() != null ? ", cause=" + getCause().getMessage() : "");
-  }
 }
