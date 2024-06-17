6fcd8fe1c00391fb4ea8ae65743ab44a0ff0ef9d
Simon <sbuettner@users.noreply.github.com>
2024-06-17 16:41:50 +0200

    fix(sagemaker): Parse response.body as as json based on response content type (#2757)

--- connectors/aws/aws-sagemaker/src/main/java/io/camunda/connector/sagemaker/model/SageMakerSyncResponse.java
+++ connectors/aws/aws-sagemaker/src/main/java/io/camunda/connector/sagemaker/model/SageMakerSyncResponse.java
@@ -7,16 +7,37 @@
 package io.camunda.connector.sagemaker.model;
 
 import com.amazonaws.services.sagemakerruntime.model.InvokeEndpointResult;
+import io.camunda.connector.aws.ObjectMapperSupplier;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 public record SageMakerSyncResponse(
-    String body, String contentType, String customAttributes, String invokedProductionVariant) {
+    Object body, String contentType, String customAttributes, String invokedProductionVariant) {
 
   public SageMakerSyncResponse(InvokeEndpointResult result) {
     this(
-        StandardCharsets.UTF_8.decode(result.getBody()).toString(),
+        mapResponseBody(result),
         result.getContentType(),
         result.getCustomAttributes(),
         result.getInvokedProductionVariant());
+  }
+
+  /* Maps the response body to the appropriate object type
+  / https://docs.aws.amazon.com/sagemaker/latest/dg/clarify-online-explainability-invoke-endpoint.html#clarify-online-explainability-response
+  */
+  private static Object mapResponseBody(InvokeEndpointResult result) {
+    if (result.getContentType().equals("application/json")) {
+      return parseJsonResponseBody(result.getBody());
+    }
+    return StandardCharsets.UTF_8.decode(result.getBody()).toString();
+  }
+
+  private static Object parseJsonResponseBody(ByteBuffer json) {
+    try {
+      return ObjectMapperSupplier.getMapperInstance().readValue(json.array(), Object.class);
+    } catch (IOException e) {
+      throw new RuntimeException("Error reading Sagemaker response.", e);
+    }
   }
 }
