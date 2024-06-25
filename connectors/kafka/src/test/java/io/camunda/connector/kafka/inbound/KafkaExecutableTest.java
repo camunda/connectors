16c4f6cd7e762c814af7a4626b39e73c3a1eb523
Pavel Kotelevsky <pavel.kotelevskii@camunda.com>
2024-06-25 14:38:55 +0200

    lint

--- connectors/kafka/src/test/java/io/camunda/connector/kafka/inbound/KafkaExecutableTest.java
+++ connectors/kafka/src/test/java/io/camunda/connector/kafka/inbound/KafkaExecutableTest.java
@@ -273,7 +273,10 @@ public class KafkaExecutableTest {
   public KafkaExecutable getConsumerMock() {
     return new KafkaExecutable(
         properties -> mockConsumer,
-        RetryConfig.custom().waitDuration(Duration.ofMillis(500)).maxAttempts(MAX_ATTEMPTS).build());
+        RetryConfig.custom()
+            .waitDuration(Duration.ofMillis(500))
+            .maxAttempts(MAX_ATTEMPTS)
+            .build());
   }
 
   @ParameterizedTest
