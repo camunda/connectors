437bf52b6282a454d8ce20bb72c5dd7167dad826
Mathias Vandaele <mathias.vandaele@camunda.com>
2024-07-04 17:48:34 +0200

    fix(authentication): fix spring security config (add comments and change order)

--- bundle/camunda-saas-bundle/src/main/java/io/camunda/connector/runtime/saas/security/SecurityConfiguration.java
+++ bundle/camunda-saas-bundle/src/main/java/io/camunda/connector/runtime/saas/security/SecurityConfiguration.java
@@ -42,17 +42,14 @@ public class SecurityConfiguration {
   @Value("${camunda.connector.auth.issuer}")
   private String issuer;
 
-  @Bean
-  @Order(1)
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.ignoringRequestMatchers("/inbound/**"))
-        .authorizeHttpRequests(
-            auth ->
-                auth.requestMatchers("/inbound", "/tenants/**").hasAuthority("SCOPE_inbound:read"))
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
-    return http.build();
-  }
-
+  /**
+   * This is the first (spring priority order) filter chain.
+   * This is going to be applied first, if nothing is matched, then the second one will be applied.
+   * Here, alls of the public endpoint are caught first.
+   * @param http
+   * @return The first security filter chain
+   * @throws Exception
+   */
   @Bean
   @Order(0)
   public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
@@ -67,6 +64,27 @@ public class SecurityConfiguration {
                     .requestMatchers("actuator/**"))
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
         .build();
+  }
+
+  /**
+   * This is the second (spring priority order) filter chain.
+   * This has to be applied in second cause spring-security is secure by default
+   * and therefore will protect every endpoint with Oauth2
+   * If this was first and endpoint like `GET /inbound/*` would respond 401
+   * Those endpoint will be caught on the first security chain
+   * @param http
+   * @return The second security filter chain
+   * @throws Exception
+   */
+  @Bean
+  @Order(1)
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.ignoringRequestMatchers("/inbound/**"))
+        .authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers(HttpMethod.GET, "/inbound", "/tenants/**").hasAuthority("SCOPE_inbound:read"))
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
+    return http.build();
   }
 
   @Bean
