4c653c5e11476716b4f1449471e86011f3a2123e
Jonathan Roques <jonathan.roques@camunda.com>
2024-07-10 15:12:12 +0200

    chore(element-template): Change logs

--- connectors/copy-element-templates.sh
+++ connectors/copy-element-templates.sh
@@ -15,10 +15,11 @@ SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 # Change to the script's directory
 cd "$SCRIPT_DIR" || exit
 
+echo -e "${BLUE}Starting to copy element templates to ~/Library/Application\ Support/camunda-modeler/resources/element-templates${NC}"
 # Find all .json files in element-templates directories and copy them to /tmp/json
 find . -type d -name "element-templates" | while read -r dir; do
   find "$dir" -type f -name "*.json" -exec cp {} ~/Library/Application\ Support/camunda-modeler/resources/element-templates/ \;
-  echo -e "${YELLOW}Copied ETs from [$dir] => ~/Library/Application\ Support/camunda-modeler/resources/element-templates${NC}"
+  echo -e "${YELLOW}Copied ETs from [$dir]${NC}"
 done
 
 echo -e "${GREEN}All element templates have been copied to ~/Library/Application\ Support/camunda-modeler/resources/element-templates${NC}"
