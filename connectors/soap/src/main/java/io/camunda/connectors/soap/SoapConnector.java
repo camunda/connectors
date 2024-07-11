5ba57a0a101f5837ac97d340053df08542935c4b
Jonathan <jonathan.roques@camunda.com>
2024-07-11 16:30:02 +0200

    chore(soap): Send response body when a Fault is received (#2835)

--- connectors/soap/src/main/java/io/camunda/connectors/soap/SoapConnector.java
+++ connectors/soap/src/main/java/io/camunda/connectors/soap/SoapConnector.java
@@ -8,6 +8,7 @@ package io.camunda.connectors.soap;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import io.camunda.connector.api.annotation.OutboundConnector;
+import io.camunda.connector.api.error.ConnectorExceptionBuilder;
 import io.camunda.connector.api.outbound.OutboundConnectorContext;
 import io.camunda.connector.api.outbound.OutboundConnectorFunction;
 import io.camunda.connector.generator.java.annotation.ElementTemplate;
@@ -17,6 +18,7 @@ import io.camunda.connectors.soap.client.SpringSoapClient;
 import io.camunda.connectors.soap.message.SoapMessageHandler;
 import io.camunda.connectors.soap.xml.Mapper;
 import io.camunda.connectors.soap.xml.XmlUtilities;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ws.FaultAwareWebServiceMessage;
@@ -69,14 +71,6 @@ public class SoapConnector implements OutboundConnectorFunction {
     return context.bindVariables(SoapConnectorInput.class);
   }
 
-  protected Object handleResult(JsonNode response) {
-    return response;
-  }
-
-  protected Exception handleException(SoapConnectorException e) {
-    return e;
-  }
-
   @Override
   public Object execute(OutboundConnectorContext context) throws Exception {
     SoapConnectorInput input = getInput(context);
@@ -98,19 +92,23 @@ public class SoapConnector implements OutboundConnectorFunction {
               .build()
               .toJson(XmlUtilities.xmlStringToDocument(soapResponseMessage));
       LOG.debug("Response to connector runtime: \n{}", response.toPrettyString());
-      return handleResult(response);
+      return response;
     } catch (WebServiceException e) {
       if (e instanceof SoapFaultClientException soapFaultClientException) {
         FaultAwareWebServiceMessage webServiceMessage =
             soapFaultClientException.getWebServiceMessage();
         if (webServiceMessage instanceof SoapMessage soapMessage) {
-          throw handleException(
-              new SoapConnectorException(
-                  e,
-                  Mapper.toJson()
-                      .withPreserveNamespaces(false)
-                      .build()
-                      .toJson(soapMessage.getDocument())));
+          throw new ConnectorExceptionBuilder()
+              .message("SOAP Fault received")
+              .errorCode("SOAP_FAULT_RECEIVED")
+              .errorVariables(
+                  Map.of(
+                      "response",
+                      Mapper.toJson()
+                          .withPreserveNamespaces(false)
+                          .build()
+                          .toJson(soapMessage.getDocument())))
+              .build();
         }
       }
       throw e;
