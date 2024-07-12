d8bc074c16684d66e2c0f4482c9b3548319d8fe5
Mathias Vandaele <mathias.vandaele@camunda.com>
2024-07-12 15:28:52 +0200

    fix(web-hook) implement what has been discussed during the architecture session

--- connectors/webhook/src/main/java/io/camunda/connector/inbound/signature/strategy/URLAndBodyEncodingStrategy.java
+++ connectors/webhook/src/main/java/io/camunda/connector/inbound/signature/strategy/URLAndBodyEncodingStrategy.java
@@ -6,45 +6,72 @@
  */
 package io.camunda.connector.inbound.signature.strategy;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.net.MediaType;
 import io.camunda.connector.api.inbound.webhook.WebhookProcessingPayload;
+import io.camunda.connector.api.json.ConnectorsObjectMapperSupplier;
 import io.camunda.connector.inbound.utils.HttpWebhookUtil;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public final class URLAndBodyEncodingStrategy implements HMACEncodingStrategy {
 
   private static String extractSignatureData(final WebhookProcessingPayload payload) {
-    if (payload.rawBody() == null || payload.rawBody().length == 0) {
-      throw new NullPointerException(
-          "Can't extract signature data from body, because body is null");
-    }
-
-    Object rawBody =
-        HttpWebhookUtil.transformRawBodyToObject(
-            payload.rawBody(), HttpWebhookUtil.extractContentType(payload.headers()));
-
-    List<String> sortedKeys = convertToListOfString(rawBody);
-    StringBuilder builder = new StringBuilder();
-    sortedKeys.forEach(builder::append);
-    return builder.toString();
+    return prepareForSignature(
+        payload.rawBody(), HttpWebhookUtil.extractContentType(payload.headers()));
   }
 
-  private static List<String> convertToListOfString(Object o) {
-    Map<String, String> result = new HashMap<>();
+  private static String prepareForSignature(byte[] rawBody, String contentTypeHeader) {
+    if (MediaType.FORM_DATA.toString().equalsIgnoreCase(contentTypeHeader)) {
+      String bodyAsString =
+          URLDecoder.decode(new String(rawBody, StandardCharsets.UTF_8), StandardCharsets.UTF_8);
+      Map<String, String> strMap =
+          Arrays.stream(bodyAsString.split("&"))
+              .filter(Objects::nonNull)
+              .map(param -> param.split("="))
+              .collect(
+                  Collectors.toMap(param -> param[0], param -> param.length == 1 ? "" : param[1]));
+      return strMap.keySet().stream()
+          .sorted()
+          .map(key -> key.concat(strMap.get(key)))
+          .reduce(String::concat)
+          .orElse("");
+    }
+    Object o = mapBytesToObject(rawBody);
     if (o instanceof Map<?, ?> map) {
       for (Map.Entry<?, ?> entry : map.entrySet()) {
-        if (entry.getKey() instanceof String key) {
-          result.put(key, entry.getValue() == null ? "" : (String) entry.getValue());
+        if (entry.getKey() instanceof String) {
+          Map<String, String> strMap = (Map<String, String>) map;
+          return strMap.keySet().stream()
+              .sorted()
+              .map(key -> key.concat(strMap.get(key)))
+              .reduce(String::concat)
+              .orElse("");
         }
       }
-    } else if (o instanceof List<?> list) {
-      for (int i = 0; i < list.size(); i = i + 2) {
-        if (list.get(i) instanceof String key && i + 1 < list.size()) {
-          result.put(key, list.get(i + 1) == null ? "" : (String) list.get(i + 1));
-        }
-      }
+    } else {
+      return mapObjectToString(o);
     }
-    return result.keySet().stream().sorted().flatMap(s -> Stream.of(s, result.get(s))).toList();
+    throw new RuntimeException("Can't extract signature data from body");
+  }
+
+  private static String mapObjectToString(Object o) {
+    try {
+      return ConnectorsObjectMapperSupplier.getCopy().writeValueAsString(o);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static Object mapBytesToObject(byte[] rawBody) {
+    try {
+      return ConnectorsObjectMapperSupplier.getCopy().readValue(rawBody, Object.class);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
