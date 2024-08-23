b2b98a8a88f5be2c8e1b2d7f3ad6a8bcd48af409
Igor Petrov <108870003+igpetrov@users.noreply.github.com>
2024-08-23 16:58:26 +0300

    feat(textract): introduce Amazon textract connector (#2928)
    Co-authored-by: DenovVasil <lasrssonmanchini@gmail.com>

---
+++ connectors/aws/aws-textract/src/main/java/io/camunda/connector/textract/model/TextractRequestData.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+package io.camunda.connector.textract.model;
+
+import io.camunda.connector.generator.dsl.Property.FeelMode;
+import io.camunda.connector.generator.java.annotation.TemplateProperty;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.apache.commons.lang3.StringUtils;
+
+public record TextractRequestData(
+    @TemplateProperty(
+            label = "Execution type",
+            group = "input",
+            type = TemplateProperty.PropertyType.Dropdown,
+            defaultValue = "ASYNC",
+            feel = FeelMode.disabled,
+            choices = {
+              @TemplateProperty.DropdownPropertyChoice(value = "ASYNC", label = "Asynchronous"),
+              @TemplateProperty.DropdownPropertyChoice(value = "SYNC", label = "Real-time"),
+              @TemplateProperty.DropdownPropertyChoice(value = "POLLING", label = "Polling")
+            },
+            description = "Endpoint inference type")
+        @NotNull
+        TextractExecutionType executionType,
+    @TemplateProperty(
+            group = "input",
+            label = "Document bucket",
+            description = "S3 bucket that contains document that needs to be processed")
+        @NotBlank
+        String documentS3Bucket,
+    @TemplateProperty(
+            group = "input",
+            label = "Document path",
+            description = "S3 document path to be processed")
+        @NotBlank
+        String documentName,
+    @TemplateProperty(
+            group = "input",
+            label = "Document version",
+            description = "S3 document version to be processed",
+            optional = true)
+        String documentVersion,
+    @TemplateProperty(
+            label = "Analyze tables",
+            group = "input",
+            type = TemplateProperty.PropertyType.Boolean,
+            defaultValueType = TemplateProperty.DefaultValueType.Boolean,
+            defaultValue = "true")
+        @NotNull
+        boolean analyzeTables,
+    @TemplateProperty(
+            label = "Analyze form",
+            group = "input",
+            type = TemplateProperty.PropertyType.Boolean,
+            defaultValueType = TemplateProperty.DefaultValueType.Boolean,
+            defaultValue = "true")
+        @NotNull
+        boolean analyzeForms,
+    @TemplateProperty(
+            label = "Analyze signatures",
+            group = "input",
+            type = TemplateProperty.PropertyType.Boolean,
+            defaultValueType = TemplateProperty.DefaultValueType.Boolean,
+            defaultValue = "true")
+        @NotNull
+        boolean analyzeSignatures,
+    @TemplateProperty(
+            label = "Analyze layout",
+            group = "input",
+            type = TemplateProperty.PropertyType.Boolean,
+            defaultValueType = TemplateProperty.DefaultValueType.Boolean,
+            defaultValue = "true")
+        @NotNull
+        boolean analyzeLayout,
+    @TemplateProperty(
+            group = "input",
+            label = "Client request token",
+            description = "The idempotent token that you use to identify the start request",
+            optional = true,
+            condition =
+                @TemplateProperty.PropertyCondition(
+                    property = "input.executionType",
+                    equals = "ASYNC"))
+        String clientRequestToken,
+    @TemplateProperty(
+            group = "input",
+            label = "Job tag",
+            description =
+                "An identifier that you specify that's included in the completion notification published to the Amazon SNS topic",
+            optional = true,
+            condition =
+                @TemplateProperty.PropertyCondition(
+                    property = "input.executionType",
+                    equals = "ASYNC"))
+        String jobTag,
+    @TemplateProperty(
+            group = "input",
+            label = "KMS key ID",
+            description = "The KMS key used to encrypt the inference results",
+            optional = true,
+            condition =
+                @TemplateProperty.PropertyCondition(
+                    property = "input.executionType",
+                    equals = "ASYNC"))
+        String kmsKeyId,
+    @TemplateProperty(
+            group = "input",
+            label = "Notification channel role ARN",
+            description =
+                "The Amazon SNS topic role ARN that you want Amazon Textract to publish the completion status of the operation to",
+            optional = true,
+            condition =
+                @TemplateProperty.PropertyCondition(
+                    property = "input.executionType",
+                    equals = "ASYNC"))
+        String notificationChannelRoleArn,
+    @TemplateProperty(
+            group = "input",
+            label = "Notification channel SNS topic ARN",
+            description =
+                "The Amazon SNS topic ARN that you want Amazon Textract to publish the completion status of the operation to",
+            optional = true,
+            condition =
+                @TemplateProperty.PropertyCondition(
+                    property = "input.executionType",
+                    equals = "ASYNC"))
+        String notificationChannelSnsTopicArn,
+    @TemplateProperty(
+            group = "input",
+            label = "Output S3 bucket",
+            description = "The name of the bucket your output will go to",
+            optional = true,
+            condition =
+                @TemplateProperty.PropertyCondition(
+                    property = "input.executionType",
+                    equals = "ASYNC"))
+        String outputConfigS3Bucket,
+    @TemplateProperty(
+            group = "input",
+            label = "Output S3 prefix",
+            description = "The prefix of the object key that the output will be saved to",
+            optional = true,
+            condition =
+                @TemplateProperty.PropertyCondition(
+                    property = "input.executionType",
+                    equals = "ASYNC"))
+        String outputConfigS3Prefix) {
+  public static final String WRONG_OUTPUT_VALUES_MSG =
+      "Output S3 bucket must be filled in if output S3 prefix is filled in";
+
+  public static final String WRONG_NOTIFICATION_VALUES_MSG =
+      "either both notification values role ARN and topic ARN must be filled in or none of them";
+
+  @AssertTrue(message = WRONG_NOTIFICATION_VALUES_MSG)
+  public boolean isValidNotificationProperties() {
+    if (executionType != TextractExecutionType.ASYNC) {
+      return true;
+    }
+    return StringUtils.isNoneBlank(notificationChannelRoleArn, notificationChannelSnsTopicArn)
+        || StringUtils.isAllBlank(notificationChannelRoleArn, notificationChannelSnsTopicArn);
+  }
+
+  @AssertTrue(message = WRONG_OUTPUT_VALUES_MSG)
+  public boolean isValidOutputConfigProperties() {
+    if (executionType != TextractExecutionType.ASYNC) {
+      return true;
+    }
+
+    if (StringUtils.isAllBlank(outputConfigS3Bucket, outputConfigS3Prefix)) {
+      return true;
+    }
+
+    if (StringUtils.isNoneBlank(outputConfigS3Bucket, outputConfigS3Prefix)) {
+      return true;
+    }
+    return !StringUtils.isBlank(outputConfigS3Bucket);
+  }
+}
---
+++ connectors/aws/aws-textract/src/test/java/io/camunda/connector/textract/caller/AsyncTextractCallerTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+package io.camunda.connector.textract.caller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.amazonaws.services.textract.AmazonTextractAsyncClient;
+import com.amazonaws.services.textract.model.StartDocumentAnalysisRequest;
+import com.amazonaws.services.textract.model.StartDocumentAnalysisResult;
+import io.camunda.connector.textract.model.TextractExecutionType;
+import io.camunda.connector.textract.model.TextractRequestData;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AsyncTextractCallerTest {
+
+  @Captor private ArgumentCaptor<StartDocumentAnalysisRequest> requestArgumentCaptor;
+
+  @Test
+  void callWithAllFields() {
+    TextractRequestData requestData = prepareReqData("roleArn", "topicArna");
+
+    AmazonTextractAsyncClient asyncClient = Mockito.mock(AmazonTextractAsyncClient.class);
+    when(asyncClient.startDocumentAnalysis(any(StartDocumentAnalysisRequest.class)))
+        .thenReturn(new StartDocumentAnalysisResult());
+
+    new AsyncTextractCaller().call(requestData, asyncClient);
+
+    verify(asyncClient).startDocumentAnalysis(requestArgumentCaptor.capture());
+
+    final StartDocumentAnalysisRequest startDocumentAnalysisRequest =
+        requestArgumentCaptor.getValue();
+
+    assertThat(startDocumentAnalysisRequest.getFeatureTypes().size()).isEqualTo(4);
+    assertThat(startDocumentAnalysisRequest.getDocumentLocation()).isNotNull();
+    assertThat(startDocumentAnalysisRequest.getClientRequestToken())
+        .isEqualTo(requestData.clientRequestToken());
+    assertThat(startDocumentAnalysisRequest.getJobTag()).isEqualTo(requestData.jobTag());
+    assertThat(startDocumentAnalysisRequest.getKMSKeyId()).isEqualTo(requestData.kmsKeyId());
+    assertThat(startDocumentAnalysisRequest.getNotificationChannel()).isNotNull();
+    assertThat(startDocumentAnalysisRequest.getNotificationChannel().getRoleArn())
+        .isEqualTo(requestData.notificationChannelRoleArn());
+    assertThat(startDocumentAnalysisRequest.getNotificationChannel().getSNSTopicArn())
+        .isEqualTo(requestData.notificationChannelSnsTopicArn());
+    assertThat(startDocumentAnalysisRequest.getOutputConfig()).isNotNull();
+    assertThat(startDocumentAnalysisRequest.getOutputConfig().getS3Bucket())
+        .isEqualTo(requestData.outputConfigS3Bucket());
+    assertThat(startDocumentAnalysisRequest.getOutputConfig().getS3Prefix())
+        .isEqualTo(requestData.outputConfigS3Prefix());
+  }
+
+  @Test
+  void callWithoutNotificationChanelFieldsShouldNotCreateNotificationObj() {
+    TextractRequestData requestData = prepareReqData("", "");
+
+    AmazonTextractAsyncClient asyncClient = Mockito.mock(AmazonTextractAsyncClient.class);
+    when(asyncClient.startDocumentAnalysis(any(StartDocumentAnalysisRequest.class)))
+        .thenReturn(new StartDocumentAnalysisResult());
+
+    new AsyncTextractCaller().call(requestData, asyncClient);
+
+    verify(asyncClient).startDocumentAnalysis(requestArgumentCaptor.capture());
+
+    final StartDocumentAnalysisRequest startDocumentAnalysisRequest =
+        requestArgumentCaptor.getValue();
+
+    assertThat(startDocumentAnalysisRequest.getNotificationChannel()).isNull();
+  }
+
+  @Test
+  void callWithoutOutputS3BucketShouldNotCreateOutputObj() {
+    TextractRequestData requestData = prepareReqDataWithoutOutputS3Bucket();
+
+    AmazonTextractAsyncClient asyncClient = Mockito.mock(AmazonTextractAsyncClient.class);
+    when(asyncClient.startDocumentAnalysis(any(StartDocumentAnalysisRequest.class)))
+        .thenReturn(new StartDocumentAnalysisResult());
+
+    new AsyncTextractCaller().call(requestData, asyncClient);
+
+    verify(asyncClient).startDocumentAnalysis(requestArgumentCaptor.capture());
+
+    final StartDocumentAnalysisRequest startDocumentAnalysisRequest =
+        requestArgumentCaptor.getValue();
+
+    assertThat(startDocumentAnalysisRequest.getOutputConfig()).isNull();
+  }
+
+  private TextractRequestData prepareReqData(String roleArn, String topicArn) {
+    return new TextractRequestData(
+        TextractExecutionType.ASYNC,
+        "test-bucket",
+        "test-object",
+        "1",
+        true,
+        true,
+        true,
+        true,
+        "token",
+        "jobTag",
+        "kmsId",
+        roleArn,
+        topicArn,
+        "outputBucket",
+        "prefix");
+  }
+
+  private TextractRequestData prepareReqDataWithoutOutputS3Bucket() {
+    return new TextractRequestData(
+        TextractExecutionType.ASYNC,
+        "test-bucket",
+        "test-object",
+        "1",
+        true,
+        true,
+        true,
+        true,
+        "token",
+        "jobTag",
+        "kmsId",
+        "roleArn",
+        "topicArn",
+        "",
+        "prefix");
+  }
+}
