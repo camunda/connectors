631740b0cb6687425c9580e1a7de2a30bfc1743c
Ev Bilske <ev.bilske@camunda.com>
2024-09-12 08:55:24 +0200

    test(zeebe): add @ZeebeSpringTest
    Otherwise, server does not always shut down correctly

--- bundle/camunda-saas-bundle/src/test/java/io/camunda/connector/runtime/saas/SecurityConfigurationTest.java
+++ bundle/camunda-saas-bundle/src/test/java/io/camunda/connector/runtime/saas/SecurityConfigurationTest.java
@@ -28,6 +28,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import io.camunda.operate.CamundaOperateClient;
 import java.time.Duration;
+
+import io.camunda.zeebe.spring.test.ZeebeSpringTest;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -60,6 +63,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @DirtiesContext
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+@ZeebeSpringTest
 public class SecurityConfigurationTest {
 
   @Autowired private MockMvc mvc;
