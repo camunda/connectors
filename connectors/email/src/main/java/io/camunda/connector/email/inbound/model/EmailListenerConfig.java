42954e9a6f54049231e9f947ccc7255b95bfcc19
Mathias Vandaele <mathias.vandaele@camunda.com>
2024-09-16 10:07:57 +0200

    feat(email connector): inbound inbound refactor and labels refinment 2

--- connectors/email/src/main/java/io/camunda/connector/email/inbound/model/EmailListenerConfig.java
+++ connectors/email/src/main/java/io/camunda/connector/email/inbound/model/EmailListenerConfig.java
@@ -19,14 +19,15 @@ public record EmailListenerConfig(
             label = "Folder to listen",
             group = "listenerInfos",
             id = "data.folderToListen",
-            description = "",
+            tooltip =
+                "Enter the names of the folders you wish to monitor, separated by commas, for new emails or changes (e.g., 'INBOX, Sent, Archive'). If left blank, the listener will default to monitoring the 'INBOX' folder.",
             optional = true,
             feel = Property.FeelMode.optional,
             binding = @TemplateProperty.PropertyBinding(name = "data.folderToListen"))
         Object folderToListen,
     @TemplateProperty(
             label = "Sync strategy",
-            description = "Chose the desired polling strategy",
+            tooltip = "Chose the desired polling strategy",
             group = "listenerInfos",
             id = "data.initialPollingConfig",
             feel = Property.FeelMode.required,
@@ -34,18 +35,22 @@ public record EmailListenerConfig(
             constraints = @TemplateProperty.PropertyConstraints(notEmpty = true),
             defaultValue = "UNSEEN",
             choices = {
-              @TemplateProperty.DropdownPropertyChoice(label = "Unseen", value = "UNSEEN"),
               @TemplateProperty.DropdownPropertyChoice(
-                  label = "No initial sync. Only new mails",
+                  label = "Unseen emails will be sync",
+                  value = "UNSEEN"),
+              @TemplateProperty.DropdownPropertyChoice(
+                  label = "No initial sync. Only new emails",
                   value = "NONE"),
-              @TemplateProperty.DropdownPropertyChoice(label = "All", value = "ALL")
+              @TemplateProperty.DropdownPropertyChoice(
+                  label = "All emails will be sync",
+                  value = "ALL")
             },
             binding = @TemplateProperty.PropertyBinding(name = "data.initialPollingConfig"))
         @NotNull
         InitialPollingConfig initialPollingConfig,
     @TemplateProperty(
             label = "Handling strategy",
-            description = "Chose the desired handling strategy",
+            tooltip = "Chose the desired handling strategy",
             group = "listenerInfos",
             id = "data.handlingStrategy",
             feel = Property.FeelMode.required,
@@ -53,17 +58,24 @@ public record EmailListenerConfig(
             constraints = @TemplateProperty.PropertyConstraints(notEmpty = true),
             defaultValue = "READ",
             choices = {
-              @TemplateProperty.DropdownPropertyChoice(label = "Read", value = "READ"),
-              @TemplateProperty.DropdownPropertyChoice(label = "None", value = "NO_HANDLING"),
-              @TemplateProperty.DropdownPropertyChoice(label = "Delete", value = "DELETE"),
-              @TemplateProperty.DropdownPropertyChoice(label = "Move", value = "MOVE")
+              @TemplateProperty.DropdownPropertyChoice(
+                  label = "Mark as read after processing",
+                  value = "READ"),
+              @TemplateProperty.DropdownPropertyChoice(label = "Do nothing", value = "NO_HANDLING"),
+              @TemplateProperty.DropdownPropertyChoice(
+                  label = "Delete after processing",
+                  value = "DELETE"),
+              @TemplateProperty.DropdownPropertyChoice(
+                  label = "Move to another folder after processing",
+                  value = "MOVE")
             },
             binding = @TemplateProperty.PropertyBinding(name = "data.handlingStrategy"))
         @NotNull
         HandlingStrategy handlingStrategy,
     @TemplateProperty(
             label = "Choose the target folder",
-            description = "Chose the target folder",
+            tooltip =
+                "Specify the destination folder to which the emails will be moved. To create a new folder or a hierarchy of folders, use a dot-separated path (e.g., 'Archive' or 'Projects.2023.January'). If any part of the path does not exist, it will be created automatically.",
             group = "listenerInfos",
             id = "data.targetFolder",
             binding = @TemplateProperty.PropertyBinding(name = "data.targetFolder"),
