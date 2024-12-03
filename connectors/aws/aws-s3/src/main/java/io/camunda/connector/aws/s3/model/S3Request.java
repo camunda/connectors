7a5253922874d674e4fedc0196a74d76d0073085
Mathias Vandaele <mathias.vandaele@camunda.com>
2024-12-03 09:39:43 +0100

    feature(s3-connector): checkpoint s3 connector

--- connectors/aws/aws-s3/src/main/java/io/camunda/connector/aws/s3/model/S3Request.java
+++ connectors/aws/aws-s3/src/main/java/io/camunda/connector/aws/s3/model/S3Request.java
@@ -1,5 +1,26 @@
 package io.camunda.connector.aws.s3.model;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.camunda.connector.aws.model.impl.AwsBaseRequest;
+import io.camunda.connector.generator.java.annotation.NestedProperties;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
-public class S3Request extends AwsBaseRequest {}
+public class S3Request extends AwsBaseRequest {
+
+  @JsonTypeInfo(
+      use = JsonTypeInfo.Id.NAME,
+      include = JsonTypeInfo.As.EXTERNAL_PROPERTY,
+      property = "action")
+  @JsonSubTypes(
+      value = {
+        @JsonSubTypes.Type(value = DeleteS3Action.class, name = "deleteObject"),
+        @JsonSubTypes.Type(value = UploadS3Action.class, name = "uploadObject"),
+        @JsonSubTypes.Type(value = DownloadS3Action.class, name = "downloadObject"),
+      })
+  @Valid
+  @NotNull
+  @NestedProperties(addNestedPath = false)
+  private S3Action data;
+}
