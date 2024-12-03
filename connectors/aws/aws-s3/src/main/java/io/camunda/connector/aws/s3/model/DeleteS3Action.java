5f2285f19701858983467026bae59b31156b3c56
Mathias Vandaele <mathias.vandaele@camunda.com>
2024-12-03 10:50:30 +0100

    feature(s3-connector): checkpoint s3 connector

--- connectors/aws/aws-s3/src/main/java/io/camunda/connector/aws/s3/model/DeleteS3Action.java
+++ connectors/aws/aws-s3/src/main/java/io/camunda/connector/aws/s3/model/DeleteS3Action.java
@@ -1,3 +1,35 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
 package io.camunda.connector.aws.s3.model;
 
-public final class DeleteS3Action implements S3Action {}
+import io.camunda.connector.generator.dsl.Property;
+import io.camunda.connector.generator.java.annotation.TemplateProperty;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+public record DeleteS3Action(
+    @TemplateProperty(
+            label = "AWS bucket",
+            id = "deleteActionBucket",
+            group = "deleteObject",
+            tooltip = "Bucket from where an object should be deleted",
+            feel = Property.FeelMode.optional,
+            binding = @TemplateProperty.PropertyBinding(name = "action.bucket"))
+        @Valid
+        @NotNull
+        String bucket,
+    @TemplateProperty(
+            label = "AWS key",
+            id = "deleteActionKey",
+            group = "deleteObject",
+            tooltip = "Key of the object which should be deleted",
+            feel = Property.FeelMode.optional,
+            binding = @TemplateProperty.PropertyBinding(name = "action.key"))
+        @Valid
+        @NotNull
+        String key)
+    implements S3Action {}
