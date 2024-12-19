9b775d18f4cabb00bc0957ca0b0e9fee1502ab38
Mathias Vandaele <mathias.vandaele@camunda.com>
2024-12-19 18:06:07 +0100

    feature(aws-s3-connector): AWS s3 connector (#3744)
    * feature(s3-connector): initial commit
    
    * feature(s3-connector): checkpoint s3 connector
    
    * feature(s3-connector): checkpoint s3 connector
    
    * feature(s3-connector): checkpoint s3 connector
    
    * feature(s3-connector): checkpoint s3 connector 3
    
    * feature(s3-connector): checkpoint s3 connector; pom commit
    
    * feature(s3-connector): checkpoint s3 connector; pom commit
    
    * feature(s3-connector): all features done
    
    * feature(s3-connector): correcting after Jonathan comments
    
    * feature(s3-connector): minor corrections
    
    * feature(s3-connector): minor corrections before rebase
    
    * feature(s3-connector): corrections
    
    * feature(s3-connector): corrections 2

---
+++ connectors/aws/aws-s3/src/main/java/io/camunda/connector/aws/s3/model/request/S3Request.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+package io.camunda.connector.aws.s3.model.request;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.camunda.connector.aws.model.impl.AwsBaseRequest;
+import io.camunda.connector.generator.java.annotation.NestedProperties;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+public class S3Request extends AwsBaseRequest {
+
+  @JsonTypeInfo(
+      use = JsonTypeInfo.Id.NAME,
+      include = JsonTypeInfo.As.EXTERNAL_PROPERTY,
+      property = "actionDiscriminator")
+  @JsonSubTypes(
+      value = {
+        @JsonSubTypes.Type(value = DeleteObject.class, name = "deleteObject"),
+        @JsonSubTypes.Type(value = UploadObject.class, name = "uploadObject"),
+        @JsonSubTypes.Type(value = DownloadObject.class, name = "downloadObject"),
+      })
+  @Valid
+  @NotNull
+  @NestedProperties(addNestedPath = false)
+  private S3Action action;
+
+  public S3Request() {}
+
+  public S3Action getAction() {
+    return action;
+  }
+
+  public void setAction(S3Action action) {
+    this.action = action;
+  }
+}
---
+++ connectors/aws/aws-s3/src/main/java/io/camunda/connector/aws/s3/model/request/UploadObject.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+package io.camunda.connector.aws.s3.model.request;
+
+import io.camunda.connector.generator.dsl.Property;
+import io.camunda.connector.generator.java.annotation.TemplateProperty;
+import io.camunda.connector.generator.java.annotation.TemplateSubType;
+import io.camunda.document.Document;
+import jakarta.validation.constraints.NotBlank;
+
+@TemplateSubType(id = "uploadObject", label = "Upload object")
+public record UploadObject(
+    @TemplateProperty(
+            label = "AWS bucket",
+            id = "uploadActionBucket",
+            group = "uploadObject",
+            tooltip = "Bucket from where an object should be uploaded",
+            feel = Property.FeelMode.optional,
+            binding = @TemplateProperty.PropertyBinding(name = "action.bucket"))
+        @NotBlank
+        String bucket,
+    @TemplateProperty(
+            label = "AWS key",
+            id = "uploadActionKey",
+            group = "uploadObject",
+            tooltip =
+                "Key of the uploaded object, if not given. The file name from the document metadata will be used",
+            optional = true,
+            feel = Property.FeelMode.optional,
+            binding = @TemplateProperty.PropertyBinding(name = "action.key"))
+        String key,
+    @TemplateProperty(
+            label = "Document",
+            group = "uploadObject",
+            id = "uploadActionDocument",
+            tooltip = "Document to be uploaded on AWS S3",
+            type = TemplateProperty.PropertyType.String,
+            feel = Property.FeelMode.required,
+            binding = @TemplateProperty.PropertyBinding(name = "action.document"))
+        Document document)
+    implements S3Action {}
