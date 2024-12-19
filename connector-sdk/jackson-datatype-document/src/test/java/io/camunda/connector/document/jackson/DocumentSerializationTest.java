a419070146b5edb6f8a6dec496e5c9a9277e3521
Mathias Vandaele <mathias.vandaele@camunda.com>
2024-12-19 14:54:42 +0100

    refactor(document-handling): split document jackson module into a deserializer module and serializer module

--- connector-sdk/jackson-datatype-document/src/test/java/io/camunda/connector/document/jackson/DocumentSerializationTest.java
+++ connector-sdk/jackson-datatype-document/src/test/java/io/camunda/connector/document/jackson/DocumentSerializationTest.java
@@ -24,7 +24,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import io.camunda.connector.document.annotation.jackson.DocumentReferenceModel.CamundaDocumentMetadataModel;
 import io.camunda.connector.document.annotation.jackson.DocumentReferenceModel.CamundaDocumentReferenceModel;
-import io.camunda.connector.document.annotation.jackson.JacksonModuleDocument;
+import io.camunda.connector.document.annotation.jackson.JacksonModuleDocumentDeserializer;
 import io.camunda.document.Document;
 import io.camunda.document.factory.DocumentFactory;
 import io.camunda.document.operation.DocumentOperationExecutor;
@@ -42,7 +42,7 @@ public class DocumentSerializationTest {
 
   private final ObjectMapper objectMapper =
       new ObjectMapper()
-          .registerModule(new JacksonModuleDocument(factory, operationExecutor))
+          .registerModule(new JacksonModuleDocumentDeserializer(factory, operationExecutor))
           .registerModule(new Jdk8Module());
 
   @Test
