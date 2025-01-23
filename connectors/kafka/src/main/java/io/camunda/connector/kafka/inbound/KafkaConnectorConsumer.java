11c0f51d1b101f220d8ea752acb7bd9c2f8fa3da
Jonathan Roques <jonathan.roques@camunda.com>
2025-01-23 17:09:53 +0100

    fix(kafka): remove global consumer and autoclose it

--- connectors/kafka/src/main/java/io/camunda/connector/kafka/inbound/KafkaConnectorConsumer.java
+++ connectors/kafka/src/main/java/io/camunda/connector/kafka/inbound/KafkaConnectorConsumer.java
@@ -64,7 +64,6 @@ public class KafkaConnectorConsumer {
   private final RetryPolicy<Object> retryPolicy;
   private final Function<Properties, Consumer<Object, Object>> consumerCreatorFunction;
   public CompletableFuture<?> future;
-  Consumer<Object, Object> consumer;
   KafkaConnectorProperties elementProps;
   boolean shouldLoop = true;
   private Health consumerStatus = Health.unknown();
@@ -92,12 +91,12 @@ public class KafkaConnectorConsumer {
 
     CheckedSupplier<Void> retryableFutureSupplier =
         () -> {
-          try {
-            prepareConsumer();
-            consume();
+          try (Consumer consumer = prepareConsumer()) {
+            consume(consumer);
             return null;
           } catch (Exception ex) {
             LOG.error("Consumer loop failure, retry pending: {}", ex.getMessage(), ex);
+<<<<<<< Updated upstream
             try {
               if (consumer != null) {
                 consumer.close();
@@ -108,6 +107,8 @@ public class KafkaConnectorConsumer {
                       + "This error will be ignored. If the consumer is still running, it will be disconnected after max.poll.interval.ms.",
                   e.getMessage());
             }
+=======
+>>>>>>> Stashed changes
             throw ex;
           }
         };
@@ -124,14 +125,16 @@ public class KafkaConnectorConsumer {
                 });
   }
 
-  private void prepareConsumer() {
+  private Consumer<Object, Object> prepareConsumer() {
     try {
-      this.consumer = consumerCreatorFunction.apply(getKafkaProperties(elementProps, context));
+      var consumer = consumerCreatorFunction.apply(getKafkaProperties(elementProps, context));
       String topicName = elementProps.topic().topicName();
       consumer.subscribe(
           List.of(topicName),
           new OffsetUpdateRequiredListener(topicName, consumer, elementProps.offsets()));
-      reportUp();
+      reportUp(consumer);
+
+      return consumer;
     } catch (Exception ex) {
       LOG.error("Failed to initialize connector", ex);
       context.log(
@@ -143,11 +146,11 @@ public class KafkaConnectorConsumer {
     }
   }
 
-  public void consume() {
+  public void consume(Consumer consumer) {
     while (shouldLoop) {
       try {
-        pollAndPublish();
-        reportUp();
+        pollAndPublish(consumer);
+        reportUp(consumer);
       } catch (Exception ex) {
         reportDown(ex);
         throw ex;
@@ -156,14 +159,14 @@ public class KafkaConnectorConsumer {
     LOG.debug("Kafka inbound loop finished");
   }
 
-  private void pollAndPublish() {
-    LOG.trace("Polling the topics: {}", this.consumer.assignment());
-    ConsumerRecords<Object, Object> records = this.consumer.poll(Duration.ofMillis(500));
+  private void pollAndPublish(Consumer consumer) {
+    LOG.trace("Polling the topics: {}", consumer.assignment());
+    ConsumerRecords<Object, Object> records = consumer.poll(Duration.ofMillis(500));
     for (ConsumerRecord<Object, Object> record : records) {
       handleMessage(record);
     }
     if (!records.isEmpty()) {
-      this.consumer.commitSync();
+      consumer.commitSync();
     }
   }
 
@@ -214,7 +217,7 @@ public class KafkaConnectorConsumer {
     }
   }
 
-  private void reportUp() {
+  private void reportUp(Consumer consumer) {
     var details = new HashMap<String, Object>();
     details.put("group-id", consumer.groupMetadata().groupId());
     details.put("group-instance-id", consumer.groupMetadata().groupInstanceId().orElse("unknown"));
