335e51d61e81038bc44e245b815b22fa14f55bad
Pavel Kotelevsky <pavel.kotelevskii@camunda.com>
2025-01-29 16:44:01 +0100

    serialize

--- connectors/javascript/src/main/java/io/camunda/connector/javascript/JavascriptConnectorFunction.java
+++ connectors/javascript/src/main/java/io/camunda/connector/javascript/JavascriptConnectorFunction.java
@@ -6,15 +6,19 @@
  */
 package io.camunda.connector.javascript;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.connector.api.annotation.OutboundConnector;
+import io.camunda.connector.api.json.ConnectorsObjectMapperSupplier;
 import io.camunda.connector.api.outbound.OutboundConnectorContext;
 import io.camunda.connector.api.outbound.OutboundConnectorFunction;
 import io.camunda.connector.generator.java.annotation.ElementTemplate;
+import io.camunda.connector.javascript.model.DenoRequest;
 import io.camunda.connector.javascript.model.JavascriptInputRequest;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Base64;
 
 @OutboundConnector(
     name = "Javascript Connector",
@@ -32,19 +36,21 @@ import java.net.http.HttpResponse;
 public class JavascriptConnectorFunction implements OutboundConnectorFunction {
 
   final static String JS_MAGIC_ENDPOINT = "http://ec2-16-171-176-240.eu-north-1.compute.amazonaws.com:8000";
+  final static ObjectMapper MAPPER = ConnectorsObjectMapperSupplier.getCopy();
 
   @Override
   public Object execute(OutboundConnectorContext context) throws Exception {
     System.out.println("Executing Javascript connector");
     var input = context.bindVariables(JavascriptInputRequest.class);
+
+    final var denoRequest = new DenoRequest(input.script(), input.parameters());
     try (final var client = HttpClient.newHttpClient()) {
-      // Execute the Javascript code
+
       final var request = HttpRequest.newBuilder()
           .uri(URI.create(JS_MAGIC_ENDPOINT))
           .header("Content-Type", "application/json")
-          .POST(HttpRequest.BodyPublishers.ofString(
-              "{\"script\": \"" + input.script() + "\", \"parameters\": " + input.parameters()
-                  + "}"));
+          .POST(HttpRequest.BodyPublishers.ofString(MAPPER.writeValueAsString(denoRequest)));
+
       final var response = client.send(request.build(), HttpResponse.BodyHandlers.ofString());
       return response.body();
     }
---
+++ connectors/javascript/src/main/java/io/camunda/connector/javascript/model/DenoRequest.java
@@ -0,0 +1,3 @@
+package io.camunda.connector.javascript.model;
+
+public record DenoRequest(String script, Object params) { }
