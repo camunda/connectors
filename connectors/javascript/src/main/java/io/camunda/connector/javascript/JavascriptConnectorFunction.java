ab2152b47056af0ce9a7f1fb2a95ff1e8fdfaa30
Pavel Kotelevsky <pavel.kotelevskii@camunda.com>
2025-01-29 16:24:59 +0100

    add js interpreter call

--- connectors/javascript/src/main/java/io/camunda/connector/javascript/JavascriptConnectorFunction.java
+++ connectors/javascript/src/main/java/io/camunda/connector/javascript/JavascriptConnectorFunction.java
@@ -11,6 +11,10 @@ import io.camunda.connector.api.outbound.OutboundConnectorContext;
 import io.camunda.connector.api.outbound.OutboundConnectorFunction;
 import io.camunda.connector.generator.java.annotation.ElementTemplate;
 import io.camunda.connector.javascript.model.JavascriptInputRequest;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @OutboundConnector(
     name = "Javascript Connector",
@@ -26,9 +30,23 @@ import io.camunda.connector.javascript.model.JavascriptInputRequest;
     documentationRef = "",
     icon = "icon.svg")
 public class JavascriptConnectorFunction implements OutboundConnectorFunction {
+
+  final static String JS_MAGIC_ENDPOINT = "http://ec2-16-171-176-240.eu-north-1.compute.amazonaws.com:8000";
+
   @Override
   public Object execute(OutboundConnectorContext context) throws Exception {
     System.out.println("Executing Javascript connector");
-    return null;
+    var input = context.bindVariables(JavascriptInputRequest.class);
+    try (final var client = HttpClient.newHttpClient()) {
+      // Execute the Javascript code
+      final var request = HttpRequest.newBuilder()
+          .uri(URI.create(JS_MAGIC_ENDPOINT))
+          .header("Content-Type", "application/json")
+          .POST(HttpRequest.BodyPublishers.ofString(
+              "{\"script\": \"" + input.script() + "\", \"parameters\": " + input.parameters()
+                  + "}"));
+      final var response = client.send(request.build(), HttpResponse.BodyHandlers.ofString());
+      return response.body();
+    }
   }
 }
