d8588d3f1cb595c81d0dd19d56a0e5fbb6e87cfb
mathieu.stennier <mathieu.stennier@camunda.com>
2025-02-13 10:36:23 +0100

    add generated json

--- connectors/webhook/src/main/java/io/camunda/connector/inbound/HttpWebhookExecutable.java
+++ connectors/webhook/src/main/java/io/camunda/connector/inbound/HttpWebhookExecutable.java
@@ -57,8 +57,7 @@ import org.slf4j.LoggerFactory;
     version = 11,
     inputDataClass = WebhookConnectorPropertiesWrapper.class,
     description = "Configure webhook to receive callbacks",
-    documentationRef =
-        "https://docs.camunda.io/docs/components/connectors/protocol/http-webhook/",
+    documentationRef = "https://docs.camunda.io/docs/components/connectors/protocol/http-webhook/",
     propertyGroups = {
       @PropertyGroup(id = "endpoint", label = "Webhook configuration"),
       @PropertyGroup(id = "authentication", label = "Authentication"),
