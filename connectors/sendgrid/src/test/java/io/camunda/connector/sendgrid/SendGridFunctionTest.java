1c337f4fab4baf048b3a11fe5f24b071308b5778
DenovVasil <vasil.denov-ext@camunda.com>
2025-02-13 15:31:36 +0200

    feat(sendgrid): attachment support (#3891)
    (cherry picked from commit 2647426de46e4749ed8e014109b7a1260c891c80)

--- connectors/sendgrid/src/test/java/io/camunda/connector/sendgrid/SendGridFunctionTest.java
+++ connectors/sendgrid/src/test/java/io/camunda/connector/sendgrid/SendGridFunctionTest.java
@@ -8,12 +8,10 @@ package io.camunda.connector.sendgrid;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
 import com.sendgrid.Response;
@@ -21,6 +19,9 @@ import com.sendgrid.SendGrid;
 import io.camunda.connector.api.outbound.OutboundConnectorContext;
 import io.camunda.connector.sendgrid.model.SendGridRequest;
 import io.camunda.connector.test.outbound.OutboundConnectorContextBuilder;
+import io.camunda.document.Document;
+import io.camunda.zeebe.client.api.response.DocumentMetadata;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
@@ -42,6 +43,8 @@ public class SendGridFunctionTest extends BaseTest {
   private static final String PERSONALIZATION_JSON_NAME = "personalizations";
   private static final String NAME_JSON_NAME = "name";
   private static final String EMAIL_JSON_NAME = "email";
+  private static final String ATTACHMENTS_JSON_NAME = "attachments";
+  private static final String ATTACHMENTS_FILE_NAME_JSON_NAME = "filename";
 
   private OutboundConnectorContext context;
   private SendGridFunction function;
@@ -107,9 +110,15 @@ public class SendGridFunctionTest extends BaseTest {
   public void execute_shouldCreateRequestWithMailAndExpectedData(String input) throws Exception {
     // Given
     context = contextBuilder.variables(input).build();
-    ArgumentCaptor<Request> requestArgumentCaptor = ArgumentCaptor.forClass(Request.class);
+
+    var contextWithMocketDocument = mock(OutboundConnectorContext.class);
+
+    var requestWithMockedDocument = prepareRequestWithDocumentMock(context);
+
+    when(contextWithMocketDocument.bindVariables(any())).thenReturn(requestWithMockedDocument);
+
     // When
-    function.execute(context);
+    function.execute(contextWithMocketDocument);
     verify(sendGridMock).api(requestArgumentCaptor.capture());
     // Then we have POST request with mail participants,
     Request requestValue = requestArgumentCaptor.getValue();
@@ -122,10 +131,14 @@ public class SendGridFunctionTest extends BaseTest {
         requestJsonObject.withObject(
             JsonPointer.valueOf("/" + PERSONALIZATION_JSON_NAME + "/0/" + TO_JSON_NAME + "/0"));
 
+    var array = (ArrayNode) requestJsonObject.get(ATTACHMENTS_JSON_NAME);
+    String attachmentName = array.get(0).get(ATTACHMENTS_FILE_NAME_JSON_NAME).textValue();
+
     assertThat(from.get(NAME_JSON_NAME).textValue()).isEqualTo(ActualValue.SENDER_NAME);
     assertThat(from.get(EMAIL_JSON_NAME).textValue()).isEqualTo(ActualValue.SENDER_EMAIL);
     assertThat(to.get(NAME_JSON_NAME).textValue()).isEqualTo(ActualValue.RECEIVER_NAME);
     assertThat(to.get(EMAIL_JSON_NAME).textValue()).isEqualTo(ActualValue.RECEIVER_EMAIL);
+    assertThat(attachmentName).isEqualTo(ActualValue.ATTACHED_FILE_NAME);
   }
 
   @ParameterizedTest(name = "Should send mail with template. Test case # {index}")
@@ -133,16 +146,27 @@ public class SendGridFunctionTest extends BaseTest {
   public void execute_shouldSendMailByTemplateIfTemplateExist(String input) throws Exception {
     // Given
     context = contextBuilder.variables(input).build();
-    ArgumentCaptor<Request> requestArgumentCaptor = ArgumentCaptor.forClass(Request.class);
+
+    var contextWithMocketDocument = mock(OutboundConnectorContext.class);
+
+    var requestWithMockedDocument = prepareRequestWithDocumentMock(context);
+
+    when(contextWithMocketDocument.bindVariables(any())).thenReturn(requestWithMockedDocument);
+
     // When
-    function.execute(context);
+    function.execute(contextWithMocketDocument);
     verify(sendGridMock).api(requestArgumentCaptor.capture());
     // Then we have 'template_id' in sendGridRequest with expected ID and 'content' is not exist
     Request requestValue = requestArgumentCaptor.getValue();
+
     var requestJsonObject = objectMapper.readTree(requestValue.getBody());
+
+    var array = (ArrayNode) requestJsonObject.get(ATTACHMENTS_JSON_NAME);
+    String attachmentName = array.get(0).get(ATTACHMENTS_FILE_NAME_JSON_NAME).textValue();
     assertThat(requestJsonObject.get(TEMPLATE_ID_JSON_NAME).textValue())
         .isEqualTo(ActualValue.Template.ID);
     assertThat(requestJsonObject.has(CONTENT_JSON_NAME)).isFalse();
+    assertThat(attachmentName).isEqualTo(ActualValue.ATTACHED_FILE_NAME);
   }
 
   @ParameterizedTest(name = "Should send mail with content. Test case # {index}")
@@ -150,8 +174,16 @@ public class SendGridFunctionTest extends BaseTest {
   public void execute_shouldSendMailIfContentExist(String input) throws Exception {
     // Given
     context = contextBuilder.variables(input).build();
+
+    var contextWithMocketDocument = mock(OutboundConnectorContext.class);
+
+    var requestWithMockedDocument = prepareRequestWithDocumentMock(context);
+
+    when(contextWithMocketDocument.bindVariables(any())).thenReturn(requestWithMockedDocument);
+
     // When
-    function.execute(context);
+    function.execute(contextWithMocketDocument);
+
     verify(sendGridMock).api(requestArgumentCaptor.capture());
     // Then we have 'content' in sendGridRequest with expected data and template ID is not exist
     var requestJsonObject = objectMapper.readTree(requestArgumentCaptor.getValue().getBody());
@@ -165,5 +197,20 @@ public class SendGridFunctionTest extends BaseTest {
         .isEqualTo(ActualValue.Content.VALUE);
 
     assertThat(requestJsonObject.has(TEMPLATE_ID_JSON_NAME)).isFalse();
+  }
+
+  private SendGridRequest prepareRequestWithDocumentMock(OutboundConnectorContext context) {
+    var request = context.bindVariables(SendGridRequest.class);
+
+    var document = mock(Document.class);
+    var documentMetadata = mock(DocumentMetadata.class);
+    when(document.metadata()).thenReturn(documentMetadata);
+
+    String fileName = request.getAttachments().getFirst().metadata().getFileName();
+    when(documentMetadata.getFileName()).thenReturn(fileName);
+    when(document.asInputStream()).thenReturn(new ByteArrayInputStream(new byte[0]));
+
+    request.setAttachments(List.of(document));
+    return request;
   }
 }
