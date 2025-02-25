c95a1a9f87656e7323bcc17e6a421283c68e5714
DenovVasil <vasil.denov-ext@camunda.com>
2025-02-25 23:05:06 +0200

    feat(slack): add attachments support (#4119)

---
+++ connectors/slack/src/main/java/io/camunda/connector/slack/outbound/caller/FileUploader.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+package io.camunda.connector.slack.outbound.caller;
+
+import static java.util.stream.Collectors.toList;
+
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.files.FilesCompleteUploadExternalRequest;
+import com.slack.api.methods.request.files.FilesGetUploadURLExternalRequest;
+import com.slack.api.methods.response.files.FilesCompleteUploadExternalResponse;
+import com.slack.api.methods.response.files.FilesGetUploadURLExternalResponse;
+import com.slack.api.model.File;
+import com.slack.api.util.http.SlackHttpClient;
+import io.camunda.document.Document;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileUploader {
+
+  public static final String GET_EXTERNAL_URL_EX = "Error during filesGetUploadURLExternal call";
+  public static final String EXTERNAL_URL_CALL_EX = "Error during external call: ";
+  public static final String COMPLETE_UPLOAD_CALL_EX =
+      "Error during filesCompleteUploadExternal call";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(FileUploader.class);
+
+  private MethodsClient methodsClient;
+
+  public FileUploader(MethodsClient methodsClient) {
+    this.methodsClient = methodsClient;
+  }
+
+  public List<File> uploadDocuments(List<Document> documents) {
+    return documents.stream()
+        .map(
+            doc -> {
+              try {
+                return this.uploadDocument(methodsClient, doc);
+              } catch (SlackApiException | IOException e) {
+                throw new RuntimeException(e);
+              }
+            })
+        .filter(Objects::nonNull)
+        .collect(toList());
+  }
+
+  private File uploadDocument(MethodsClient methodsClient, Document document)
+      throws SlackApiException, IOException {
+    FilesGetUploadURLExternalResponse externalURLUploadResponse =
+        getFileUploadURL(methodsClient, document);
+    uploadFileByURL(externalURLUploadResponse, document);
+    return completeFileUpload(externalURLUploadResponse, document);
+  }
+
+  private FilesGetUploadURLExternalResponse getFileUploadURL(
+      MethodsClient methodsClient, Document document) throws SlackApiException, IOException {
+    var filesGetUploadURLExternalRequest =
+        FilesGetUploadURLExternalRequest.builder()
+            .filename(document.metadata().getFileName())
+            .length(document.asByteArray().length)
+            .build();
+    return methodsClient.filesGetUploadURLExternal(filesGetUploadURLExternalRequest);
+  }
+
+  private void uploadFileByURL(
+      FilesGetUploadURLExternalResponse uploadFileURLResp, Document document) throws IOException {
+    if (!uploadFileURLResp.isOk()) {
+      String msg = GET_EXTERNAL_URL_EX + "\n Errors: " + uploadFileURLResp.getError();
+      LOGGER.error(msg);
+      throw new RuntimeException(msg);
+    }
+    var config = methodsClient.getSlackHttpClient().getConfig();
+    OkHttpClient okHttpClient = SlackHttpClient.buildOkHttpClient(config);
+
+    var request =
+        new Request.Builder()
+            .url(uploadFileURLResp.getUploadUrl())
+            .post(RequestBody.create(document.asByteArray()))
+            .build();
+
+    try (Response directCallResp = okHttpClient.newCall(request).execute()) {
+      if (directCallResp.code() != 200) {
+        String msg = EXTERNAL_URL_CALL_EX + directCallResp.message();
+        LOGGER.error(msg);
+        throw new RuntimeException(msg);
+      }
+    }
+  }
+
+  private File completeFileUpload(
+      FilesGetUploadURLExternalResponse uploadFileUrlResp, Document document)
+      throws SlackApiException, IOException {
+    FilesCompleteUploadExternalResponse completeUploadResp =
+        methodsClient.filesCompleteUploadExternal(
+            FilesCompleteUploadExternalRequest.builder()
+                .files(
+                    List.of(
+                        FilesCompleteUploadExternalRequest.FileDetails.builder()
+                            .id(uploadFileUrlResp.getFileId())
+                            .title(document.metadata().getFileName())
+                            .build()))
+                .build());
+
+    if (completeUploadResp.isOk()) {
+      List<File> files = completeUploadResp.getFiles();
+      return getFirst(files);
+    } else {
+      String msg = COMPLETE_UPLOAD_CALL_EX + "\n Errors: " + completeUploadResp.getError();
+      LOGGER.error(msg);
+      throw new RuntimeException(msg);
+    }
+  }
+
+  // In fact, we always have only one file in List
+  private File getFirst(List<File> files) {
+    return files == null || files.isEmpty() ? null : files.getFirst();
+  }
+
+  public void setMethodsClient(MethodsClient methodsClient) {
+    this.methodsClient = methodsClient;
+  }
+
+  public MethodsClient getMethodsClient() {
+    return methodsClient;
+  }
+}
---
+++ connectors/slack/src/test/java/io/camunda/connector/slack/outbound/caller/FileUploaderTest.java
@@ -0,0 +1,223 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+package io.camunda.connector.slack.outbound.caller;
+
+import static io.camunda.connector.slack.outbound.caller.FileUploader.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.slack.api.SlackConfig;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.files.FilesCompleteUploadExternalRequest;
+import com.slack.api.methods.request.files.FilesGetUploadURLExternalRequest;
+import com.slack.api.methods.response.files.FilesCompleteUploadExternalResponse;
+import com.slack.api.methods.response.files.FilesGetUploadURLExternalResponse;
+import com.slack.api.model.File;
+import com.slack.api.util.http.SlackHttpClient;
+import io.camunda.client.api.response.DocumentMetadata;
+import io.camunda.connector.document.annotation.jackson.DocumentReferenceModel;
+import io.camunda.document.CamundaDocument;
+import io.camunda.document.Document;
+import io.camunda.document.reference.DocumentReference;
+import io.camunda.document.store.CamundaDocumentStore;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Map;
+import okhttp3.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class FileUploaderTest {
+
+  @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+  private MethodsClient methodsClient;
+
+  @InjectMocks private FileUploader fileUploader;
+
+  @Test
+  void uploadDocuments() throws SlackApiException, IOException {
+    List<Document> documents = List.of(prepareDocument());
+
+    mockGetExternalURL();
+    when(methodsClient.getSlackHttpClient().getConfig()).thenReturn(SlackConfig.DEFAULT);
+
+    OkHttpClient okHttpClient = Mockito.mock(OkHttpClient.class, Answers.RETURNS_DEEP_STUBS);
+
+    try (MockedStatic<SlackHttpClient> slackHttpClient =
+        Mockito.mockStatic(SlackHttpClient.class)) {
+      slackHttpClient.when(() -> SlackHttpClient.buildOkHttpClient(any())).thenReturn(okHttpClient);
+
+      var response = mock(Response.class);
+      when(response.code()).thenReturn(200);
+
+      when(okHttpClient.newCall(any(Request.class)).execute()).thenReturn(response);
+
+      List<File> files = List.of(File.builder().id("id").build());
+      mockCompleteUpload(files);
+
+      List<File> result = fileUploader.uploadDocuments(documents);
+      assertThat(result).hasSize(1);
+      assertThat(result).isEqualTo(files);
+    }
+  }
+
+  @Test
+  void uploadDocumentsShouldFilterEmptyFiles() throws SlackApiException, IOException {
+    List<Document> documents = List.of(prepareDocument());
+    mockGetExternalURL();
+    when(methodsClient.getSlackHttpClient().getConfig()).thenReturn(SlackConfig.DEFAULT);
+
+    OkHttpClient okHttpClient = Mockito.mock(OkHttpClient.class, Answers.RETURNS_DEEP_STUBS);
+
+    try (MockedStatic<SlackHttpClient> slackHttpClient =
+        Mockito.mockStatic(SlackHttpClient.class)) {
+      slackHttpClient.when(() -> SlackHttpClient.buildOkHttpClient(any())).thenReturn(okHttpClient);
+
+      var response = mock(Response.class);
+      when(response.code()).thenReturn(200);
+
+      when(okHttpClient.newCall(any(Request.class)).execute()).thenReturn(response);
+
+      List<File> emptyFiles = List.of();
+      mockCompleteUpload(emptyFiles);
+
+      List<File> result = fileUploader.uploadDocuments(documents);
+      assertThat(result).hasSize(0);
+    }
+  }
+
+  @Test
+  void exceptionDuringGetExternalURLCall() throws SlackApiException, IOException {
+    List<Document> documents = List.of(prepareDocument());
+    var uploadURLResp = new FilesGetUploadURLExternalResponse();
+    uploadURLResp.setOk(false);
+    uploadURLResp.setError("Do not have Read permissions");
+
+    when(methodsClient.filesGetUploadURLExternal(any(FilesGetUploadURLExternalRequest.class)))
+        .thenReturn(uploadURLResp);
+
+    Exception ex =
+        assertThrows(RuntimeException.class, () -> fileUploader.uploadDocuments(documents));
+
+    assertThat(ex.getMessage()).contains(GET_EXTERNAL_URL_EX);
+    assertThat(ex.getMessage()).contains(uploadURLResp.getError());
+  }
+
+  @Test
+  void exceptionDuringExternalURlCall() throws SlackApiException, IOException {
+    List<Document> documents = List.of(prepareDocument());
+
+    var uploadURLResp = new FilesGetUploadURLExternalResponse();
+    uploadURLResp.setOk(true);
+    uploadURLResp.setUploadUrl("https:example.com");
+
+    when(methodsClient.filesGetUploadURLExternal(any(FilesGetUploadURLExternalRequest.class)))
+        .thenReturn(uploadURLResp);
+    when(methodsClient.getSlackHttpClient().getConfig()).thenReturn(SlackConfig.DEFAULT);
+    OkHttpClient okHttpClient = Mockito.mock(OkHttpClient.class, Answers.RETURNS_DEEP_STUBS);
+
+    try (MockedStatic<SlackHttpClient> slackHttpClient =
+        Mockito.mockStatic(SlackHttpClient.class)) {
+      slackHttpClient.when(() -> SlackHttpClient.buildOkHttpClient(any())).thenReturn(okHttpClient);
+
+      String msg = "Internal Server Error";
+
+      Response response = mock(Response.class);
+      when(response.code()).thenReturn(500);
+      when(response.message()).thenReturn(msg);
+
+      when(okHttpClient.newCall(any(Request.class)).execute()).thenReturn(response);
+
+      Exception ex =
+          assertThrows(RuntimeException.class, () -> fileUploader.uploadDocuments(documents));
+
+      assertThat(ex.getMessage()).contains(EXTERNAL_URL_CALL_EX);
+      assertThat(ex.getMessage()).contains(msg);
+    }
+  }
+
+  @Test
+  void ExceptionDuringCompleteFileUploadCall() throws SlackApiException, IOException {
+    List<Document> documents = List.of(prepareDocument());
+    var uploadURLResp = new FilesGetUploadURLExternalResponse();
+    uploadURLResp.setOk(true);
+    uploadURLResp.setUploadUrl("https:example.com");
+
+    when(methodsClient.filesGetUploadURLExternal(any(FilesGetUploadURLExternalRequest.class)))
+        .thenReturn(uploadURLResp);
+    when(methodsClient.getSlackHttpClient().getConfig()).thenReturn(SlackConfig.DEFAULT);
+
+    OkHttpClient okHttpClient = Mockito.mock(OkHttpClient.class, Answers.RETURNS_DEEP_STUBS);
+
+    try (MockedStatic<SlackHttpClient> slackHttpClient =
+        Mockito.mockStatic(SlackHttpClient.class)) {
+      slackHttpClient.when(() -> SlackHttpClient.buildOkHttpClient(any())).thenReturn(okHttpClient);
+
+      var response = mock(Response.class);
+      when(response.code()).thenReturn(200);
+
+      when(okHttpClient.newCall(any(Request.class)).execute()).thenReturn(response);
+
+      FilesCompleteUploadExternalResponse completeUploadResp =
+          new FilesCompleteUploadExternalResponse();
+      completeUploadResp.setOk(false);
+      completeUploadResp.setError("somthing go wrong");
+
+      when(methodsClient.filesCompleteUploadExternal(any(FilesCompleteUploadExternalRequest.class)))
+          .thenReturn(completeUploadResp);
+
+      Exception ex =
+          assertThrows(RuntimeException.class, () -> fileUploader.uploadDocuments(documents));
+
+      assertThat(ex.getMessage()).contains(completeUploadResp.getError());
+      assertThat(ex.getMessage()).contains(COMPLETE_UPLOAD_CALL_EX);
+    }
+  }
+
+  private Document prepareDocument() {
+    DocumentReference.CamundaDocumentReference documentReference =
+        Mockito.mock(DocumentReference.CamundaDocumentReference.class);
+    CamundaDocumentStore documentStore = Mockito.mock(CamundaDocumentStore.class);
+
+    var byteInput = new ByteArrayInputStream(new byte[0]);
+    when(documentStore.getDocumentContent(any())).thenReturn(byteInput);
+
+    DocumentMetadata documentMetadata =
+        new DocumentReferenceModel.CamundaDocumentMetadataModel(
+            "txt", OffsetDateTime.now(), 3000L, "fileName", "processId", 2000L, Map.of());
+
+    return new CamundaDocument(documentMetadata, documentReference, documentStore);
+  }
+
+  private void mockGetExternalURL() throws SlackApiException, IOException {
+    var uploadURLResp = new FilesGetUploadURLExternalResponse();
+    uploadURLResp.setOk(true);
+    uploadURLResp.setUploadUrl("https:example.com");
+
+    when(methodsClient.filesGetUploadURLExternal(any(FilesGetUploadURLExternalRequest.class)))
+        .thenReturn(uploadURLResp);
+  }
+
+  private void mockCompleteUpload(List<File> filesResponse) throws SlackApiException, IOException {
+    FilesCompleteUploadExternalResponse completeUploadResp =
+        new FilesCompleteUploadExternalResponse();
+    completeUploadResp.setOk(true);
+    completeUploadResp.setFiles(filesResponse);
+
+    when(methodsClient.filesCompleteUploadExternal(any(FilesCompleteUploadExternalRequest.class)))
+        .thenReturn(completeUploadResp);
+  }
+}
