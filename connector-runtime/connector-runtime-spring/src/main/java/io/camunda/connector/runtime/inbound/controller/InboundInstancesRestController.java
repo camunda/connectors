de3d9bb0975b2c37389d9a261d4ae7379cda6e1e
Jonathan Roques <jonathan.roques@camunda.com>
2025-03-03 16:13:20 +0100

    chore(update-api): rework the Inbound Instances API, Controller, and Security config + tests

---
+++ connector-runtime/connector-runtime-spring/src/main/java/io/camunda/connector/runtime/inbound/controller/InboundInstancesRestController.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.connector.runtime.inbound.controller;
+
+import io.camunda.connector.api.inbound.Activity;
+import io.camunda.connector.api.inbound.ProcessElement;
+import io.camunda.connector.runtime.inbound.controller.exception.DataNotFoundException;
+import io.camunda.connector.runtime.inbound.executable.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * This controller is used by the c4-connectors to get the active inbound connectors, executables
+ * and logs.
+ *
+ * <p><b>Note:</b> Be aware that changing the response format will break the c4-connectors, so make
+ * sure to update the c4-connectors as well.
+ */
+@RestController
+@RequestMapping("/inbound-instances")
+public class InboundInstancesRestController {
+
+  private final InboundExecutableRegistry executableRegistry;
+
+  public InboundInstancesRestController(InboundExecutableRegistry executableRegistry) {
+    this.executableRegistry = executableRegistry;
+  }
+
+  @GetMapping()
+  public List<ConnectorInstances> getConnectorInstances() {
+    return getConnectorsInstances(null);
+  }
+
+  @GetMapping("/{type}")
+  public ConnectorInstances getConnectorInstance(@PathVariable(name = "type") String type) {
+    var instances = getConnectorsInstances(type);
+    if (instances.isEmpty()) {
+      throw new DataNotFoundException(ConnectorInstances.class, type);
+    }
+    return instances.getFirst();
+  }
+
+  @GetMapping("/{type}/executables/{executableId}")
+  public ActiveInboundConnectorResponse getConnectorInstanceExecutable(
+      @PathVariable(name = "type") String type,
+      @PathVariable(name = "executableId") String executableId) {
+    var instances = getConnectorsInstances(type);
+    if (instances.isEmpty()) {
+      throw new DataNotFoundException(ConnectorInstances.class, type);
+    }
+    var instance = instances.getFirst();
+    var executables =
+        instance.instances().stream()
+            .filter(
+                activeInboundConnectorResponse ->
+                    activeInboundConnectorResponse.executableId().toString().equals(executableId))
+            .toList();
+    if (executables.isEmpty()) {
+      throw new DataNotFoundException(ActiveInboundConnectorResponse.class, executableId);
+    }
+    return executables.getFirst();
+  }
+
+  @GetMapping("/{type}/executables/{executableId}/logs")
+  public Collection<Activity> getConnectorInstanceExecutableLogs(
+      @PathVariable(name = "type") String type,
+      @PathVariable(name = "executableId") String executableId) {
+    var executable = getConnectorInstanceExecutable(type, executableId);
+    var processIds =
+        executable.elements().stream().map(ProcessElement::bpmnProcessId).distinct().toList();
+    if (processIds.size() > 1) {
+      throw new RuntimeException(
+          "Multiple process ids found for the executable id: "
+              + executableId
+              + ". This is not supported yet.");
+    }
+    var processId = processIds.getFirst();
+    var result =
+        executableRegistry
+            .query(new ActiveExecutableQuery(processId, null, type, executable.tenantId()))
+            .stream()
+            .filter(
+                activeExecutableResponse ->
+                    activeExecutableResponse.executableId().toString().equals(executableId))
+            .findFirst();
+    if (result.isEmpty()) {
+      throw new DataNotFoundException(Activity.class, executableId);
+    }
+    return result.get().logs();
+  }
+
+  /**
+   * Be aware that this API is used by c4-connectors to get the active inbound connectors grouped by
+   * connector type. Changing the response format will break the c4-connectors, so make sure to
+   * update the c4-connectors as well.
+   *
+   * @param type the connectorId to filter by (also called 'connectorId')
+   */
+  private List<ConnectorInstances> getConnectorsInstances(String type) {
+    var activeInboundConnectors = getActiveInboundConnectors(type);
+    return activeInboundConnectors.stream()
+        .collect(Collectors.groupingBy(ActiveInboundConnectorResponse::type, Collectors.toList()))
+        .entrySet()
+        .stream()
+        .map(
+            e ->
+                new ConnectorInstances(
+                    e.getKey(), executableRegistry.getConnectorName(e.getKey()), e.getValue()))
+        .toList();
+  }
+
+  private List<ActiveInboundConnectorResponse> getActiveInboundConnectors(String type) {
+    return executableRegistry.query(new ActiveExecutableQuery(null, null, type, null)).stream()
+        .map(ActiveInboundConnectorResponse::from)
+        .collect(Collectors.toList());
+  }
+}
---
+++ connector-runtime/spring-boot-starter-camunda-connectors/src/test/java/io/camunda/connector/runtime/inbound/InboundInstancesRestControllerTest.java
@@ -0,0 +1,313 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.connector.runtime.inbound;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.camunda.connector.api.inbound.*;
+import io.camunda.connector.api.inbound.webhook.WebhookConnectorExecutable;
+import io.camunda.connector.api.inbound.webhook.WebhookProcessingPayload;
+import io.camunda.connector.api.inbound.webhook.WebhookResult;
+import io.camunda.connector.api.json.ConnectorsObjectMapperSupplier;
+import io.camunda.connector.runtime.app.TestConnectorRuntimeApplication;
+import io.camunda.connector.runtime.core.inbound.InboundConnectorElement;
+import io.camunda.connector.runtime.core.inbound.correlation.MessageCorrelationPoint.StandaloneMessageCorrelationPoint;
+import io.camunda.connector.runtime.inbound.controller.ActiveInboundConnectorResponse;
+import io.camunda.connector.runtime.inbound.controller.InboundInstancesRestController;
+import io.camunda.connector.runtime.inbound.executable.ActiveExecutableQuery;
+import io.camunda.connector.runtime.inbound.executable.ActiveExecutableResponse;
+import io.camunda.connector.runtime.inbound.executable.ConnectorInstances;
+import io.camunda.connector.runtime.inbound.executable.InboundExecutableRegistry;
+import java.util.*;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(classes = TestConnectorRuntimeApplication.class)
+@AutoConfigureMockMvc
+class InboundInstancesRestControllerTest {
+
+  @Autowired private MockMvc mockMvc;
+
+  @MockitoSpyBean private InboundExecutableRegistry executableRegistry;
+
+  @InjectMocks private InboundInstancesRestController controller;
+
+  private static final String TYPE_1 = "webhook";
+  private static final UUID UUID_1 = UUID.randomUUID();
+  private static final String TYPE_2 = "anotherType";
+  private static final UUID UUID_2 = UUID.randomUUID();
+  private static final UUID UUID_3 = UUID.randomUUID();
+
+  static class AnotherExecutable implements InboundConnectorExecutable<InboundConnectorContext> {
+
+    @Override
+    public void activate(InboundConnectorContext context) throws Exception {}
+
+    @Override
+    public void deactivate() throws Exception {}
+  }
+
+  static class TestWebhookExecutable implements WebhookConnectorExecutable {
+
+    @Override
+    public WebhookResult triggerWebhook(WebhookProcessingPayload payload) throws Exception {
+      return null;
+    }
+  }
+
+  @BeforeEach
+  public void init() {
+    when(executableRegistry.getConnectorName(TYPE_1)).thenReturn("Webhook");
+    when(executableRegistry.getConnectorName(TYPE_2)).thenReturn("AnotherType");
+    when(executableRegistry.query(any()))
+        .thenAnswer(
+            invocationOnMock ->
+                "UNKNOWN-ID"
+                        .equals(invocationOnMock.getArgument(0, ActiveExecutableQuery.class).type())
+                    ? Collections.emptyList()
+                    : Stream.of(
+                            new ActiveExecutableResponse(
+                                UUID_1,
+                                TestWebhookExecutable.class,
+                                List.of(
+                                    new InboundConnectorElement(
+                                        Map.of("inbound.context", "myPath", "inbound.type", TYPE_1),
+                                        new StandaloneMessageCorrelationPoint(
+                                            "myPath", "=expression", "=myPath", null),
+                                        new ProcessElement("ProcessA", 1, 1, "", ""))),
+                                Health.up(),
+                                Collections.emptyList(),
+                                System.currentTimeMillis()),
+                            new ActiveExecutableResponse(
+                                UUID_2,
+                                AnotherExecutable.class,
+                                List.of(
+                                    new InboundConnectorElement(
+                                        Map.of(
+                                            "inbound.other.prop",
+                                            "myOtherValue",
+                                            "inbound.type",
+                                            TYPE_2),
+                                        new StandaloneMessageCorrelationPoint(
+                                            "myPath", "=expression", "=myPath", null),
+                                        new ProcessElement("ProcessB", 2, 1, "", ""))),
+                                Health.up(),
+                                Collections.emptyList(),
+                                System.currentTimeMillis()),
+                            new ActiveExecutableResponse(
+                                UUID_3,
+                                AnotherExecutable.class,
+                                List.of(
+                                    new InboundConnectorElement(
+                                        Map.of(
+                                            "inbound.other.prop",
+                                            "myOtherValue2",
+                                            "inbound.type",
+                                            TYPE_2),
+                                        new StandaloneMessageCorrelationPoint(
+                                            "myPath", "=expression", "=myPath", null),
+                                        new ProcessElement("ProcessC", 2, 1, "id1", "")),
+                                    new InboundConnectorElement(
+                                        Map.of(
+                                            "inbound.other.prop",
+                                            "myOtherValue2_2",
+                                            "inbound.type",
+                                            TYPE_2),
+                                        new StandaloneMessageCorrelationPoint(
+                                            "myPath", "=expression2", "=myPath2", null),
+                                        new ProcessElement("ProcessC", 2, 1, "id2", ""))),
+                                Health.up(),
+                                List.of(
+                                    Activity.level(Severity.INFO).tag("myTag").message("myMessage"),
+                                    Activity.level(Severity.ERROR)
+                                        .tag("myTag2")
+                                        .message("myMessage2")),
+                                System.currentTimeMillis()))
+                        .filter(
+                            response -> {
+                              var providedType =
+                                  invocationOnMock
+                                      .getArgument(0, ActiveExecutableQuery.class)
+                                      .type();
+                              return switch (providedType) {
+                                case null -> true;
+                                case TYPE_1 -> response.executableId().equals(UUID_1);
+                                case TYPE_2 ->
+                                    response.executableId().equals(UUID_2)
+                                        || response.executableId().equals(UUID_3);
+                                default -> false;
+                              };
+                            })
+                        .toList());
+  }
+
+  @Test
+  public void shouldReturnConnectorInstances() throws Exception {
+    var response =
+        mockMvc
+            .perform(get("/inbound-instances"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+    List<ConnectorInstances> instance =
+        ConnectorsObjectMapperSupplier.getCopy().readValue(response, new TypeReference<>() {});
+    assertEquals(2, instance.size());
+    var instance1 = instance.get(0);
+    assertEquals(TYPE_1, instance1.connectorId());
+    assertEquals("Webhook", instance1.connectorName());
+    assertEquals(1, instance1.instances().size());
+    assertEquals(UUID_1, instance1.instances().get(0).executableId());
+    assertEquals("ProcessA", instance1.instances().get(0).elements().getFirst().bpmnProcessId());
+
+    var instance2 = instance.get(1);
+    assertEquals(TYPE_2, instance2.connectorId());
+    assertEquals("AnotherType", instance2.connectorName());
+    assertEquals(2, instance2.instances().size());
+    assertEquals(UUID_2, instance2.instances().get(0).executableId());
+    assertEquals("ProcessB", instance2.instances().get(0).elements().getFirst().bpmnProcessId());
+    assertEquals(UUID_3, instance2.instances().get(1).executableId());
+    assertEquals("ProcessC", instance2.instances().get(1).elements().getFirst().bpmnProcessId());
+  }
+
+  @Test
+  public void shouldReturn404_whenUnknownConnectorType() throws Exception {
+    mockMvc
+        .perform(get("/inbound-instances/UNKNOWN-ID"))
+        .andExpect(status().isNotFound())
+        .andExpect(
+            content()
+                .string(
+                    containsString(
+                        "Data of type 'ConnectorInstances' with id 'UNKNOWN-ID' not found")));
+  }
+
+  @Test
+  public void shouldReturnSingleConnectorInstance() throws Exception {
+    var response =
+        mockMvc
+            .perform(get("/inbound-instances/" + TYPE_1))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+    ConnectorInstances instance =
+        ConnectorsObjectMapperSupplier.getCopy().readValue(response, ConnectorInstances.class);
+    assertEquals(TYPE_1, instance.connectorId());
+    assertEquals("Webhook", instance.connectorName());
+    assertEquals(1, instance.instances().size());
+    assertEquals(UUID_1, instance.instances().get(0).executableId());
+    assertEquals("ProcessA", instance.instances().get(0).elements().getFirst().bpmnProcessId());
+  }
+
+  @Test
+  public void shouldReturn404_whenUnknownExecutableId() throws Exception {
+    mockMvc
+        .perform(get("/inbound-instances/" + TYPE_1 + "/executables/UNKNOWN-ID"))
+        .andExpect(status().isNotFound())
+        .andExpect(
+            content()
+                .string(
+                    containsString(
+                        "Data of type 'ActiveInboundConnectorResponse' with id 'UNKNOWN-ID' not found")));
+  }
+
+  @Test
+  public void shouldReturn404_whenUnknownConnectorTypeAndValidExecutableId() throws Exception {
+    mockMvc
+        .perform(get("/inbound-instances/UNKNOWN-ID/executables/" + UUID_1))
+        .andExpect(status().isNotFound())
+        .andExpect(
+            content()
+                .string(
+                    containsString(
+                        "Data of type 'ConnectorInstances' with id 'UNKNOWN-ID' not found")));
+  }
+
+  @Test
+  public void shouldReturnSingleExecutable() throws Exception {
+    var response =
+        mockMvc
+            .perform(get("/inbound-instances/" + TYPE_1 + "/executables/" + UUID_1))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+    ActiveInboundConnectorResponse executable =
+        ConnectorsObjectMapperSupplier.getCopy()
+            .readValue(response, ActiveInboundConnectorResponse.class);
+    assertEquals(UUID_1, executable.executableId());
+    assertEquals("ProcessA", executable.elements().getFirst().bpmnProcessId());
+  }
+
+  @Test
+  public void shouldReturnEmptyActivityLogs_whenNoLogs() throws Exception {
+    var response =
+        mockMvc
+            .perform(get("/inbound-instances/" + TYPE_1 + "/executables/" + UUID_1 + "/logs"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+    List<Collection<Activity>> logs =
+        ConnectorsObjectMapperSupplier.getCopy().readValue(response, new TypeReference<>() {});
+    assertEquals(1, logs.size());
+    var log = logs.get(0);
+    assertEquals(0, log.size());
+  }
+
+  @Test
+  public void shouldReturnActivityLogs_whenTypeProvided() throws Exception {
+    var response =
+        mockMvc
+            .perform(get("/inbound-instances/" + TYPE_2 + "/executables/" + UUID_3 + "/logs"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+    List<Collection<Activity>> logs =
+        ConnectorsObjectMapperSupplier.getCopy().readValue(response, new TypeReference<>() {});
+    assertEquals(1, logs.size());
+    var log = logs.get(0);
+    assertEquals(2, log.size());
+    var log1 = log.stream().filter(a -> a.severity() == Severity.INFO).findFirst().get();
+    assertEquals("myTag", log1.tag());
+    assertEquals("myMessage", log1.message());
+    var log2 = log.stream().filter(a -> a.severity() == Severity.ERROR).findFirst().get();
+    assertEquals("myTag2", log2.tag());
+    assertEquals("myMessage2", log2.message());
+  }
+}
