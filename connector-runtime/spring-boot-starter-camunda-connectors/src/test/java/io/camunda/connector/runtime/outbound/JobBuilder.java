09a6f4d4af6d36861e428e7719280bd2dda30d60
Mathias Vandaele <mathias.vandaele@camunda.com>
2025-03-25 23:31:04 +0800

    others(tests): add end to end tests for the connector Job handler

--- connector-runtime/spring-boot-starter-camunda-connectors/src/test/java/io/camunda/connector/runtime/outbound/JobBuilder.java
+++ connector-runtime/spring-boot-starter-camunda-connectors/src/test/java/io/camunda/connector/runtime/outbound/JobBuilder.java
@@ -22,13 +22,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import io.camunda.client.api.command.CompleteJobCommandStep1;
-import io.camunda.client.api.command.FailJobCommandStep1;
-import io.camunda.client.api.command.ThrowErrorCommandStep1;
-import io.camunda.client.api.response.ActivatedJob;
-import io.camunda.client.api.worker.JobClient;
 import io.camunda.connector.runtime.core.Keywords;
 import io.camunda.connector.runtime.core.outbound.ConnectorJobHandler;
+import io.camunda.zeebe.client.api.command.CompleteJobCommandStep1;
+import io.camunda.zeebe.client.api.command.FailJobCommandStep1;
+import io.camunda.zeebe.client.api.command.ThrowErrorCommandStep1;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
 import java.util.HashMap;
 import java.util.Map;
 import org.mockito.ArgumentCaptor;
