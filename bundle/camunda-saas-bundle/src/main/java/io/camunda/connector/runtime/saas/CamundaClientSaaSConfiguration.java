a43bfa874d97a8389bf461ac9fe6a0b828a9d980
Pavel Kotelevsky <pavel.kotelevskii@camunda.com>
2025-03-25 13:28:51 +0100

    add conditional legacy property support

--- bundle/camunda-saas-bundle/src/main/java/io/camunda/connector/runtime/saas/CamundaClientSaaSConfiguration.java
+++ bundle/camunda-saas-bundle/src/main/java/io/camunda/connector/runtime/saas/CamundaClientSaaSConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.camunda.connector.runtime.saas;
 
 import io.camunda.client.CamundaClientConfiguration;
@@ -14,9 +30,15 @@ import org.apache.hc.client5.http.async.AsyncExecChainHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+/**
+ * This class configures the custom credentials provider for the Camunda client. If the default
+ * client ID and secret are not present, the client will use the internal secret provider to fetch
+ * the client ID and secret from the SaaS secret manager.
+ */
 @Configuration
 public class CamundaClientSaaSConfiguration {
 
@@ -25,45 +47,40 @@ public class CamundaClientSaaSConfiguration {
 
   private final SecretProvider internalSecretProvider;
 
-  @Value("${camunda.operate.client.authUrl}")
-  private String operateAuthUrl;
+  @Value("${camunda.client.auth.token-url}")
+  private String camundaClientTokenUrl;
 
-  public CamundaClientSaaSConfiguration(@Autowired SaaSConfiguration saaSConfiguration) {
+  @Value("${camunda.client.auth.audience}")
+  private String camundaClientAudience;
+
+  public CamundaClientSaaSConfiguration(@Autowired SaaSSecretConfiguration saaSConfiguration) {
     this.internalSecretProvider = saaSConfiguration.getInternalSecretProvider();
-  }
-
-  private static class SaaSZeebeClientConfiguration extends CamundaClientConfigurationImpl {
-
-    public SaaSZeebeClientConfiguration(
-        CamundaClientProperties camundaClientProperties,
-        JsonMapper jsonMapper,
-        List<ClientInterceptor> interceptors,
-        List<AsyncExecChainHandler> chainHandlers,
-        CamundaClientExecutorService zeebeClientExecutorService,
-        CredentialsProvider credentialsProvider) {
-      super(camundaClientProperties, jsonMapper, interceptors, chainHandlers,
-          zeebeClientExecutorService, credentialsProvider);
-    }
   }
 
   @Bean
   @Primary
-  public CamundaClientConfiguration saasZeebeClientConfiguration(
+  @Conditional(AuthPropertiesNotPresentCondition.class)
+  public CamundaClientConfiguration saasCamundaClientConfiguration(
       final CamundaClientProperties camundaClientProperties,
       final JsonMapper jsonMapper,
       final List<ClientInterceptor> interceptors,
       final List<AsyncExecChainHandler> chainHandlers,
       final CamundaClientExecutorService zeebeClientExecutorService) {
-    return new SaaSZeebeClientConfiguration(camundaClientProperties, jsonMapper,
-        interceptors, chainHandlers, zeebeClientExecutorService, credentialsProvider());
+    return new CamundaClientConfigurationImpl(
+        camundaClientProperties,
+        jsonMapper,
+        interceptors,
+        chainHandlers,
+        zeebeClientExecutorService,
+        internalConnectorsSecretCredentialsProvider());
   }
 
-  private CredentialsProvider credentialsProvider() {
+  public CredentialsProvider internalConnectorsSecretCredentialsProvider() {
     final var builder = new OAuthCredentialsProviderBuilder();
     builder.clientId(internalSecretProvider.getSecret(SECRET_NAME_CLIENT_ID));
     builder.clientSecret(internalSecretProvider.getSecret(SECRET_NAME_SECRET));
-    builder.authorizationServerUrl(operateAuthUrl);
-    builder.audience("zeebe.dev.ultrawombat.com");
+    builder.authorizationServerUrl(camundaClientTokenUrl);
+    builder.audience(camundaClientAudience);
     return builder.build();
   }
 }
