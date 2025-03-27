fef96ca565af34c5788d198eab974e08d1b65b5c
Mathias Vandaele <mathias.vandaele@camunda.com>
2025-03-27 22:58:40 +0800

    chore(etg): add the compatible runtime to all element template using the etg

--- element-template-generator/core/src/main/java/io/camunda/connector/generator/java/annotation/ElementTemplate.java
+++ element-template-generator/core/src/main/java/io/camunda/connector/generator/java/annotation/ElementTemplate.java
@@ -60,6 +60,9 @@ public @interface ElementTemplate {
    */
   String documentationRef() default "";
 
+  /** Semantic version range */
+  String runtimeSemanticVersion() default "";
+
   /**
    * Element template description. Will be displayed along with the connector name in Camunda
    * Modeler.
