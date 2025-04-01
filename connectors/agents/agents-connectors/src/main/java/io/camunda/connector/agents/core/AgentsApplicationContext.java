39259074b926bdf13c60b69f3e73fb01e3cd6c82
Mathias Geat <mathias.geat@camunda.com>
2025-04-01 09:49:45 +0200

    feat: add foundation for an agents module/connector with Spring Boot dependency integration

---
+++ connectors/agents/agents-connectors/src/main/java/io/camunda/connector/agents/core/AgentsApplicationContext.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+package io.camunda.connector.agents.core;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.camunda.client.CamundaClient;
+import java.util.Objects;
+
+public interface AgentsApplicationContext {
+  ObjectMapper objectMapper();
+
+  CamundaClient camundaClient();
+
+  record DefaultAgentsApplicationContext(ObjectMapper objectMapper, CamundaClient camundaClient)
+      implements AgentsApplicationContext {
+    public DefaultAgentsApplicationContext {
+      Objects.requireNonNull(objectMapper, "ObjectMapper cannot be null");
+      Objects.requireNonNull(camundaClient, "CamundaClient cannot be null");
+    }
+  }
+}
