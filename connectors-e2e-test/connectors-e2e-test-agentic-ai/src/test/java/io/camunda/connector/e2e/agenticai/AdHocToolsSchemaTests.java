b9a33838526fd0f9f73f0452b25b61d1da4893c4
Mathias Geat <mathias.geat@camunda.com>
2025-04-15 17:07:01 +0200

    test: add e2e tests verifying AI agent connector feedback loop and tool calling

--- connectors-e2e-test/connectors-e2e-test-agentic-ai/src/test/java/io/camunda/connector/e2e/agenticai/AdHocToolsSchemaTests.java
+++ connectors-e2e-test/connectors-e2e-test-agentic-ai/src/test/java/io/camunda/connector/e2e/agenticai/AdHocToolsSchemaTests.java
@@ -18,49 +18,24 @@ package io.camunda.connector.e2e.agenticai;
 
 import static io.camunda.process.test.api.CamundaAssert.assertThat;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.camunda.client.CamundaClient;
-import io.camunda.connector.e2e.ZeebeTest;
-import io.camunda.connector.e2e.app.TestConnectorRuntimeApplication;
-import io.camunda.process.test.api.CamundaSpringProcessTest;
-import io.camunda.zeebe.model.bpmn.Bpmn;
 import java.io.IOException;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 
-@SpringBootTest(
-    classes = {TestConnectorRuntimeApplication.class},
-    properties = {
-      "spring.main.allow-bean-definition-overriding=true",
-      "camunda.connector.agenticai.tools.cache.enabled=false"
-    },
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@CamundaSpringProcessTest
-public class AdHocToolsSchemaTests {
-
-  @Autowired private CamundaClient camundaClient;
-  @Autowired private ObjectMapper objectMapper;
-
-  @Value("classpath:agentic-ai-connectors.bpmn")
-  private Resource process;
+public class AdHocToolsSchemaTests extends BaseAgenticAiTest {
 
   @Value("classpath:expected-schema-result.json")
   private Resource expectedSchemaResult;
 
   @Test
   void loadsAdHocToolsSchema() throws IOException {
-    final var model = Bpmn.readModelFromFile(process.getFile());
-    var bpmnTest =
-        ZeebeTest.with(camundaClient)
-            .deploy(model)
-            .createInstance(Map.of("action", "resolveSchema"))
-            .waitForProcessCompletion();
+    final var zeebeTest =
+        createProcessInstance(Map.of("action", "resolveSchema")).waitForProcessCompletion();
 
     final var expectedSchemaMap = objectMapper.readValue(expectedSchemaResult.getFile(), Map.class);
-    assertThat(bpmnTest.getProcessInstanceEvent()).hasVariable("resolvedSchema", expectedSchemaMap);
+    assertThat(zeebeTest.getProcessInstanceEvent())
+        .hasVariable("resolvedSchema", expectedSchemaMap);
   }
 }
