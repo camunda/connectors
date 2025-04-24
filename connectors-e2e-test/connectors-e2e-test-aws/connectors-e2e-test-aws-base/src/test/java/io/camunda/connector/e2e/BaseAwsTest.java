b71ed257f77c776c65f2e3ba89ba504529d5e1c6
Mathias Vandaele <mathias.vandaele@camunda.com>
2025-04-24 15:10:31 +0800

    chore(integration-test): fix integration tests

--- connectors-e2e-test/connectors-e2e-test-aws/connectors-e2e-test-aws-base/src/test/java/io/camunda/connector/e2e/BaseAwsTest.java
+++ connectors-e2e-test/connectors-e2e-test-aws/connectors-e2e-test-aws-base/src/test/java/io/camunda/connector/e2e/BaseAwsTest.java
@@ -37,7 +37,7 @@ import org.junit.jupiter.api.io.TempDir;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -57,7 +57,7 @@ public abstract class BaseAwsTest {
       DockerImageName.parse("localstack/localstack");
   @TempDir File tempDir;
   @Autowired CamundaClient camundaClient;
-  @MockBean ProcessDefinitionSearch processDefinitionSearch;
+  @MockitoBean ProcessDefinitionSearch processDefinitionSearch;
 
   static LocalStackContainer localstack;
 
