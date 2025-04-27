9a3bd3edaa6a16b3262fe54c9ff8dbc66f707801
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2025-04-27 20:51:51 +0200

    feat(message): Apply proposals from pull request discussion

--- connectors-e2e-test/connectors-e2e-test-message/src/test/java/io/camunda/connector/e2e/MessageTests.java
+++ connectors-e2e-test/connectors-e2e-test-message/src/test/java/io/camunda/connector/e2e/MessageTests.java
@@ -21,6 +21,7 @@ import static io.camunda.process.test.api.CamundaAssert.assertThat;
 import io.camunda.client.CamundaClient;
 import io.camunda.client.api.response.ProcessInstanceEvent;
 import io.camunda.connector.e2e.app.TestConnectorRuntimeApplication;
+import io.camunda.connector.test.SlowTest;
 import io.camunda.process.test.api.CamundaAssert;
 import io.camunda.process.test.api.CamundaSpringProcessTest;
 import io.camunda.zeebe.model.bpmn.Bpmn;
@@ -47,6 +48,7 @@ import org.springframework.context.annotation.ComponentScan;
     },
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @CamundaSpringProcessTest
+@SlowTest
 @ExtendWith(MockitoExtension.class)
 @ComponentScan(basePackages = {"io.camunda.connector.runtime.app"})
 public class MessageTests {
