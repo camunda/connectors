0d2fea3fedf24023b618a56c8bf6ec7d0ff4d153
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2025-04-30 11:50:05 +0200

    feat(message): Publish message outbound connector (#4199)
    * feat(message): publish message outbound connector
    
    * feat(message): Reduce potential sensitive infos from logging output
    
    * feat(message): Apply proposals from pull request discussion
    
    * feat(message): Add message connector to default bundle
    
    * fix: add extended ComponentScan to the saas bundle
    
    ---------
    
    Co-authored-by: Pavel Kotelevsky <pavel.kotelevskii@camunda.com>

--- bundle/camunda-saas-bundle/src/main/java/io/camunda/connector/runtime/saas/SaaSConnectorRuntimeApplication.java
+++ bundle/camunda-saas-bundle/src/main/java/io/camunda/connector/runtime/saas/SaaSConnectorRuntimeApplication.java
@@ -18,8 +18,10 @@ package io.camunda.connector.runtime.saas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "io.camunda.connector")
 public class SaaSConnectorRuntimeApplication {
 
   public static void main(String[] args) {
