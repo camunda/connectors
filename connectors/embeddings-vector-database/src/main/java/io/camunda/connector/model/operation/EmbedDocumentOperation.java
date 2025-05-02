2785434908d52da3ce0cb6a142300aec187f8fbe
Igor Petrov <108870003+igpetrov@users.noreply.github.com>
2025-05-02 11:28:45 +0300

    feat(vector-db): added plain text support as input (#4706)
    (cherry picked from commit a17d1948b72c82a67f4c76641ac6692ec0aaa68e)

--- connectors/embeddings-vector-database/src/main/java/io/camunda/connector/model/operation/EmbedDocumentOperation.java
+++ connectors/embeddings-vector-database/src/main/java/io/camunda/connector/model/operation/EmbedDocumentOperation.java
@@ -9,22 +9,54 @@ package io.camunda.connector.model.operation;
 import static io.camunda.connector.model.operation.EmbedDocumentOperation.OPERATION_EMBED_DOCUMENT;
 
 import io.camunda.connector.generator.dsl.Property;
+import io.camunda.connector.generator.dsl.Property.FeelMode;
 import io.camunda.connector.generator.java.annotation.TemplateProperty;
+import io.camunda.connector.generator.java.annotation.TemplateProperty.PropertyCondition;
+import io.camunda.connector.generator.java.annotation.TemplateProperty.PropertyConstraints;
 import io.camunda.connector.generator.java.annotation.TemplateSubType;
 import io.camunda.connector.model.embedding.splitter.DocumentSplitter;
 import io.camunda.document.Document;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 @TemplateSubType(label = "Embed document", id = OPERATION_EMBED_DOCUMENT)
 public record EmbedDocumentOperation(
-    @NotNull
+    @NotBlank
+        @TemplateProperty(
+            group = "document",
+            id = "documentSource",
+            label = "Document source",
+            feel = Property.FeelMode.required,
+            type = TemplateProperty.PropertyType.Dropdown,
+            constraints = @TemplateProperty.PropertyConstraints(notEmpty = true),
+            description = "Whether you want to embed a Camunda document file or plain text",
+            defaultValue = "CamundaDocument")
+        EmbedDocumentSource documentSource,
+    @NotBlank
+        @TemplateProperty(
+            group = "document",
+            id = "documentSourceFromProcessVariable",
+            label = "Plain text to embed",
+            feel = FeelMode.optional,
+            constraints = @PropertyConstraints(notEmpty = true),
+            condition =
+                @PropertyCondition(
+                    property = "vectorDatabaseConnectorOperation.documentSource",
+                    equals = "PlainText"))
+        String documentSourceFromProcessVariable,
+    @NotBlank
         @TemplateProperty(
             label = "Documents",
             group = "document",
             id = "newDocuments",
             feel = Property.FeelMode.required,
-            binding = @TemplateProperty.PropertyBinding(name = "newDocuments"))
+            binding = @TemplateProperty.PropertyBinding(name = "newDocuments"),
+            constraints = @PropertyConstraints(notEmpty = true),
+            condition =
+                @PropertyCondition(
+                    property = "vectorDatabaseConnectorOperation.documentSource",
+                    equals = "CamundaDocument"))
         List<Document> newDocuments,
     @NotNull DocumentSplitter documentSplitter)
     implements VectorDatabaseConnectorOperation {
