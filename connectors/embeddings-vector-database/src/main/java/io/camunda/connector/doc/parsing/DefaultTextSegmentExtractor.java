3cecf0e31164f153f7f0478e810c82fcfa69381b
Igor Petrov <108870003+igpetrov@users.noreply.github.com>
2025-05-02 11:28:45 +0300

    feat(vector-db): added plain text support as input (#4706)

--- connectors/embeddings-vector-database/src/main/java/io/camunda/connector/doc/parsing/DefaultTextSegmentExtractor.java
+++ connectors/embeddings-vector-database/src/main/java/io/camunda/connector/doc/parsing/DefaultTextSegmentExtractor.java
@@ -9,14 +9,20 @@ package io.camunda.connector.doc.parsing;
 import dev.langchain4j.data.document.DocumentLoader;
 import dev.langchain4j.data.document.DocumentSource;
 import dev.langchain4j.data.document.Metadata;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
 import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
 import dev.langchain4j.data.segment.TextSegment;
 import io.camunda.connector.doc.splitting.DefaultDocumentSplitterFactory;
 import io.camunda.connector.model.EmbeddingsVectorDBRequest;
 import io.camunda.connector.model.operation.EmbedDocumentOperation;
+import io.camunda.connector.model.operation.EmbedDocumentSource;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import org.apache.commons.io.IOUtils;
+import org.apache.tika.metadata.HttpHeaders;
+import org.apache.tika.mime.MediaType;
 
 public class DefaultTextSegmentExtractor {
 
@@ -36,27 +42,46 @@ public class DefaultTextSegmentExtractor {
     final var embedRequest = (EmbedDocumentOperation) request.vectorDatabaseConnectorOperation();
     final var splitter =
         documentSplitterFactory.createDocumentSplitter(embedRequest.documentSplitter());
-    return embedRequest.newDocuments().stream()
-        .map(
-            camundaDoc ->
-                DocumentLoader.load(
-                    new DocumentSource() {
-                      @Override
-                      public InputStream inputStream() {
-                        return camundaDoc.asInputStream();
-                      }
+    if (embedRequest.documentSource() == EmbedDocumentSource.CamundaDocument) {
+      return embedRequest.newDocuments().stream()
+          .map(
+              camundaDoc ->
+                  DocumentLoader.load(
+                      new DocumentSource() {
+                        @Override
+                        public InputStream inputStream() {
+                          return camundaDoc.asInputStream();
+                        }
 
-                      @Override
-                      public Metadata metadata() {
-                        return Metadata.from(
-                            Map.of(FILENAME_METADATA_KEY, camundaDoc.metadata().getFileName()));
-                      }
-                    },
-                    // Apache Tika includes metadata, such as
-                    // real content type, encoding
-                    new ApacheTikaDocumentParser(true)))
-        .map(splitter::split)
-        .flatMap(List::stream)
-        .toList();
+                        @Override
+                        public Metadata metadata() {
+                          return Metadata.from(
+                              Map.of(FILENAME_METADATA_KEY, camundaDoc.metadata().getFileName()));
+                        }
+                      },
+                      // Apache Tika includes metadata, such as
+                      // real content type, encoding
+                      new ApacheTikaDocumentParser(true)))
+          .map(splitter::split)
+          .flatMap(List::stream)
+          .toList();
+    } else {
+      return splitter.split(
+          DocumentLoader.load(
+              new DocumentSource() {
+
+                @Override
+                public InputStream inputStream() throws IOException {
+                  return IOUtils.toInputStream(
+                      embedRequest.documentSourceFromProcessVariable(), "UTF-8");
+                }
+
+                @Override
+                public Metadata metadata() {
+                  return Metadata.from(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN.toString());
+                }
+              },
+              new TextDocumentParser()));
+    }
   }
 }
