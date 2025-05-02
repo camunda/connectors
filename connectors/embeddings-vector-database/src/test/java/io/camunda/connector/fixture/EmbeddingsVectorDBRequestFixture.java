a17d1948b72c82a67f4c76641ac6692ec0aaa68e
Igor Petrov <108870003+igpetrov@users.noreply.github.com>
2025-05-02 11:28:45 +0300

    feat(vector-db): added plain text support as input (#4706)

--- connectors/embeddings-vector-database/src/test/java/io/camunda/connector/fixture/EmbeddingsVectorDBRequestFixture.java
+++ connectors/embeddings-vector-database/src/test/java/io/camunda/connector/fixture/EmbeddingsVectorDBRequestFixture.java
@@ -8,10 +8,16 @@ package io.camunda.connector.fixture;
 
 import io.camunda.connector.model.EmbeddingsVectorDBRequest;
 import io.camunda.connector.model.operation.EmbedDocumentOperation;
+import io.camunda.connector.model.operation.EmbedDocumentSource;
 import io.camunda.connector.model.operation.RetrieveDocumentOperation;
 import java.util.List;
 
 public class EmbeddingsVectorDBRequestFixture {
+
+  private static final String CONVERSATION =
+      """
+[{"user":"operator123","message":"Hey how are you?"},{"user":"customer9000","message":"Yes I am fine, just busy with RAG connector"}]
+""";
 
   public static EmbeddingsVectorDBRequest createDefaultRetrieve() {
     final var operation = new RetrieveDocumentOperation("What is RAG?", 5, 0.6);
@@ -26,6 +32,8 @@ public class EmbeddingsVectorDBRequestFixture {
   public static EmbeddingsVectorDBRequest createDefaultEmbedOperation() {
     final var operation =
         new EmbedDocumentOperation(
+            EmbedDocumentSource.CamundaDocument,
+            null,
             List.of(CamundaDocumentFixture.inMemoryTxtDocument()),
             DocumentSplitterFixture.noopDocumentSplitter());
     final var embeddingModeProvider =
@@ -39,6 +47,23 @@ public class EmbeddingsVectorDBRequestFixture {
   public static EmbeddingsVectorDBRequest createEmbedOperationWithPdfFile() {
     final var operation =
         new EmbedDocumentOperation(
+            EmbedDocumentSource.CamundaDocument,
+            null,
+            List.of(CamundaDocumentFixture.inMemoryPdfDocument()),
+            DocumentSplitterFixture.noopDocumentSplitter());
+    final var embeddingModeProvider =
+        EmbeddingModelProviderFixture.createDefaultBedrockEmbeddingModel();
+    final var vectorStore = EmbeddingsVectorStoreFixture.createAmazonManagedOpenVectorStore();
+    final var request =
+        new EmbeddingsVectorDBRequest(operation, embeddingModeProvider, vectorStore);
+    return request;
+  }
+
+  public static EmbeddingsVectorDBRequest createEmbedOperationWithPlainText() {
+    final var operation =
+        new EmbedDocumentOperation(
+            EmbedDocumentSource.PlainText,
+            CONVERSATION,
             List.of(CamundaDocumentFixture.inMemoryPdfDocument()),
             DocumentSplitterFixture.noopDocumentSplitter());
     final var embeddingModeProvider =
