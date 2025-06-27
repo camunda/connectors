65fc94a04c8ecea3be50957502c8369e58ca5839
Mathias Vandaele <mathias.vandaele@camunda.com>
2025-06-27 10:24:55 +0200

    feature(inbound-email-connector): the initial connection now has a retry mechanism for email inbound connector 2

--- connectors/email/src/main/java/io/camunda/connector/email/exception/EmailConnectorException.java
+++ connectors/email/src/main/java/io/camunda/connector/email/exception/EmailConnectorException.java
@@ -1,7 +1,15 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
 package io.camunda.connector.email.exception;
 
 import jakarta.mail.MessagingException;
 
 public class EmailConnectorException extends RuntimeException {
-  public EmailConnectorException(MessagingException e) {}
+  public EmailConnectorException(MessagingException e) {
+    super(e);
+  }
 }
