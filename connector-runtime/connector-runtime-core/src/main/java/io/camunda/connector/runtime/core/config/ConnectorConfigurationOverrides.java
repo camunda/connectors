5803fa13e6b71f9e73afd007e61ce7d949989b6e
Mathias Geat <mathias.geat@camunda.com>
2025-06-30 17:03:36 +0200

    refactor: extract connector registration overrides (type, timeout) to a reusable ConnectorConfigurationOverrides class

---
+++ connector-runtime/connector-runtime-core/src/main/java/io/camunda/connector/runtime/core/config/ConnectorConfigurationOverrides.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.connector.runtime.core.config;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public class ConnectorConfigurationOverrides {
+  private final String normalizedConnectorName;
+  private final Function<String, String> propertySource;
+
+  public ConnectorConfigurationOverrides(
+      String connectorName, Function<String, String> propertySource) {
+    this.normalizedConnectorName = toNormalizedConnectorName(connectorName);
+    this.propertySource = propertySource;
+  }
+
+  public String typeOverride() {
+    return getProperty("CONNECTOR_" + normalizedConnectorName + "_TYPE");
+  }
+
+  public Long timeoutOverride() {
+    return Optional.ofNullable(getProperty("CONNECTOR_" + normalizedConnectorName + "_TIMEOUT"))
+        .map(Long::parseLong)
+        .orElse(null);
+  }
+
+  private String getProperty(String propertyName) {
+    return propertySource.apply(propertyName);
+  }
+
+  private String toNormalizedConnectorName(final String connectorName) {
+    return connectorName.trim().replaceAll("[^a-zA-Z0-9_ ]", "").replaceAll(" ", "_").toUpperCase();
+  }
+}
