7ea2225bdee1c6dcc8e0a138f6f9da632e3080c1
Mathias Vandaele <mathias.vandaele@camunda.com>
2025-08-07 10:56:33 +0200

    fix(polling): remove @FEEL as we revert ET to previous state

--- connectors/http/http-base/src/main/java/io/camunda/connector/http/base/model/HttpCommonRequest.java
+++ connectors/http/http-base/src/main/java/io/camunda/connector/http/base/model/HttpCommonRequest.java
@@ -100,7 +100,6 @@ public class HttpCommonRequest {
       defaultValueType = TemplateProperty.DefaultValueType.Boolean,
       defaultValue = "false",
       description = "Store the response as a document in the document store")
-  @FEEL
   private boolean storeResponse;
 
   @TemplateProperty(
