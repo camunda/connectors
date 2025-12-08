c93713f527d26f9d069884de002985e04f8ecc43
Mathias Vandaele <mathias.vandaele@camunda.com>
2025-12-08 12:08:08 +0100

    fix: remove unused dependencies

--- connectors/email/src/test/java/io/camunda/connector/email/integration/InboundRecoveringTest.java
+++ connectors/email/src/test/java/io/camunda/connector/email/integration/InboundRecoveringTest.java
@@ -30,7 +30,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
