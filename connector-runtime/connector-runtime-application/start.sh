6319930cf0b5b2eac2be1f5a13411046bbb80d5d
Pavel Kotelevskii <pavel.kotelevskii@camunda.com>
2025-12-09 11:47:59 +0100

    fix(metrics): add a default java option to the image to fix metrics (#5890)
    * fix(metrics): add a default java option to the image
    
    This is needed for micrometer metrics to work
    correctly with virtual thread executors
    
    * fix(metrics): replicate the change in the other start.sh

--- connector-runtime/connector-runtime-application/start.sh
+++ connector-runtime/connector-runtime-application/start.sh
@@ -12,6 +12,9 @@ if [[ -n "${JAVAX_NET_SSL_TRUSTSTOREPASSWORD}" ]]; then
   JAVA_OPTS="${JAVA_OPTS} -Djavax.net.ssl.trustStorePassword=${JAVAX_NET_SSL_TRUSTSTOREPASSWORD}"
 fi
 
+# For thread pool metrics with virtual threads
+JAVA_OPTS="${JAVA_OPTS} --add-opens=java.base/java.util.concurrent=ALL-UNNAMED"
+
 if [[ -n ${DEBUG_JVM_PRINT_JAVA_OPTS} ]]; then
   echo "Applied JVM options: $JAVA_OPTS"
 fi
