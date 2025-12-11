3ef70a217b2ebe589bc3c7586a8a0782033fb5a8
Jonathan Roques <jonathan.roques@camunda.com>
2025-12-11 16:16:50 +0100

    refactor: bring back the JacksonModuleFeelFunction in the outbound connectors config

--- connector-runtime/spring-boot-starter-camunda-connectors/src/main/java/io/camunda/connector/runtime/OutboundConnectorsAutoConfiguration.java
+++ connector-runtime/spring-boot-starter-camunda-connectors/src/main/java/io/camunda/connector/runtime/OutboundConnectorsAutoConfiguration.java
@@ -23,6 +23,7 @@ import io.camunda.connector.api.document.DocumentFactory;
 import io.camunda.connector.document.jackson.JacksonModuleDocumentDeserializer;
 import io.camunda.connector.document.jackson.JacksonModuleDocumentDeserializer.DocumentModuleSettings;
 import io.camunda.connector.document.jackson.JacksonModuleDocumentSerializer;
+import io.camunda.connector.feel.jackson.JacksonModuleFeelFunction;
 import io.camunda.connector.jackson.ConnectorsObjectMapperSupplier;
 import io.camunda.connector.runtime.core.intrinsic.DefaultIntrinsicFunctionExecutor;
 import io.camunda.connector.runtime.outbound.OutboundConnectorRuntimeConfiguration;
@@ -75,6 +76,8 @@ public class OutboundConnectorsAutoConfiguration {
     // pointer of the object mapper
     // we are overloading
     return copy.registerModules(
-        jacksonModuleDocumentDeserializer, new JacksonModuleDocumentSerializer());
+        jacksonModuleDocumentDeserializer,
+        new JacksonModuleFeelFunction(),
+        new JacksonModuleDocumentSerializer());
   }
 }
