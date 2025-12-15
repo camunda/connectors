b0932dfebcb9636df439f84e6f5d57863030766d
Stefan Zabka <stefan.zabka@camunda.com>
2025-12-15 13:15:31 +0100

    More fixups

--- connectors/microsoft/email-inbound/src/main/java/io/camunda/connector/azure/email/model/config/Folder.java
+++ connectors/microsoft/email-inbound/src/main/java/io/camunda/connector/azure/email/model/config/Folder.java
@@ -6,6 +6,7 @@
  */
 package io.camunda.connector.azure.email.model.config;
 
+import io.camunda.connector.api.annotation.FEEL;
 import io.camunda.connector.generator.dsl.Property;
 import io.camunda.connector.generator.java.annotation.TemplateProperty;
 
@@ -14,12 +15,17 @@ public record Folder(
             label = "Folder Name/Folder ID",
             feel = Property.FeelMode.optional,
             // TODO: Add link
+            defaultValue = "inbox",
             description =
                 "The folder name or folder ID. Folder names must be unique. Consider using the well-known folder IDs described here")
         String folderName,
     @TemplateProperty(
             label = "Specified folder ID",
             description = "Did you specify a folder ID?",
+            defaultValueType = TemplateProperty.DefaultValueType.Boolean,
+            defaultValue = "true",
+            feel = Property.FeelMode.optional,
             tooltip =
                 "To prevent name collisions, you can instead specify the folder ID. <a href='https://learn.microsoft.com/en-us/graph/api/resources/mailfolder?view=graph-rest-1.0#properties' target='_blank'> See the folder Properties described in the API</a> ")
-        boolean isFolderId) {}
+        @FEEL
+    boolean isFolderId) {}
