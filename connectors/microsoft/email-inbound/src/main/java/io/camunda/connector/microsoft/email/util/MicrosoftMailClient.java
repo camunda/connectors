53f9b3fff4050e6a60ed4fb74d303561c2f5bdf1
Stefan Zabka <stefan.zabka@camunda.com>
2025-12-15 13:15:31 +0100

    refactor(MailClient): extract mail client

--- connectors/microsoft/email-inbound/src/main/java/io/camunda/connector/microsoft/email/util/MicrosoftMailClient.java
+++ connectors/microsoft/email-inbound/src/main/java/io/camunda/connector/microsoft/email/util/MicrosoftMailClient.java
@@ -8,12 +8,18 @@ package io.camunda.connector.microsoft.email.util;
 
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
+import com.microsoft.graph.models.Message;
 import com.microsoft.graph.serviceclient.GraphServiceClient;
 import com.microsoft.graph.users.item.UserItemRequestBuilder;
+import com.microsoft.graph.users.item.messages.item.MessageItemRequestBuilder;
+import com.microsoft.graph.users.item.messages.item.move.MovePostRequestBody;
+import io.camunda.connector.api.document.Document;
 import io.camunda.connector.api.error.ConnectorException;
+import io.camunda.connector.microsoft.email.model.config.Folder;
 import io.camunda.connector.microsoft.email.model.config.MsInboundEmailProperties;
 import io.camunda.connector.microsoft.email.model.output.EmailMessage;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class MicrosoftMailClient implements MailClient {
 
@@ -38,36 +44,65 @@ public class MicrosoftMailClient implements MailClient {
   }
 
   @Override
-  public String getFolderIdByFolderName(String folderName) {
+  public String getFolderId(Folder folder) {
+    if (folder.isFolderId()) {
+      return folder.folderName();
+    }
     // TODO: This technically allows people to sneak in arbitrary filters
     // But is this an injection scenario we care about?
     var resp =
         graphClient
             .mailFolders()
-            .get(c -> c.queryParameters.filter = "displayName eq '" + folderName + "'");
+            .get(c -> c.queryParameters.filter = "displayName eq '" + folder.folderName() + "'");
     // TODO: Should we specify the name of the user mailbox in the error message?
     if (resp.getValue().size() > 1) {
-      throw new ConnectorException("Folder name " + folderName + " matches more than one folder.");
+      throw new ConnectorException(
+          "Folder name " + folder.folderName() + " matches more than one folder.");
     }
     if (resp.getValue().isEmpty()) {
-      throw new ConnectorException("No folder with name " + folderName + " could be found.");
+      throw new ConnectorException(
+          "No folder with name " + folder.folderName() + " could be found.");
     }
     return resp.getValue().getFirst().getId();
   }
 
   @Override
-  public List<EmailMessage> getMessages(String filterString) {
-    return List.of();
+  public String getMessages(
+      String deltaToken, String filterString, Consumer<EmailMessage> handler) {
+    return null;
+  }
+
+  private MessageItemRequestBuilder constructCommonMessage(EmailMessage msg) {
+    return graphClient.messages().byMessageId(msg.id());
   }
 
   @Override
-  public void deleteMessage(EmailMessage msg, boolean force) {}
+  public void deleteMessage(EmailMessage msg, boolean force) {
+    if (force) constructCommonMessage(msg).permanentDelete();
+    else {
+      constructCommonMessage(msg).delete();
+    }
+  }
 
   @Override
-  public void markMessageRead(EmailMessage msg) {}
+  public void markMessageRead(EmailMessage msg) {
+    Message updatedMessage = new Message();
+    updatedMessage.setIsRead(true);
+    constructCommonMessage(msg).patch(updatedMessage);
+  }
 
   @Override
-  public void moveMessage(EmailMessage msg, String targetFolderId) {}
+  public void moveMessage(EmailMessage msg, Folder folder) {
+    String folderId = getFolderId(folder);
+    var body = new MovePostRequestBody();
+    body.setDestinationId(folderId);
+    constructCommonMessage(msg).move().post(body);
+  }
+
+  @Override
+  public List<Document> fetchAttachments(EmailMessage msg) {
+    return List.of();
+  }
 
   @Deprecated
   public UserItemRequestBuilder getClient() {
