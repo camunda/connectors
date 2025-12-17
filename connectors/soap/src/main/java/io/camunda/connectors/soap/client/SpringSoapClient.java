7eff50d83333bc397d88de486c34d6833ba85940
Alessandro Tuttobene <alessandro.tuttobene@camunda.com>
2025-12-17 10:40:42 +0100

    chore(soap-connector): add null check on encryption parts

--- connectors/soap/src/main/java/io/camunda/connectors/soap/client/SpringSoapClient.java
+++ connectors/soap/src/main/java/io/camunda/connectors/soap/client/SpringSoapClient.java
@@ -191,10 +191,12 @@ public class SpringSoapClient implements SoapClient {
         securityInterceptor.setSecurementSignatureCrypto(crypto);
         securityInterceptor.setValidationSignatureCrypto(crypto);
       }
-      securityInterceptor.setSecurementSignatureParts(
-          signature.encryptionParts().stream()
-              .map(part -> String.format("{}{%s}%s;", part.namespace(), part.localName()))
-              .collect(Collectors.joining("")));
+      if (signature.encryptionParts() != null && !signature.encryptionParts().isEmpty()) {
+        securityInterceptor.setSecurementSignatureParts(
+            signature.encryptionParts().stream()
+                .map(part -> String.format("{}{%s}%s;", part.namespace(), part.localName()))
+                .collect(Collectors.joining("")));
+      }
       securityInterceptor.setSecurementMustUnderstand(true);
       ofNullable(signature.digestAlgorithm())
           .ifPresent(securityInterceptor::setSecurementSignatureDigestAlgorithm);
