eb9e27e8bc38274600d60a6f4ebac2afb41bb468
Dmitri Nikonov <dmitri.nikonov@camunda.com>
2025-12-23 12:12:25 +0100

    chore: Refactor mcp tools filter to allow injection of further filters

--- connectors/agentic-ai/src/main/java/io/camunda/connector/agenticai/mcp/client/framework/langchain4j/rpc/Langchain4JMcpClientExecutor.java
+++ connectors/agentic-ai/src/main/java/io/camunda/connector/agenticai/mcp/client/framework/langchain4j/rpc/Langchain4JMcpClientExecutor.java
@@ -9,7 +9,7 @@ package io.camunda.connector.agenticai.mcp.client.framework.langchain4j.rpc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.langchain4j.mcp.client.McpClient;
 import io.camunda.connector.agenticai.aiagent.framework.langchain4j.tool.ToolSpecificationConverter;
-import io.camunda.connector.agenticai.mcp.client.McpToolNameFilter;
+import io.camunda.connector.agenticai.mcp.client.filters.FilterOptions;
 import io.camunda.connector.agenticai.mcp.client.model.McpClientOperation;
 import io.camunda.connector.agenticai.mcp.client.model.McpClientOperation.McpClientCallToolOperation;
 import io.camunda.connector.agenticai.mcp.client.model.McpClientOperation.McpClientListToolsOperation;
@@ -26,12 +26,14 @@ public class Langchain4JMcpClientExecutor {
   }
 
   public McpClientResult execute(
-      McpClient client, McpClientOperation operation, McpToolNameFilter toolNameFilter) {
+      McpClient client, McpClientOperation operation, FilterOptions filterOptions) {
     return switch (operation) {
       case McpClientListToolsOperation ignored ->
-          new ListToolsRequest(toolSpecificationConverter).execute(client, toolNameFilter);
+          new ListToolsRequest(toolSpecificationConverter)
+              .execute(client, filterOptions.toolFilters());
       case McpClientCallToolOperation callTool ->
-          new ToolExecutionRequest(objectMapper).execute(client, toolNameFilter, callTool.params());
+          new ToolExecutionRequest(objectMapper)
+              .execute(client, filterOptions.toolFilters(), callTool.params());
       case McpClientOperation.McpClientListResourcesOperation ignored ->
           new ListResourcesRequest().execute(client);
     };
