9c8ab51f4c74d29b2c4d5882e293366aecb65523
Stefan Zabka <stefan.zabka@camunda.com>
2026-01-14 16:37:38 +0100

    docs(ms-email-inbound): clarify unsupported attachment types are intentional

--- connectors/microsoft/email-inbound/src/main/java/io/camunda/connector/microsoft/email/util/MicrosoftMailClient.java
+++ connectors/microsoft/email-inbound/src/main/java/io/camunda/connector/microsoft/email/util/MicrosoftMailClient.java
@@ -175,7 +175,7 @@ public class MicrosoftMailClient implements MailClient {
                     .contentType(file.getContentType())
                     .build()));
       }
-      // TODO: log unsupported attachment
+      // Note: ItemAttachment and ReferenceAttachment are intentionally not supported
     }
     return docs;
   }
