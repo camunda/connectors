64fe34a1c5150fd174524515de21ca6fdf8be41d
Mathias Vandaele <mathias.vandaele@camunda.com>
2026-01-16 16:12:43 +0100

    test(email): splotless apply

--- connectors/email/src/test/java/io/camunda/connector/email/integration/InboundRecoveringTest.java
+++ connectors/email/src/test/java/io/camunda/connector/email/integration/InboundRecoveringTest.java
@@ -6,6 +6,10 @@
  */
 package io.camunda.connector.email.integration;
 
+import static org.awaitility.Awaitility.await;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 import io.camunda.connector.api.inbound.ActivationCheckResult;
 import io.camunda.connector.api.inbound.CorrelationResult;
 import io.camunda.connector.api.inbound.Health;
@@ -19,194 +23,192 @@ import io.camunda.connector.email.inbound.model.EmailListenerConfig;
 import io.camunda.connector.email.inbound.model.HandlingStrategy;
 import io.camunda.connector.email.inbound.model.PollUnseen;
 import io.camunda.connector.email.response.ReadEmailResponse;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
-
-import static org.awaitility.Awaitility.await;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
 public class InboundRecoveringTest extends BaseEmailTest {
 
-    private final int PROXY_PORT = 22223;
-    JakartaEmailListener jakartaEmailListener = new JakartaEmailListener();
+  private final int PROXY_PORT = 22223;
+  JakartaEmailListener jakartaEmailListener = new JakartaEmailListener();
 
-    @BeforeEach
-    public void beforeEach() {
-        Mockito.clearAllCaches();
-        super.reset();
+  @BeforeEach
+  public void beforeEach() {
+    Mockito.clearAllCaches();
+    super.reset();
+  }
+
+  @AfterEach
+  public void afterEach() {
+    jakartaEmailListener.stopListener();
+  }
+
+  @RepeatedTest(
+      value = 5,
+      name = "{displayName} - repetition {currentRepetition} of {totalRepetitions}",
+      failureThreshold = 1)
+  public void pollingManagerBreaksAndRecoverAfterServerNotResponding() {
+    try (ImapServerProxy proxyImap =
+        new ImapServerProxy(
+            PROXY_PORT, "localhost", Integer.parseInt(super.getUnsecureImapPort()))) {
+      InboundConnectorContext inboundConnectorContext = mock(InboundConnectorContext.class);
+
+      EmailInboundConnectorProperties emailInboundConnectorProperties =
+          new EmailInboundConnectorProperties(
+              new SimpleAuthentication("test@camunda.com", "password"),
+              new EmailListenerConfig(
+                  new ImapConfig("localhost", PROXY_PORT, CryptographicProtocol.NONE),
+                  "",
+                  Duration.of(1, ChronoUnit.SECONDS),
+                  new PollUnseen(HandlingStrategy.READ, "")));
+
+      doNothing().when(inboundConnectorContext).log(any(Consumer.class));
+      when(inboundConnectorContext.bindProperties(EmailInboundConnectorProperties.class))
+          .thenReturn(emailInboundConnectorProperties);
+      when(inboundConnectorContext.canActivate(any()))
+          .thenReturn(new ActivationCheckResult.Success.CanActivate(null));
+      when(inboundConnectorContext.correlate(any()))
+          .thenReturn(new CorrelationResult.Success.ProcessInstanceCreated(null, null, null));
+
+      proxyImap.setOkProxy();
+      this.jakartaEmailListener.startListener(inboundConnectorContext);
+
+      await()
+          .atMost(5, TimeUnit.SECONDS)
+          .untilAsserted(
+              () ->
+                  // We want to check it was called once at startup and once at poll
+                  verify(inboundConnectorContext, atLeast(2))
+                      .reportHealth(argThat(health -> health.getStatus() == Health.Status.UP)));
+
+      // It will make the proxy switch to a kill mode, will send BYE all the time to the server,
+      // closing the connection
+      proxyImap.cutConnection();
+
+      await()
+          .atMost(5, TimeUnit.SECONDS)
+          .untilAsserted(
+              () ->
+                  // We want to check health was down for at least 1 times
+                  verify(inboundConnectorContext, atLeast(1))
+                      .reportHealth(argThat(health -> health.getStatus() == Health.Status.DOWN)));
+
+      Mockito.clearInvocations(inboundConnectorContext);
+      proxyImap.setOkProxy();
+
+      super.sendEmail("camunda@test.com", "Subject", "Content");
+
+      await()
+          .atMost(5, TimeUnit.SECONDS)
+          .untilAsserted(
+              () ->
+                  verify(inboundConnectorContext, atLeast(1))
+                      .correlate(
+                          argThat(
+                              correlationRequest -> {
+                                ReadEmailResponse response =
+                                    ((ReadEmailResponse) correlationRequest.getVariables());
+                                return response.plainTextBody().equals("Content")
+                                    && response.subject().equals("Subject");
+                              })));
+
+      await()
+          .atMost(5, TimeUnit.SECONDS)
+          .untilAsserted(
+              () ->
+                  verify(inboundConnectorContext, atLeast(1))
+                      .reportHealth(argThat(health -> health.getStatus() == Health.Status.UP)));
+
+    } catch (Exception e) {
+      if (e instanceof IllegalStateException
+          && e.getCause().getCause() instanceof java.net.SocketException) {
+        // This exception is expected because the proxy may cut the connection while the client
+        // is using it
+        return;
+      }
+      throw new RuntimeException(e);
     }
+  }
 
-    @AfterEach
-    public void afterEach() {
-        jakartaEmailListener.stopListener();
+  @Test
+  public void pollingManagerBreaksAndRecoverAfterRuntimeErrorFromTheRuntime() {
+    try (ImapServerProxy proxyImap =
+        new ImapServerProxy(
+            PROXY_PORT, "localhost", Integer.parseInt(super.getUnsecureImapPort()))) {
+      InboundConnectorContext inboundConnectorContext = mock(InboundConnectorContext.class);
+
+      EmailInboundConnectorProperties emailInboundConnectorProperties =
+          new EmailInboundConnectorProperties(
+              new SimpleAuthentication("test@camunda.com", "password"),
+              new EmailListenerConfig(
+                  new ImapConfig("localhost", PROXY_PORT, CryptographicProtocol.NONE),
+                  "",
+                  Duration.of(1, ChronoUnit.SECONDS),
+                  new PollUnseen(HandlingStrategy.READ, "")));
+
+      doNothing().when(inboundConnectorContext).log(any(Consumer.class));
+      when(inboundConnectorContext.bindProperties(EmailInboundConnectorProperties.class))
+          .thenReturn(emailInboundConnectorProperties);
+      when(inboundConnectorContext.canActivate(any()))
+          .thenReturn(new ActivationCheckResult.Success.CanActivate(null));
+      doThrow(new RuntimeException()).when(inboundConnectorContext).correlate(any());
+
+      proxyImap.setOkProxy();
+      this.jakartaEmailListener.startListener(inboundConnectorContext);
+
+      await()
+          .atMost(5, TimeUnit.SECONDS)
+          .untilAsserted(
+              () ->
+                  // We want to check it was called once at startup and once at poll
+                  verify(inboundConnectorContext, atLeast(2))
+                      .reportHealth(argThat(health -> health.getStatus() == Health.Status.UP)));
+
+      super.sendEmail("camunda@test.com", "Subject", "Content");
+
+      await()
+          .atMost(10, TimeUnit.SECONDS)
+          .untilAsserted(
+              () ->
+                  // We want to check health was down for at least 1 times
+                  verify(inboundConnectorContext, atLeast(1))
+                      .reportHealth(argThat(health -> health.getStatus() == Health.Status.DOWN)));
+
+      Mockito.clearInvocations(inboundConnectorContext);
+
+      doReturn(new CorrelationResult.Success.ProcessInstanceCreated(null, null, null))
+          .when(inboundConnectorContext)
+          .correlate(any());
+
+      await()
+          .atMost(5, TimeUnit.SECONDS)
+          .untilAsserted(
+              () ->
+                  verify(inboundConnectorContext, atLeast(1))
+                      .correlate(
+                          argThat(
+                              correlationRequest -> {
+                                ReadEmailResponse response =
+                                    ((ReadEmailResponse) correlationRequest.getVariables());
+                                return response.plainTextBody().equals("Content")
+                                    && response.subject().equals("Subject");
+                              })));
+
+      await()
+          .atMost(5, TimeUnit.SECONDS)
+          .untilAsserted(
+              () ->
+                  verify(inboundConnectorContext, atLeast(1))
+                      .reportHealth(argThat(health -> health.getStatus() == Health.Status.UP)));
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
-
-    @RepeatedTest(value = 5, name = "{displayName} - repetition {currentRepetition} of {totalRepetitions}", failureThreshold = 1)
-    public void pollingManagerBreaksAndRecoverAfterServerNotResponding() {
-        try (ImapServerProxy proxyImap =
-                     new ImapServerProxy(
-                             PROXY_PORT, "localhost", Integer.parseInt(super.getUnsecureImapPort()))) {
-            InboundConnectorContext inboundConnectorContext = mock(InboundConnectorContext.class);
-
-            EmailInboundConnectorProperties emailInboundConnectorProperties =
-                    new EmailInboundConnectorProperties(
-                            new SimpleAuthentication("test@camunda.com", "password"),
-                            new EmailListenerConfig(
-                                    new ImapConfig("localhost", PROXY_PORT, CryptographicProtocol.NONE),
-                                    "",
-                                    Duration.of(1, ChronoUnit.SECONDS),
-                                    new PollUnseen(HandlingStrategy.READ, "")));
-
-            doNothing().when(inboundConnectorContext).log(any(Consumer.class));
-            when(inboundConnectorContext.bindProperties(EmailInboundConnectorProperties.class))
-                    .thenReturn(emailInboundConnectorProperties);
-            when(inboundConnectorContext.canActivate(any()))
-                    .thenReturn(new ActivationCheckResult.Success.CanActivate(null));
-            when(inboundConnectorContext.correlate(any()))
-                    .thenReturn(new CorrelationResult.Success.ProcessInstanceCreated(null, null, null));
-
-            proxyImap.setOkProxy();
-            this.jakartaEmailListener.startListener(inboundConnectorContext);
-
-            await()
-                    .atMost(5, TimeUnit.SECONDS)
-                    .untilAsserted(
-                            () ->
-                                    // We want to check it was called once at startup and once at poll
-                                    verify(inboundConnectorContext, atLeast(2))
-                                            .reportHealth(argThat(health -> health.getStatus() == Health.Status.UP)));
-
-            // It will make the proxy switch to a kill mode, will send BYE all the time to the server,
-            // closing the connection
-            proxyImap.cutConnection();
-
-            await()
-                    .atMost(5, TimeUnit.SECONDS)
-                    .untilAsserted(
-                            () ->
-                                    // We want to check health was down for at least 1 times
-                                    verify(inboundConnectorContext, atLeast(1))
-                                            .reportHealth(argThat(health -> health.getStatus() == Health.Status.DOWN)));
-
-            Mockito.clearInvocations(inboundConnectorContext);
-            proxyImap.setOkProxy();
-
-            super.sendEmail("camunda@test.com", "Subject", "Content");
-
-            await()
-                    .atMost(5, TimeUnit.SECONDS)
-                    .untilAsserted(
-                            () ->
-                                    verify(inboundConnectorContext, atLeast(1))
-                                            .correlate(
-                                                    argThat(
-                                                            correlationRequest -> {
-                                                                ReadEmailResponse response =
-                                                                        ((ReadEmailResponse) correlationRequest.getVariables());
-                                                                return response.plainTextBody().equals("Content")
-                                                                        && response.subject().equals("Subject");
-                                                            })));
-
-            await()
-                    .atMost(5, TimeUnit.SECONDS)
-                    .untilAsserted(
-                            () ->
-                                    verify(inboundConnectorContext, atLeast(1))
-                                            .reportHealth(argThat(health -> health.getStatus() == Health.Status.UP)));
-
-        } catch (Exception e) {
-            if (e instanceof IllegalStateException
-                    && e.getCause().getCause() instanceof java.net.SocketException) {
-                // This exception is expected because the proxy may cut the connection while the client
-                // is using it
-                return;
-            }
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    public void pollingManagerBreaksAndRecoverAfterRuntimeErrorFromTheRuntime() {
-        try (ImapServerProxy proxyImap =
-                     new ImapServerProxy(
-                             PROXY_PORT, "localhost", Integer.parseInt(super.getUnsecureImapPort()))) {
-            InboundConnectorContext inboundConnectorContext = mock(InboundConnectorContext.class);
-
-            EmailInboundConnectorProperties emailInboundConnectorProperties =
-                    new EmailInboundConnectorProperties(
-                            new SimpleAuthentication("test@camunda.com", "password"),
-                            new EmailListenerConfig(
-                                    new ImapConfig("localhost", PROXY_PORT, CryptographicProtocol.NONE),
-                                    "",
-                                    Duration.of(1, ChronoUnit.SECONDS),
-                                    new PollUnseen(HandlingStrategy.READ, "")));
-
-            doNothing().when(inboundConnectorContext).log(any(Consumer.class));
-            when(inboundConnectorContext.bindProperties(EmailInboundConnectorProperties.class))
-                    .thenReturn(emailInboundConnectorProperties);
-            when(inboundConnectorContext.canActivate(any()))
-                    .thenReturn(new ActivationCheckResult.Success.CanActivate(null));
-            doThrow(new RuntimeException()).when(inboundConnectorContext).correlate(any());
-
-            proxyImap.setOkProxy();
-            this.jakartaEmailListener.startListener(inboundConnectorContext);
-
-            await()
-                    .atMost(5, TimeUnit.SECONDS)
-                    .untilAsserted(
-                            () ->
-                                    // We want to check it was called once at startup and once at poll
-                                    verify(inboundConnectorContext, atLeast(2))
-                                            .reportHealth(argThat(health -> health.getStatus() == Health.Status.UP)));
-
-            super.sendEmail("camunda@test.com", "Subject", "Content");
-
-            await()
-                    .atMost(10, TimeUnit.SECONDS)
-                    .untilAsserted(
-                            () ->
-                                    // We want to check health was down for at least 1 times
-                                    verify(inboundConnectorContext, atLeast(1))
-                                            .reportHealth(argThat(health -> health.getStatus() == Health.Status.DOWN)));
-
-            Mockito.clearInvocations(inboundConnectorContext);
-
-            doReturn(new CorrelationResult.Success.ProcessInstanceCreated(null, null, null))
-                    .when(inboundConnectorContext)
-                    .correlate(any());
-
-            await()
-                    .atMost(5, TimeUnit.SECONDS)
-                    .untilAsserted(
-                            () ->
-                                    verify(inboundConnectorContext, atLeast(1))
-                                            .correlate(
-                                                    argThat(
-                                                            correlationRequest -> {
-                                                                ReadEmailResponse response =
-                                                                        ((ReadEmailResponse) correlationRequest.getVariables());
-                                                                return response.plainTextBody().equals("Content")
-                                                                        && response.subject().equals("Subject");
-                                                            })));
-
-            await()
-                    .atMost(5, TimeUnit.SECONDS)
-                    .untilAsserted(
-                            () ->
-                                    verify(inboundConnectorContext, atLeast(1))
-                                            .reportHealth(argThat(health -> health.getStatus() == Health.Status.UP)));
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+  }
 }
