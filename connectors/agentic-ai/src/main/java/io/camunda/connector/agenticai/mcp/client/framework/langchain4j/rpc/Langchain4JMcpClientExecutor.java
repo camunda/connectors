38fa55c376a1a760097d0f2c33f5571acb19e99f
Dmitri Nikonov <dmitri.nikonov@camunda.com>
2026-01-19 07:27:04 +0100

    refactor: Reorder switch case

--- connectors/agentic-ai/src/main/java/io/camunda/connector/agenticai/mcp/client/framework/langchain4j/rpc/Langchain4JMcpClientExecutor.java
+++ connectors/agentic-ai/src/main/java/io/camunda/connector/agenticai/mcp/client/framework/langchain4j/rpc/Langchain4JMcpClientExecutor.java
@@ -49,12 +49,12 @@ public class Langchain4JMcpClientExecutor {
           new ListResourcesRequest().execute(client, filterOptions.resourceFilters());
       case LIST_RESOURCE_TEMPLATES ->
           new ListResourceTemplatesRequest().execute(client, filterOptions.resourceFilters());
+      case READ_RESOURCE ->
+              new ReadResourceRequest()
+                      .execute(client, filterOptions.resourceFilters(), operation.params());
       case LIST_PROMPTS -> new ListPromptsRequest().execute(client, filterOptions.promptFilters());
       case GET_PROMPT ->
           new GetPromptRequest().execute(client, filterOptions.promptFilters(), operation.params());
-      case READ_RESOURCE ->
-          new ReadResourceRequest()
-              .execute(client, filterOptions.resourceFilters(), operation.params());
     };
   }
 }
