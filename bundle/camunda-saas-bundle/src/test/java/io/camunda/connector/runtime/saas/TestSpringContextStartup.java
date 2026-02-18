3a6a23aa17b568210fb2e7a96eebbca4974e0434
Pavel Kotelevskii <pavel.kotelevskii@camunda.com>
2026-02-18 17:18:08 +0100

    spotless
    Signed-off-by: Pavel Kotelevskii <pavel.kotelevskii@camunda.com>

--- bundle/camunda-saas-bundle/src/test/java/io/camunda/connector/runtime/saas/TestSpringContextStartup.java
+++ bundle/camunda-saas-bundle/src/test/java/io/camunda/connector/runtime/saas/TestSpringContextStartup.java
@@ -21,7 +21,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(
