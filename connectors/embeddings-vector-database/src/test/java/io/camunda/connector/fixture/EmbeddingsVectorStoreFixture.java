4cac75d4691b641d4d857127a76d1c1d6b8430cd
Mehdi Riahi <mehdi.riahi@camunda.com>
2026-02-20 16:15:31 +0100

    test(vector-db): add tests

--- connectors/embeddings-vector-database/src/test/java/io/camunda/connector/fixture/EmbeddingsVectorStoreFixture.java
+++ connectors/embeddings-vector-database/src/test/java/io/camunda/connector/fixture/EmbeddingsVectorStoreFixture.java
@@ -7,6 +7,9 @@
 package io.camunda.connector.fixture;
 
 import io.camunda.connector.model.embedding.vector.store.AmazonManagedOpenSearchVectorStore;
+import io.camunda.connector.model.embedding.vector.store.AzureAiSearchVectorStore;
+import io.camunda.connector.model.embedding.vector.store.AzureAuthentication;
+import io.camunda.connector.model.embedding.vector.store.AzureCosmosDbNoSqlVectorStore;
 import io.camunda.connector.model.embedding.vector.store.ElasticsearchVectorStore;
 import io.camunda.connector.model.embedding.vector.store.OpenSearchVectorStore;
 
@@ -28,5 +31,25 @@ public class EmbeddingsVectorStoreFixture {
     return new AmazonManagedOpenSearchVectorStore(
         new AmazonManagedOpenSearchVectorStore.Configuration(
             "ACCESS_KEY", "SECRET_KEY", "https://opensearch.aws", "us-east-1", "embeddings_idx"));
+  }
+
+  public static AzureCosmosDbNoSqlVectorStore createAzureCosmosDbNoSqlVectorStore() {
+    return new AzureCosmosDbNoSqlVectorStore(
+        new AzureCosmosDbNoSqlVectorStore.Configuration(
+            "https://example.documents.azure.com:443/",
+            new AzureAuthentication.AzureApiKeyAuthentication("api-key"),
+            "database-name",
+            "container-name",
+            AzureCosmosDbNoSqlVectorStore.ConsistencyLevel.STRONG,
+            AzureCosmosDbNoSqlVectorStore.DistanceFunction.COSINE,
+            AzureCosmosDbNoSqlVectorStore.IndexType.FLAT));
+  }
+
+  public static AzureAiSearchVectorStore createAzureAiSearchVectorStore() {
+    return new AzureAiSearchVectorStore(
+        new AzureAiSearchVectorStore.Configuration(
+            "https://your-search-service.search.windows.net",
+            new AzureAuthentication.AzureApiKeyAuthentication("api-key"),
+            "searchindex"));
   }
 }
