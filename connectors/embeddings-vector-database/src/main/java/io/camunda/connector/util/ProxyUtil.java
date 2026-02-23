9cde62e7fb32a912adc9cdc8168c47dde4f36a10
Mehdi Riahi <mehdi.riahi@camunda.com>
2026-02-23 15:54:43 +0100

    refactor(vector-db): fix bedrock embedding model initialization

--- connectors/embeddings-vector-database/src/main/java/io/camunda/connector/util/ProxyUtil.java
+++ connectors/embeddings-vector-database/src/main/java/io/camunda/connector/util/ProxyUtil.java
@@ -6,8 +6,12 @@
  */
 package io.camunda.connector.util;
 
+import io.camunda.connector.http.client.proxy.NonProxyHosts;
 import io.camunda.connector.http.client.proxy.ProxyConfiguration;
 import java.net.URI;
+import java.util.stream.Collectors;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
 
 public final class ProxyUtil {
   private ProxyUtil() {}
@@ -15,5 +19,31 @@ public final class ProxyUtil {
   public static URI toUri(ProxyConfiguration.ProxyDetails proxyDetails) {
     return URI.create(
         proxyDetails.scheme() + "://" + proxyDetails.host() + ":" + proxyDetails.port());
+  }
+
+  public static SdkHttpClient createAwsProxyAwareHttpClient(
+      ProxyConfiguration proxyConfig, String schemeName) {
+    software.amazon.awssdk.http.apache.ProxyConfiguration.Builder awsProxyConfigBuilder =
+        software.amazon.awssdk.http.apache.ProxyConfiguration.builder()
+            .useSystemPropertyValues(true);
+
+    proxyConfig
+        .getProxyDetails(schemeName)
+        .ifPresent(
+            proxyDetails -> {
+              awsProxyConfigBuilder
+                  .scheme(proxyDetails.scheme())
+                  .endpoint(ProxyUtil.toUri(proxyDetails))
+                  // the SDK does not sanitize the patterns
+                  .nonProxyHosts(
+                      NonProxyHosts.getNonProxyHostRegexPatterns().collect(Collectors.toSet()));
+
+              if (proxyDetails.hasCredentials()) {
+                awsProxyConfigBuilder.username(proxyDetails.user());
+                awsProxyConfigBuilder.password(proxyDetails.password());
+              }
+            });
+
+    return ApacheHttpClient.builder().proxyConfiguration(awsProxyConfigBuilder.build()).build();
   }
 }
