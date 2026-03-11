88412da458e336a3c3c9bd6a6750858f4f54ac33
Pavel Kotelevskii <pavel.kotelevskii@camunda.com>
2026-03-11 16:37:41 +0100

    test: fix NonProxyHostsTest
    Signed-off-by: Pavel Kotelevskii <pavel.kotelevskii@camunda.com>

--- connector-commons/http-client/src/test/java/io/camunda/connector/http/client/proxy/NonProxyHostsTest.java
+++ connector-commons/http-client/src/test/java/io/camunda/connector/http/client/proxy/NonProxyHostsTest.java
@@ -20,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -27,6 +28,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class NonProxyHostsTest {
 
+  @BeforeEach
   @AfterEach
   public void clearSystemProperties() {
     System.clearProperty("http.nonProxyHosts");
