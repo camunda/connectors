33959eccf23d3f3787e2039958f04b8712bee676
Pavel Kotelevskii <pavel.kotelevskii@camunda.com>
2026-03-11 12:35:56 +0100

    feat: smart activation condition check (#6599)
    * feat: smart activation condition check
    
    Signed-off-by: Pavel Kotelevskii <pavel.kotelevskii@camunda.com>
    
    * test: add a test for the smart compatibility check
    
    Signed-off-by: Pavel Kotelevskii <pavel.kotelevskii@camunda.com>
    
    * add message ID/ttl to check; improve logging
    
    Signed-off-by: Pavel Kotelevskii <pavel.kotelevskii@camunda.com>
    
    * use stream.distinct
    
    Signed-off-by: Pavel Kotelevskii <pavel.kotelevskii@camunda.com>
    
    ---------
    
    Signed-off-by: Pavel Kotelevskii <pavel.kotelevskii@camunda.com>

---
+++ connector-runtime/connector-runtime-core/src/test/java/io/camunda/connector/runtime/core/inbound/correlation/ActivationConditionEvaluatorTest.java
@@ -0,0 +1,413 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.connector.runtime.core.inbound.correlation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import io.camunda.connector.api.inbound.ActivationCheckResult;
+import io.camunda.connector.feel.FeelEngineWrapper;
+import io.camunda.connector.runtime.core.inbound.InboundConnectorElement;
+import io.camunda.connector.runtime.core.inbound.ProcessElementWithRuntimeData;
+import io.camunda.connector.runtime.core.inbound.correlation.MessageCorrelationPoint.StandaloneMessageCorrelationPoint;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for {@link ActivationConditionEvaluator}, focusing on the smart compatibility check.
+ */
+public class ActivationConditionEvaluatorTest {
+
+  private ActivationConditionEvaluator evaluator;
+
+  @BeforeEach
+  void setUp() {
+    evaluator = new ActivationConditionEvaluator(new FeelEngineWrapper());
+  }
+
+  private InboundConnectorElement createMessageElement(
+      String elementId,
+      String messageName,
+      String activationCondition,
+      String resultExpression,
+      String resultVariable,
+      String correlationKeyExpression) {
+    return createMessageElement(
+        elementId,
+        messageName,
+        activationCondition,
+        resultExpression,
+        resultVariable,
+        correlationKeyExpression,
+        null,
+        Duration.ofHours(1));
+  }
+
+  private InboundConnectorElement createMessageElement(
+      String elementId,
+      String messageName,
+      String activationCondition,
+      String resultExpression,
+      String resultVariable,
+      String correlationKeyExpression,
+      String messageIdExpression,
+      Duration timeToLive) {
+    var element = mock(InboundConnectorElement.class);
+    var correlationPoint =
+        new StandaloneMessageCorrelationPoint(
+            messageName, correlationKeyExpression, messageIdExpression, timeToLive);
+    when(element.correlationPoint()).thenReturn(correlationPoint);
+    when(element.element())
+        .thenReturn(new ProcessElementWithRuntimeData("process1", 0, 0, elementId, "default"));
+    when(element.activationCondition()).thenReturn(activationCondition);
+    when(element.resultExpression()).thenReturn(resultExpression);
+    when(element.resultVariable()).thenReturn(resultVariable);
+    return element;
+  }
+
+  private InboundConnectorElement createStartEventElement(
+      String elementId, String activationCondition) {
+    var element = mock(InboundConnectorElement.class);
+    var correlationPoint = new StartEventCorrelationPoint("process1", 0, 0);
+    when(element.correlationPoint()).thenReturn(correlationPoint);
+    when(element.element())
+        .thenReturn(new ProcessElementWithRuntimeData("process1", 0, 0, elementId, "default"));
+    when(element.activationCondition()).thenReturn(activationCondition);
+    return element;
+  }
+
+  @Nested
+  @DisplayName("Single element scenarios")
+  class SingleElement {
+
+    @Test
+    @DisplayName("Single element with matching condition should activate")
+    void singleElement_matchingCondition_shouldActivate() {
+      var element = createMessageElement("elem1", "msg1", "=true", null, null, "=key");
+
+      var result = evaluator.checkActivation(List.of(element), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Success.CanActivate.class);
+    }
+
+    @Test
+    @DisplayName("Single element with non-matching condition should not activate")
+    void singleElement_nonMatchingCondition_shouldNotActivate() {
+      var element = createMessageElement("elem1", "msg1", "=false", null, null, "=key");
+
+      var result = evaluator.checkActivation(List.of(element), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.NoMatchingElement.class);
+    }
+
+    @Test
+    @DisplayName("Single element with blank condition should activate")
+    void singleElement_blankCondition_shouldActivate() {
+      var element = createMessageElement("elem1", "msg1", "", null, null, "=key");
+
+      var result = evaluator.checkActivation(List.of(element), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Success.CanActivate.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Multiple elements with different activation conditions")
+  class MultipleElementsDifferentConditions {
+
+    @Test
+    @DisplayName("Multiple elements where only one matches should activate that one")
+    void multipleElements_oneMatches_shouldActivateThatOne() {
+      var element1 = createMessageElement("elem1", "msg1", "=type = \"A\"", null, null, "=key");
+      var element2 = createMessageElement("elem2", "msg2", "=type = \"B\"", null, null, "=key");
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of("type", "A"));
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Success.CanActivate.class);
+      var success = (ActivationCheckResult.Success.CanActivate) result;
+      assertThat(success.activatedElement().elementId()).isEqualTo("elem1");
+    }
+  }
+
+  @Nested
+  @DisplayName("Compatible message elements (same message name, same properties)")
+  class CompatibleMessageElements {
+
+    @Test
+    @DisplayName("Two message elements with same name and same properties should be compatible")
+    void twoElements_sameMessageName_sameProperties_shouldBeCompatible() {
+      var element1 =
+          createMessageElement(
+              "elem1", "shared-msg", "", "=result", "outputVar", "=correlationKey");
+      var element2 =
+          createMessageElement(
+              "elem2", "shared-msg", "", "=result", "outputVar", "=correlationKey");
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Success.CanActivate.class);
+    }
+
+    @Test
+    @DisplayName("Three message elements with same name and same properties should be compatible")
+    void threeElements_sameMessageName_sameProperties_shouldBeCompatible() {
+      var element1 =
+          createMessageElement(
+              "elem1", "shared-msg", "", "=result", "outputVar", "=correlationKey");
+      var element2 =
+          createMessageElement(
+              "elem2", "shared-msg", "", "=result", "outputVar", "=correlationKey");
+      var element3 =
+          createMessageElement(
+              "elem3", "shared-msg", "", "=result", "outputVar", "=correlationKey");
+
+      var result = evaluator.checkActivation(List.of(element1, element2, element3), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Success.CanActivate.class);
+    }
+
+    @Test
+    @DisplayName("Elements with null properties should be compatible if all null")
+    void elements_nullProperties_shouldBeCompatibleIfAllNull() {
+      var element1 = createMessageElement("elem1", "shared-msg", "", null, null, null);
+      var element2 = createMessageElement("elem2", "shared-msg", "", null, null, null);
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Success.CanActivate.class);
+    }
+
+    @Test
+    @DisplayName("Elements with same messageIdExpression and timeToLive should be compatible")
+    void elements_sameMessageIdAndTtl_shouldBeCompatible() {
+      var element1 =
+          createMessageElement(
+              "elem1",
+              "shared-msg",
+              "",
+              "=result",
+              "outputVar",
+              "=correlationKey",
+              "=msgId",
+              Duration.ofMinutes(30));
+      var element2 =
+          createMessageElement(
+              "elem2",
+              "shared-msg",
+              "",
+              "=result",
+              "outputVar",
+              "=correlationKey",
+              "=msgId",
+              Duration.ofMinutes(30));
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Success.CanActivate.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Incompatible message elements (same message name, different properties)")
+  class IncompatibleMessageElements {
+
+    @Test
+    @DisplayName("Different resultExpression should be incompatible")
+    void differentResultExpression_shouldBeIncompatible() {
+      var element1 =
+          createMessageElement("elem1", "shared-msg", "", "=result.v1", "outputVar", "=key");
+      var element2 =
+          createMessageElement("elem2", "shared-msg", "", "=result.v2", "outputVar", "=key");
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+
+    @Test
+    @DisplayName("Different resultVariable should be incompatible")
+    void differentResultVariable_shouldBeIncompatible() {
+      var element1 = createMessageElement("elem1", "shared-msg", "", "=result", "var1", "=key");
+      var element2 = createMessageElement("elem2", "shared-msg", "", "=result", "var2", "=key");
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+
+    @Test
+    @DisplayName("Different correlationKeyExpression should be incompatible")
+    void differentCorrelationKeyExpression_shouldBeIncompatible() {
+      var element1 = createMessageElement("elem1", "shared-msg", "", "=result", "var", "=key1");
+      var element2 = createMessageElement("elem2", "shared-msg", "", "=result", "var", "=key2");
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+
+    @Test
+    @DisplayName("One null and one non-null resultExpression should be incompatible")
+    void oneNullOneNonNull_resultExpression_shouldBeIncompatible() {
+      var element1 = createMessageElement("elem1", "shared-msg", "", null, "var", "=key");
+      var element2 = createMessageElement("elem2", "shared-msg", "", "=result", "var", "=key");
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+
+    @Test
+    @DisplayName("Different messageIdExpression should be incompatible")
+    void differentMessageIdExpression_shouldBeIncompatible() {
+      var element1 =
+          createMessageElement(
+              "elem1", "shared-msg", "", "=result", "var", "=key", "=msgId1", Duration.ofHours(1));
+      var element2 =
+          createMessageElement(
+              "elem2", "shared-msg", "", "=result", "var", "=key", "=msgId2", Duration.ofHours(1));
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+
+    @Test
+    @DisplayName("One null and one non-null messageIdExpression should be incompatible")
+    void oneNullOneNonNull_messageIdExpression_shouldBeIncompatible() {
+      var element1 =
+          createMessageElement(
+              "elem1", "shared-msg", "", "=result", "var", "=key", null, Duration.ofHours(1));
+      var element2 =
+          createMessageElement(
+              "elem2", "shared-msg", "", "=result", "var", "=key", "=msgId", Duration.ofHours(1));
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+
+    @Test
+    @DisplayName("Different timeToLive should be incompatible")
+    void differentTimeToLive_shouldBeIncompatible() {
+      var element1 =
+          createMessageElement(
+              "elem1", "shared-msg", "", "=result", "var", "=key", null, Duration.ofHours(1));
+      var element2 =
+          createMessageElement(
+              "elem2", "shared-msg", "", "=result", "var", "=key", null, Duration.ofHours(2));
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+
+    @Test
+    @DisplayName("One null and one non-null timeToLive should be incompatible")
+    void oneNullOneNonNull_timeToLive_shouldBeIncompatible() {
+      var element1 =
+          createMessageElement("elem1", "shared-msg", "", "=result", "var", "=key", null, null);
+      var element2 =
+          createMessageElement(
+              "elem2", "shared-msg", "", "=result", "var", "=key", null, Duration.ofHours(1));
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Different message names (always incompatible when both match)")
+  class DifferentMessageNames {
+
+    @Test
+    @DisplayName("Different message names with blank conditions should be incompatible")
+    void differentMessageNames_blankConditions_shouldBeIncompatible() {
+      var element1 = createMessageElement("elem1", "msg-A", "", "=result", "var", "=key");
+      var element2 = createMessageElement("elem2", "msg-B", "", "=result", "var", "=key");
+
+      var result = evaluator.checkActivation(List.of(element1, element2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Mixed correlation point types")
+  class MixedCorrelationPointTypes {
+
+    @Test
+    @DisplayName("Mix of message and start event elements should be incompatible")
+    void mixedTypes_shouldBeIncompatible() {
+      var messageElement = createMessageElement("elem1", "msg1", "", "=result", "var", "=key");
+      var startElement = createStartEventElement("elem2", "");
+
+      var result = evaluator.checkActivation(List.of(messageElement, startElement), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+
+    @Test
+    @DisplayName("Two start event elements with blank conditions should be incompatible")
+    void twoStartEvents_blankConditions_shouldBeIncompatible() {
+      var startElement1 = createStartEventElement("elem1", "");
+      var startElement2 = createStartEventElement("elem2", "");
+
+      var result = evaluator.checkActivation(List.of(startElement1, startElement2), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.TooManyMatchingElements.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("consumeUnmatchedEvents flag")
+  class ConsumeUnmatchedEvents {
+
+    @Test
+    @DisplayName("No matching elements with consumeUnmatchedEvents=true should return flag")
+    void noMatch_consumeUnmatchedTrue_shouldReturnFlag() {
+      var element = createMessageElement("elem1", "msg1", "=false", null, null, "=key");
+      when(element.consumeUnmatchedEvents()).thenReturn(true);
+
+      var result = evaluator.checkActivation(List.of(element), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.NoMatchingElement.class);
+      var failure = (ActivationCheckResult.Failure.NoMatchingElement) result;
+      assertThat(failure.discardUnmatchedEvents()).isTrue();
+    }
+
+    @Test
+    @DisplayName("No matching elements with consumeUnmatchedEvents=false should return flag")
+    void noMatch_consumeUnmatchedFalse_shouldReturnFlag() {
+      var element = createMessageElement("elem1", "msg1", "=false", null, null, "=key");
+      when(element.consumeUnmatchedEvents()).thenReturn(false);
+
+      var result = evaluator.checkActivation(List.of(element), Map.of());
+
+      assertThat(result).isInstanceOf(ActivationCheckResult.Failure.NoMatchingElement.class);
+      var failure = (ActivationCheckResult.Failure.NoMatchingElement) result;
+      assertThat(failure.discardUnmatchedEvents()).isFalse();
+    }
+  }
+}
---
+++ connectors-e2e-test/connectors-e2e-test-inbound-runtime/src/test/java/io/camunda/connector/e2e/inbound/ActivationConditionMultiVersionTests.java
@@ -0,0 +1,234 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.connector.e2e.inbound;
+
+import static io.camunda.connector.e2e.BpmnFile.Replace.replace;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.connector.api.inbound.CorrelationResult;
+import io.camunda.connector.api.inbound.Health;
+import io.camunda.connector.e2e.BpmnFile;
+import io.camunda.connector.e2e.ElementTemplate;
+import io.camunda.connector.e2e.inbound.InboundConnectorTestConfiguration.InboundConnectorTestHelper;
+import io.camunda.connector.runtime.core.inbound.correlation.InboundCorrelationHandler;
+import io.camunda.connector.runtime.inbound.executable.ActiveExecutableQuery;
+import io.camunda.connector.runtime.inbound.executable.ActiveExecutableResponse;
+import io.camunda.connector.runtime.inbound.executable.InboundExecutableRegistry;
+import io.camunda.connector.test.utils.annotation.SlowTest;
+import io.camunda.process.test.api.CamundaSpringProcessTest;
+import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
+import java.io.File;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Tests for activation condition validation with multi-version inbound connectors.
+ *
+ * <p>These tests verify the behavior when multiple process versions share the same inbound
+ * connector configuration (deduplicated into a single executable) and have blank/no activation
+ * conditions.
+ *
+ * <p>The issue being tested: When 2+ process versions with the same inbound connector config are
+ * deployed and deduplicated, intermediate catch events with blank activation conditions should
+ * still correlate correctly because they have distinct correlation keys. The runtime should not
+ * fail with "TooManyMatchingElements" error.
+ */
+@SpringBootTest(
+    classes = TestConnectorRuntimeApplication.class,
+    properties = {
+      "spring.main.allow-bean-definition-overriding=true",
+      "camunda.connector.polling.enabled=true"
+    },
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@CamundaSpringProcessTest
+@Import(InboundConnectorTestConfiguration.class)
+@SlowTest
+public class ActivationConditionMultiVersionTests {
+
+  private static final Duration AWAIT_TIMEOUT = Duration.ofSeconds(30);
+  private static final String INBOUND_BPMN = "bpmn/inbound-test-process.bpmn";
+  private static final String INBOUND_TEMPLATE = "element-templates/test-inbound-connector.json";
+
+  @TempDir File tempDir;
+
+  @Autowired CamundaClient camundaClient;
+  @Autowired InboundExecutableRegistry executableRegistry;
+  @Autowired InboundConnectorTestHelper inboundConnectorTestHelper;
+  @Autowired InboundCorrelationHandler correlationHandler;
+
+  private volatile String testProcessId;
+
+  @BeforeEach
+  void setUp() {
+    testProcessId = "activationConditionTest_" + UUID.randomUUID().toString().substring(0, 8);
+    TestInboundConnector.resetCounters();
+    inboundConnectorTestHelper.setUpTest();
+  }
+
+  /**
+   * Creates a BPMN model with an Intermediate Catch Event connector. No activation condition is
+   * set, which is the default/blank state.
+   */
+  private BpmnModelInstance createProcessWithBlankActivationCondition(
+      String configValue, String deduplicationId) {
+    var bpmnFile = getResourceFile(INBOUND_BPMN);
+    var modifiedBpmn = BpmnFile.replace(bpmnFile, replace("inboundTestProcess", testProcessId));
+
+    var tempBpmnFile = new File(tempDir, testProcessId + ".bpmn");
+    Bpmn.writeModelToFile(tempBpmnFile, modifiedBpmn);
+
+    var templateBuilder =
+        ElementTemplate.from(getResourcePath(INBOUND_TEMPLATE))
+            .property("configValue", configValue)
+            .property("correlationKeyProcess", "=correlationKey")
+            .property("correlationKeyPayload", "=body.correlationKey")
+            .property("deduplicationModeManualFlag", deduplicationId != null ? "true" : "false");
+    // Note: No activationCondition is set - this is intentionally blank
+
+    if (deduplicationId != null) {
+      templateBuilder.property("deduplicationId", deduplicationId);
+    }
+
+    var templateFile = templateBuilder.writeTo(new File(tempDir, "template.json"));
+    return new BpmnFile(tempBpmnFile)
+        .apply(templateFile, "catchEvent1", new File(tempDir, testProcessId + "-result.bpmn"));
+  }
+
+  private long deploy(BpmnModelInstance model) {
+    var deployment =
+        camundaClient
+            .newDeployResourceCommand()
+            .addProcessModel(model, testProcessId + ".bpmn")
+            .send()
+            .join();
+    return deployment.getProcesses().getFirst().getProcessDefinitionKey();
+  }
+
+  private void waitForProcessDefinitionIndexed(long processDefinitionKey) {
+    Awaitility.await("process definition should be indexed")
+        .atMost(AWAIT_TIMEOUT)
+        .ignoreExceptions()
+        .untilAsserted(
+            () -> camundaClient.newProcessDefinitionGetRequest(processDefinitionKey).send().join());
+  }
+
+  private List<ActiveExecutableResponse> queryExecutables(String processId) {
+    return executableRegistry.query(new ActiveExecutableQuery(processId, null, null, null));
+  }
+
+  private void awaitHealthyExecutable(String processId) {
+    Awaitility.await("process " + processId + " should have healthy executable")
+        .atMost(AWAIT_TIMEOUT)
+        .untilAsserted(
+            () -> {
+              var executables = queryExecutables(processId);
+              assertThat(executables).isNotEmpty();
+              assertThat(executables.getFirst().health().getStatus()).isEqualTo(Health.Status.UP);
+            });
+  }
+
+  private File getResourceFile(String resourceName) {
+    try {
+      var resource = getClass().getClassLoader().getResource(resourceName);
+      if (resource == null) {
+        throw new IllegalArgumentException("Resource not found: " + resourceName);
+      }
+      return new File(resource.toURI());
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to get resource file: " + resourceName, e);
+    }
+  }
+
+  private String getResourcePath(String resourceName) {
+    var resource = getClass().getClassLoader().getResource(resourceName);
+    if (resource == null) {
+      throw new IllegalArgumentException("Resource not found: " + resourceName);
+    }
+    return resource.getPath();
+  }
+
+  @Test
+  @DisplayName(
+      "Two versions with same config and blank activation condition should correlate successfully")
+  void twoVersions_sameConfig_blankActivationCondition_shouldCorrelateSuccessfully() {
+    // Given: Deploy v1 with intermediate catch event connector (blank activation condition)
+    var model1 = createProcessWithBlankActivationCondition("config-a", "shared-dedup");
+    long keyV1 = deploy(model1);
+    waitForProcessDefinitionIndexed(keyV1);
+    awaitHealthyExecutable(testProcessId);
+
+    // Start instance on v1 - creates message subscription
+    camundaClient
+        .newCreateInstanceCommand()
+        .processDefinitionKey(keyV1)
+        .variable("correlationKey", "correlation-key-v1")
+        .send()
+        .join();
+
+    // Deploy v2 with same config (will be deduplicated)
+    var model2 = createProcessWithBlankActivationCondition("config-a", "shared-dedup");
+    long keyV2 = deploy(model2);
+    waitForProcessDefinitionIndexed(keyV2);
+
+    // Wait for executable to have both version elements
+    Awaitility.await("executable should have elements from both versions")
+        .atMost(AWAIT_TIMEOUT)
+        .untilAsserted(
+            () -> {
+              var executables = queryExecutables(testProcessId);
+              assertThat(executables).hasSize(1);
+              assertThat(executables.getFirst().elements()).hasSize(2);
+            });
+
+    // Start instance on v2 - creates another message subscription with different correlation key
+    camundaClient
+        .newCreateInstanceCommand()
+        .processDefinitionKey(keyV2)
+        .variable("correlationKey", "correlation-key-v2")
+        .send()
+        .join();
+
+    // When: Try to correlate a message through the correlation handler
+    // This should NOT fail with "TooManyMatchingElements" because:
+    // - Both elements have blank activation conditions (both "match")
+    // - BUT they have different correlation keys, so Zeebe will route correctly
+    var executables = queryExecutables(testProcessId);
+    var elements = executables.getFirst().elements();
+
+    // Correlate with payload targeting v1's correlation key
+    var correlationResult =
+        correlationHandler.correlate(
+            elements, Map.of("body", Map.of("correlationKey", "correlation-key-v1")));
+
+    // then
+    assertThat(correlationResult)
+        .as("Correlation should succeed without TooManyMatchingElements error")
+        .isInstanceOf(CorrelationResult.Success.MessagePublished.class);
+  }
+}
