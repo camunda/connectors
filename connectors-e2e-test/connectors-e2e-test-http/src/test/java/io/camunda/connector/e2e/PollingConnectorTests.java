2775c561af8bce9f89887de3b85cbe5fba45f434
Pavel Kotelevskii <pavel.kotelevskii@camunda.com>
2026-03-23 11:23:31 +0100

    test: add test case for polling connector with cluster vars
    Signed-off-by: Pavel Kotelevskii <pavel.kotelevskii@camunda.com>

--- connectors-e2e-test/connectors-e2e-test-http/src/test/java/io/camunda/connector/e2e/PollingConnectorTests.java
+++ connectors-e2e-test/connectors-e2e-test-http/src/test/java/io/camunda/connector/e2e/PollingConnectorTests.java
@@ -31,7 +31,10 @@ import io.camunda.connector.e2e.app.TestConnectorRuntimeApplication;
 import io.camunda.connector.test.utils.annotation.SlowTest;
 import io.camunda.process.test.api.CamundaSpringProcessTest;
 import io.camunda.zeebe.model.bpmn.instance.Process;
+import java.time.Duration;
 import java.util.Map;
+import java.util.UUID;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -53,6 +56,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 @SlowTest
 @ExtendWith(MockitoExtension.class)
 public class PollingConnectorTests {
+
+  private static final Duration CLUSTER_VAR_TIMEOUT = Duration.ofSeconds(30);
 
   @RegisterExtension
   static WireMockExtension wm =
@@ -100,5 +105,71 @@ public class PollingConnectorTests {
 
     bpmnTest.waitForProcessCompletion();
     assertThat(bpmnTest.getProcessInstanceEvent()).hasVariable("isOrderSuccessful", "true");
+  }
+
+  @Test
+  void shouldResolveClusterVariableInUrlAndTerminate() {
+    wm.stubFor(
+        get(urlEqualTo("/mock"))
+            .willReturn(
+                aResponse()
+                    .withHeader("Content-Type", "application/json")
+                    .withBody("{\"orderId\": \"1234\", \"successfulOrder\": \"true\"}")
+                    .withStatus(200)));
+
+    var variableName =
+        "pollingUrlVar_" + UUID.randomUUID().toString().replace("-", "").substring(0, 8);
+    var variableValue = "mock";
+    camundaClient
+        .newGloballyScopedClusterVariableCreateRequest()
+        .create(variableName, variableValue)
+        .send()
+        .join();
+    awaitClusterVariableAvailable(variableName);
+
+    var mockUrl =
+        "=&#34;http://localhost:" + wm.getPort() + "/&#34; + camunda.vars.env." + variableName;
+
+    var model =
+        replace(
+            "polling_connector.bpmn",
+            replace("URL_SET", mockUrl),
+            replace("CORRELATION_KEY_EXPRESSION", "=body.orderId"),
+            replace("CORRELATION_KEY_PROCESS", "=orderId"),
+            replace(
+                "RESULT_EXPRESSION", "={ &#34;isOrderSuccessful&#34; : body.successfulOrder  }"));
+
+    when(processDef.getProcessDefinitionKey()).thenReturn(2L);
+    when(processDef.getTenantId())
+        .thenReturn(camundaClient.getConfiguration().getDefaultTenantId());
+    when(processDef.getProcessDefinitionId())
+        .thenReturn(model.getModelElementsByType(Process.class).stream().findFirst().get().getId());
+
+    var bpmnTest =
+        ZeebeTest.with(camundaClient).deploy(model).createInstance(Map.of("orderId", "1234"));
+
+    bpmnTest.waitForProcessCompletion();
+    assertThat(bpmnTest.getProcessInstanceEvent()).hasVariable("isOrderSuccessful", "true");
+  }
+
+  private void awaitClusterVariableAvailable(String variableName) {
+    Awaitility.await("cluster variable " + variableName + " should be available")
+        .atMost(CLUSTER_VAR_TIMEOUT)
+        .pollInterval(Duration.ofMillis(500))
+        .ignoreExceptions()
+        .until(
+            () -> {
+              try {
+                var result =
+                    camundaClient
+                        .newEvaluateExpressionCommand()
+                        .expression("=camunda.vars.env." + variableName)
+                        .send()
+                        .join();
+                return result.getResult() != null;
+              } catch (Exception e) {
+                return false;
+              }
+            });
   }
 }
