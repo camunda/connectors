409e3a6e60231645ffe4983868261b3ce1c83ece
Jonathan Roques <jonathan.roques@camunda.com>
2026-05-13 14:57:02 +0200

    fix: prefer connector virtual thread executor

--- connector-runtime/spring-boot-starter-camunda-connectors/src/main/java/io/camunda/connector/runtime/OutboundConnectorsAutoConfiguration.java
+++ connector-runtime/spring-boot-starter-camunda-connectors/src/main/java/io/camunda/connector/runtime/OutboundConnectorsAutoConfiguration.java
@@ -18,6 +18,8 @@ package io.camunda.connector.runtime;
 
 import io.camunda.client.jobhandling.CamundaClientExecutorService;
 import io.camunda.client.metrics.MeteredCamundaClientExecutorService;
+import io.camunda.client.spring.configuration.CamundaAutoConfiguration;
+import io.camunda.client.spring.configuration.ExecutorServiceConfiguration;
 import io.camunda.connector.runtime.instances.InstanceForwardingConfiguration;
 import io.camunda.connector.runtime.outbound.OutboundConnectorRuntimeConfiguration;
 import io.micrometer.core.instrument.MeterRegistry;
@@ -25,12 +27,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
+@AutoConfigureBefore({CamundaAutoConfiguration.class, ExecutorServiceConfiguration.class})
 @Import({OutboundConnectorRuntimeConfiguration.class, InstanceForwardingConfiguration.class})
 public class OutboundConnectorsAutoConfiguration {
 
@@ -40,7 +44,7 @@ public class OutboundConnectorsAutoConfiguration {
       name = "camunda.connector.virtual-threads.enabled",
       havingValue = "true",
       matchIfMissing = true)
-  public CamundaClientExecutorService camundaClientExecutorService(
+  public CamundaClientExecutorService connectorCamundaClientExecutorService(
       @Autowired(required = false) MeterRegistry meterRegistry) {
     ThreadFactory factory = Thread.ofVirtual().name("job-worker-virtual-", 0).factory();
     var vThreadExecutor = Executors.newThreadPerTaskExecutor(factory);
