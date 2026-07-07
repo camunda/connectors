f99dc9286ac2afbeda4b13235234abdec6ce870e
ztefanie <stefanie.metzger@camunda.com>
2026-07-07 10:55:45 +0200

    feat(validator): auto-detect inbound connectors by elementType instead of manual listing

--- element-template-generator/validator/src/main/java/io/camunda/connector/validator/rule/StepLeafShapeRule.java
+++ element-template-generator/validator/src/main/java/io/camunda/connector/validator/rule/StepLeafShapeRule.java
@@ -33,7 +33,7 @@ public class StepLeafShapeRule implements Rule {
 
   @Override
   public List<Finding> apply(Path file, JsonNode template) {
-    if (OperationMetadataIgnoreList.isIgnored(file)) {
+    if (OperationMetadataIgnoreList.isIgnored(file, template)) {
       return List.of();
     }
     JsonNode steps = template.path(ElementTemplate.STEPS);
