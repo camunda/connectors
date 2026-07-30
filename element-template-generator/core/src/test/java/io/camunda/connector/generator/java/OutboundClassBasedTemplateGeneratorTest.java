cb3d12c7f3214b357d850291d6ec580a07ceb96c
Jonathan Roques <jonathan.roques@camunda.com>
2026-07-30 11:42:55 +0200

    feat: add jobTimeout property to the generated Retries group

--- element-template-generator/core/src/test/java/io/camunda/connector/generator/java/OutboundClassBasedTemplateGeneratorTest.java
+++ element-template-generator/core/src/test/java/io/camunda/connector/generator/java/OutboundClassBasedTemplateGeneratorTest.java
@@ -256,6 +256,17 @@ public class OutboundClassBasedTemplateGeneratorTest extends BaseTest {
     }
 
     @Test
+    void jobTimeoutProperty() {
+      var template = generator.generate(MyConnectorFunction.MinimallyAnnotated.class).getFirst();
+      var property = getPropertyByLabel("Job timeout", template);
+      assertThat(property.getType()).isEqualTo("String");
+      assertThat(property.getBinding().type()).isEqualTo("zeebe:taskHeader");
+      assertThat(((ZeebeTaskHeader) property.getBinding()).key()).isEqualTo("jobTimeout");
+      assertThat(property.getGroup()).isEqualTo("retries");
+      assertThat(property.getValue()).isNull();
+    }
+
+    @Test
     void retryCountProperty() {
       var templates = generator.generate(MyConnectorFunction.MinimallyAnnotated.class);
       var property = getPropertyByLabel("Retries", templates.getFirst());
@@ -1458,7 +1469,7 @@ public class OutboundClassBasedTemplateGeneratorTest extends BaseTest {
       assertThat(template.id()).isNotNull();
       assertThat(template.id()).isEqualTo(OperationAnnotatedConnector.ID);
       assertThat(template.name()).isEqualTo(OperationAnnotatedConnector.NAME);
-      assertThat(template.properties()).hasSize(14);
+      assertThat(template.properties()).hasSize(15);
 
       DropdownProperty operationProperty =
           (DropdownProperty) getPropertyById("operation", template);
